99afba41000e305ae3c9a78f6e43c0aac4a6d800
Christian Hergert <christian@hergert.me>
2014-12-04 13:33:37 -0800

    GbTabStack: allow model to be set after creation

--- src/tabs/gb-tab-stack.c
+++ src/tabs/gb-tab-stack.c
@@ -605,7 +605,6 @@ gb_tab_stack_class_init (GbTabStackClass *klass)
                          _("The model containing the buffers."),
                          GTK_TYPE_TREE_MODEL,
                          (G_PARAM_READWRITE |
-                          G_PARAM_CONSTRUCT_ONLY |
                           G_PARAM_STATIC_STRINGS));
   g_object_class_install_property (object_class, PROP_MODEL,
                                    gParamSpecs [PROP_MODEL]);
