7dedcfc6b1e5f7c8b5edbc2984e6390377768453
Christian Hergert <christian@hergert.me>
2014-12-14 20:23:43 -0800

    search-context: add signature for getting results

--- src/search/gb-search-context.h
+++ src/search/gb-search-context.h
@@ -55,6 +55,7 @@ GType            gb_search_context_get_type        (void);
 GbSearchContext *gb_search_context_new             (const GList      *providers,
                                                     const gchar      *search_text);
 void             gb_search_context_cancel          (GbSearchContext  *context);
+const GList     *gb_search_context_get_results     (GbSearchContext  *context);
 void             gb_search_context_add_results     (GbSearchContext  *context,
                                                     GbSearchProvider *provider,
                                                     GList            *results,
