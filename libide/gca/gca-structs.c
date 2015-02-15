2f3bfde2d8791717655e2d5052acce5656ad2c25
Christian Hergert <christian@hergert.me>
2015-02-14 21:35:33 -0800

    libide: start on IdeGcaService
    This service will help us manage connecting to GCA and providing proxy
    structures. Doing so will allow us to progress towards creating an
    IdeGcaDiagnosticsProvider. Once that is in place, we basically have
    everything we have today in src/gca, but with the ability to bring some
    parts in process (like clang).

---
+++ libide/gca/gca-structs.c
@@ -0,0 +1,106 @@
+/* gca-structs.c
+ *
+ * Copyright (C) 2014 Christian Hergert <christian@hergert.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "gca-structs.h"
+
+static void
+gca_diagnostic_destroy (gpointer data)
+{
+  GcaDiagnostic *diag = data;
+
+  if (diag)
+    {
+      g_array_unref (diag->fixits);
+      g_array_unref (diag->locations);
+      g_free (diag->message);
+    }
+}
+
+static void
+gca_fixit_destroy (gpointer data)
+{
+  GcaFixit *fixit = data;
+
+  if (fixit)
+    g_free (fixit->value);
+}
+
+GArray *
+gca_diagnostics_from_variant (GVariant *variant)
+{
+  GVariantIter iter;
+  GArray *ret;
+  GVariantIter *b;
+  GVariantIter *c;
+  gchar *d;
+  guint a;
+
+  g_return_val_if_fail (variant, NULL);
+
+  ret = g_array_new (FALSE, FALSE, sizeof (GcaDiagnostic));
+
+  g_array_set_clear_func (ret, gca_diagnostic_destroy);
+
+  g_variant_iter_init (&iter, variant);
+
+  while (g_variant_iter_loop (&iter, "(ua((x(xx)(xx))s)a(x(xx)(xx))s)",
+                              &a, &b, &c, &d))
+    {
+      GcaDiagnostic diag = { 0 };
+      gint64 x1, x2, x3, x4, x5;
+      gchar *e;
+
+      diag.severity = a;
+      diag.fixits = g_array_new (FALSE, FALSE, sizeof (GcaFixit));
+      diag.locations = g_array_new (FALSE, FALSE, sizeof (GcaSourceRange));
+      diag.message = g_strdup (d);
+
+      g_array_set_clear_func (diag.fixits, gca_fixit_destroy);
+
+      while (g_variant_iter_next (b, "((x(xx)(xx))s)", &x1, &x2, &x3, &x4, &x5, &e))
+        {
+          GcaFixit fixit = {{ 0 }};
+
+          fixit.range.file = x1;
+          fixit.range.begin.line = x2 - 1;
+          fixit.range.begin.column = x3 - 1;
+          fixit.range.end.line = x4 - 1;
+          fixit.range.end.column = x5 - 1;
+          fixit.value = g_strdup (e);
+
+          g_array_append_val (diag.fixits, fixit);
+        }
+
+      while (g_variant_iter_next (c, "(x(xx)(xx))", &x1, &x2, &x3, &x4, &x5))
+        {
+          GcaSourceRange range = { 0 };
+
+          range.file = x1;
+          range.begin.line = x2 - 1;
+          range.begin.column = x3 - 1;
+          range.end.line = x4 - 1;
+          range.end.column = x5 - 1;
+
+          g_array_append_val (diag.locations, range);
+        }
+
+      g_array_append_val (ret, diag);
+    }
+
+  return ret;
+}
---
+++ libide/gca/ide-gca-service.c
@@ -0,0 +1,155 @@
+/* ide-gca-service.c
+ *
+ * Copyright (C) 2015 Christian Hergert <christian@hergert.me>
+ *
+ * This file is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gca-service.h>
+#include <glib/gi18n.h>
+
+#include "ide-gca-service.h"
+
+struct _IdeGcaService
+{
+  IdeService parent_instance;
+};
+
+static GDBusConnection *gDBus;
+
+G_DEFINE_TYPE (IdeGcaService, ide_gca_service, IDE_TYPE_SERVICE)
+
+static const gchar *
+remap_language (const gchar *lang_id)
+{
+  g_return_val_if_fail (lang_id, NULL);
+
+  if (g_str_equal (lang_id, "chdr") ||
+      g_str_equal (lang_id, "objc") ||
+      g_str_equal (lang_id, "cpp"))
+    return "c";
+
+  return lang_id;
+}
+
+static void
+proxy_new_cb (GObject      *object,
+              GAsyncResult *result,
+              gpointer      user_data)
+{
+  g_autoptr(GTask) task = user_data;
+  GcaService *proxy;
+  GError *error = NULL;
+
+  g_assert (G_IS_TASK (task));
+  g_assert (G_IS_ASYNC_RESULT (result));
+
+  proxy = gca_service_proxy_new_finish (result, &error);
+
+  if (!proxy)
+    {
+      g_task_return_error (task, error);
+      return;
+    }
+
+  g_task_return_pointer (task, g_object_ref (proxy), g_object_unref);
+
+  g_clear_object (&proxy);
+}
+
+void
+ide_gca_service_get_proxy_async (IdeGcaService       *self,
+                                 const gchar         *language_id,
+                                 GCancellable        *cancellable,
+                                 GAsyncReadyCallback  callback,
+                                 gpointer             user_data)
+{
+  g_autoptr(GTask) task = NULL;
+  g_autoptr(gchar) name = NULL;
+  g_autoptr(gchar) object_path = NULL;
+
+  g_return_if_fail (IDE_IS_GCA_SERVICE (self));
+  g_return_if_fail (language_id);
+  g_return_if_fail (!cancellable || G_IS_CANCELLABLE (cancellable));
+
+  task = g_task_new (self, cancellable, callback, user_data);
+
+  if (!gDBus)
+    {
+      g_task_return_new_error (task,
+                               G_IO_ERROR,
+                               G_IO_ERROR_NOT_CONNECTED,
+                               _("Not connected to DBus."));
+      return;
+    }
+
+  language_id = remap_language (language_id);
+
+  name = g_strdup_printf ("org.gnome.CodeAssist.v1.%s", language_id);
+  object_path = g_strdup_printf ("/org/gnome/CodeAssist/v1/%s", language_id);
+
+  gca_service_proxy_new (gDBus,
+                         G_DBUS_PROXY_FLAGS_NONE,
+                         name,
+                         object_path,
+                         cancellable,
+                         proxy_new_cb,
+                         g_object_ref (task));
+}
+
+GcaService *
+ide_gca_service_get_proxy_finish (IdeGcaService  *self,
+                                  GAsyncResult   *result,
+                                  GError        **error)
+{
+  GTask *task = (GTask *)result;
+
+  g_return_val_if_fail (IDE_IS_GCA_SERVICE (self), NULL);
+  g_return_val_if_fail (G_IS_TASK (task), NULL);
+
+  return g_task_propagate_pointer (task, error);
+}
+
+static void
+ide_gca_service_finalize (GObject *object)
+{
+  IdeGcaService *self = (IdeGcaService *)object;
+
+  G_OBJECT_CLASS (ide_gca_service_parent_class)->finalize (object);
+}
+
+static void
+ide_gca_service_class_init (IdeGcaServiceClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+  GError *error = NULL;
+
+  object_class->finalize = ide_gca_service_finalize;
+
+  gDBus = g_bus_get_sync (G_BUS_TYPE_SESSION, NULL, &error);
+
+  if (!gDBus)
+    {
+      g_warning (_("Failed to load DBus connection to session bus. "
+                   "Code assistance will be disabled. "
+                   "Error was: %s"),
+                 error->message);
+      g_clear_error (&error);
+    }
+}
+
+static void
+ide_gca_service_init (IdeGcaService *self)
+{
+}
