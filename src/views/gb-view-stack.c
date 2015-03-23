7453afd06f76fa051ad92dcad92a852ce9b28554
Christian Hergert <christian@hergert.me>
2015-03-23 13:35:35 -0700

    view: hide modified label when last view is released

--- src/views/gb-view-stack.c
+++ src/views/gb-view-stack.c
@@ -161,6 +161,7 @@ gb_view_stack_real_empty (GbViewStack *self)
     {
       gtk_widget_set_sensitive (GTK_WIDGET (self->close_button), FALSE);
       gtk_widget_set_sensitive (GTK_WIDGET (self->document_button), FALSE);
+      gtk_widget_set_visible (GTK_WIDGET (self->modified_label), FALSE);
     }
 }
 
