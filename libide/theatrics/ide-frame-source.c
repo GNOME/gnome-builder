7d137ac9278091c2a71262e5f647ceec545d3852
Christian Hergert <christian@hergert.me>
2015-03-29 18:35:03 -0700

    libide is a GPL-3.0 shared library
    Typically I use LGPL-2.1+ for shared libraries. However, due to the
    nature of libide being the "guts" of Builder, I do intend for this to
    be GPL-3.0. Additionally, it simplifies what we can include in the
    library without worrying about staying LGPL safe.
    
    Exceptions will of course be made for upstreaming into libraries such as
    GtkSourceView. Just ask if you want something in particular.

--- libide/theatrics/ide-frame-source.c
+++ libide/theatrics/ide-frame-source.c
@@ -1,23 +1,19 @@
-/*
- * Based upon code from Clutter:
+/* ide-frame-source.c
  *
- * Authored By Neil Roberts <neil@linux.intel.com>
+ * Copyright (C) 2015 Christian Hergert <christian@hergert.me>
  *
- * Copyright (C) 2009 Intel Corporation.
- * Copyright (C) 2012 Christian Hergert.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "ide-frame-source.h"
