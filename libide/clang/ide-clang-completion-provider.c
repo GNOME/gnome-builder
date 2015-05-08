94f84297746c00a8e96d4deb4e008bcfb2d13125
Christian Hergert <christian@hergert.me>
2015-05-08 11:22:44 -0700

    Revert "fix some static analysis warnings"
    This reverts commit a8bc71e7690283f5b415f096e01aaf39a52c57ca.

--- libide/clang/ide-clang-completion-provider.c
+++ libide/clang/ide-clang-completion-provider.c
@@ -220,8 +220,7 @@ ide_clang_completion_provider_complete_cb (GObject      *object,
       goto failure;
     }
 
-  if (self->last_results != NULL)
-    g_ptr_array_free (self->last_results, TRUE);
+  g_ptr_array_free (self->last_results, TRUE);
   self->last_results = g_ptr_array_ref (ar);
 
   gtk_source_completion_context_get_iter (state->context, &iter);
