3d1e1f586c3009586eb87411805564973a30f07b
Christian Hergert <christian@hergert.me>
2015-05-15 16:29:52 -0700

    ctags: add a simple ctags parser and index
    This is meant to be a fairly minimal ctags index. Of course it could use
    support for the more extended key/value pair stuff, but this gets the
    basics going.
    
    Of note:
    
     - We read in the entire contents of the ctags file into one contiguous
       buffer.
     - We walk that buffer, line by line, and replace both \n and \t with
       a NUL byte (\0) so that we can use the substrings as C strings. This
       allows us to only have a single allocation in memory.
     - We then use a GArray to contain the sorted index. That includes
       pointers into the string heap as well as a bit of information about
       them such as the token kind.
     - The pointer math in the lookup function looks scary (ptr[-1]) but I
       assure you it is not. We check bounds beforehand to ensure we don't
       jump outside the bsearch()'d region.
    
    That's about it!

---
+++ libide/ctags/ide-ctags-index.h
@@ -0,0 +1,75 @@
+/* ide-ctags-index.h
+ *
+ * Copyright (C) 2015 Christian Hergert <christian@hergert.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IDE_CTAGS_INDEX_H
+#define IDE_CTAGS_INDEX_H
+
+#include <gio/gio.h>
+
+#include "ide-object.h"
+
+G_BEGIN_DECLS
+
+#define IDE_TYPE_CTAGS_INDEX (ide_ctags_index_get_type())
+
+G_DECLARE_FINAL_TYPE (IdeCtagsIndex, ide_ctags_index, IDE, CTAGS_INDEX, IdeObject)
+
+typedef enum
+{
+  IDE_CTAGS_INDEX_ENTRY_ANCHOR = 'a',
+  IDE_CTAGS_INDEX_ENTRY_CLASS_NAME = 'c',
+  IDE_CTAGS_INDEX_ENTRY_DEFINE = 'd',
+  IDE_CTAGS_INDEX_ENTRY_ENUMERATOR = 'e',
+  IDE_CTAGS_INDEX_ENTRY_FUNCTION = 'f',
+  IDE_CTAGS_INDEX_ENTRY_FILE_NAME = 'F',
+  IDE_CTAGS_INDEX_ENTRY_ENUMERATION_NAME = 'g',
+  IDE_CTAGS_INDEX_ENTRY_MEMBER = 'm',
+  IDE_CTAGS_INDEX_ENTRY_PROTOTYPE = 'p',
+  IDE_CTAGS_INDEX_ENTRY_STRUCTURE = 's',
+  IDE_CTAGS_INDEX_ENTRY_TYPEDEF = 't',
+  IDE_CTAGS_INDEX_ENTRY_UNION = 'u',
+  IDE_CTAGS_INDEX_ENTRY_VARIABLE = 'v',
+} IdeCtagsIndexEntryKind;
+
+typedef struct
+{
+  const gchar            *name;
+  const gchar            *path;
+  const gchar            *pattern;
+  IdeCtagsIndexEntryKind  kind : 8;
+  guint8                  padding[3];
+} IdeCtagsIndexEntry;
+
+IdeCtagsIndex            *ide_ctags_index_new         (GFile                *file);
+void                      ide_ctags_index_load_async  (IdeCtagsIndex        *self,
+                                                       GFile                *file,
+                                                       GCancellable         *cancellable,
+                                                       GAsyncReadyCallback   callback,
+                                                       gpointer              user_data);
+gboolean                  ide_ctags_index_load_finish (IdeCtagsIndex        *index,
+                                                       GAsyncResult         *result,
+                                                       GError              **error);
+GFile                    *ide_ctags_index_get_file    (IdeCtagsIndex         *self);
+gsize                     ide_ctags_index_get_size    (IdeCtagsIndex         *self);
+const IdeCtagsIndexEntry *ide_ctags_index_lookup      (IdeCtagsIndex         *self,
+                                                       const gchar           *keyword,
+                                                       gsize                 *length);
+
+G_END_DECLS
+
+#endif /* IDE_CTAGS_INDEX_H */
