31a64d94369d21bfe9dd890e0e8412361c49563d
Ignacio Casal Quinteiro <icq@gnome.org>
2015-07-17 12:04:50 +0200

    new-project-dialog: use the new fetch options to add the remote callbacks

--- src/dialogs/gb-new-project-dialog.c
+++ src/dialogs/gb-new-project-dialog.c
@@ -188,6 +188,7 @@ gb_new_project_dialog__clone_worker (GTask        *task,
   g_autoptr(GFile) workdir = NULL;
   CloneRequest *req = task_data;
   GgitCloneOptions *clone_options;
+  GgitFetchOptions *fetch_options;
   GgitRemoteCallbacks *callbacks;
   IdeProgress *progress;
   GError *error = NULL;
@@ -197,16 +198,20 @@ gb_new_project_dialog__clone_worker (GTask        *task,
   g_assert (req != NULL);
   g_assert (!cancellable || G_IS_CANCELLABLE (cancellable));
 
-  clone_options = ggit_clone_options_new ();
-  ggit_clone_options_set_is_bare (clone_options, FALSE);
-  ggit_clone_options_set_checkout_branch (clone_options, "master");
-
   callbacks = g_object_new (IDE_TYPE_GIT_REMOTE_CALLBACKS, NULL);
   progress = ide_git_remote_callbacks_get_progress (IDE_GIT_REMOTE_CALLBACKS (callbacks));
   g_object_bind_property (progress, "fraction",
                           self->clone_progress, "fraction",
                           G_BINDING_SYNC_CREATE);
-  ggit_clone_options_set_remote_callbacks (clone_options, callbacks);
+
+  fetch_options = ggit_fetch_options_new ();
+  ggit_fetch_options_set_remote_callbacks (fetch_options, callbacks);
+
+  clone_options = ggit_clone_options_new ();
+  ggit_clone_options_set_is_bare (clone_options, FALSE);
+  ggit_clone_options_set_checkout_branch (clone_options, "master");
+  ggit_clone_options_set_fetch_options (clone_options, fetch_options);
+  ggit_fetch_options_free (fetch_options);
 
   repository = ggit_repository_clone (req->uri, req->location, clone_options, &error);
 
