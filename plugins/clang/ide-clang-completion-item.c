f0d8f48fc0d9e4aa16d745df2699e4b4a025f972
Christian Hergert <christian@hergert.me>
2015-09-27 18:18:51 -0700

    clang: cleanup

--- plugins/clang/ide-clang-completion-item.c
+++ plugins/clang/ide-clang-completion-item.c
@@ -362,19 +362,19 @@ ide_clang_completion_item_class_init (IdeClangCompletionItemClass *klass)
 
   gParamSpecs [PROP_INDEX] =
     g_param_spec_uint ("index",
-                         "Index",
-                         "The index in the result set.",
-                         0,
-                         G_MAXUINT-1,
-                         0,
-                         (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
+                       "Index",
+                       "The index in the result set.",
+                       0,
+                       G_MAXUINT-1,
+                       0,
+                       (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
 
   gParamSpecs [PROP_RESULTS] =
     g_param_spec_boxed ("results",
-                         "Results",
-                         "The Clang result set.",
-                         IDE_TYPE_REF_PTR,
-                         (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
+                        "Results",
+                        "The Clang result set.",
+                        IDE_TYPE_REF_PTR,
+                        (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
 
   g_object_class_install_properties (object_class, LAST_PROP, gParamSpecs);
 }
