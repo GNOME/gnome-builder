ea12b2dfd8a1e88ecdb5695784af37185ef8979a
Christian Hergert <christian@hergert.me>
2015-11-11 12:15:50 -0800

    libide: work towards keeping public API in libide/ directory
    Still a few things to hide or migrate, but lets not make it worse.

--- libide/ide.h
+++ libide/ide.h
@@ -64,6 +64,8 @@ G_BEGIN_DECLS
 #include "ide-object.h"
 #include "ide-pattern-spec.h"
 #include "ide-perspective.h"
+#include "ide-preferences.h"
+#include "ide-preferences-addin.h"
 #include "ide-process.h"
 #include "ide-progress.h"
 #include "ide-project.h"
@@ -110,8 +112,6 @@ G_BEGIN_DECLS
 #include "git/ide-git-remote-callbacks.h"
 #include "git/ide-git-vcs.h"
 #include "local/ide-local-device.h"
-#include "preferences/ide-preferences.h"
-#include "preferences/ide-preferences-addin.h"
 #include "util/ide-line-reader.h"
 #include "util/ide-list-inline.h"
 
