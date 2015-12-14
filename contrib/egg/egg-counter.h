5f06ff916bd7babfd2c2b43cc0ed4fe58f9cc9ce
Christian Hergert <christian@hergert.me>
2015-12-14 14:48:28 -0800

    counters: use LGPL-2.1 for EggCounter
    In case people want to start using this externally as a library, feel free
    to do so. Macro expansion explicitly copyright of the consumer.

--- contrib/egg/egg-counter.h
+++ contrib/egg/egg-counter.h
@@ -2,18 +2,21 @@
  *
  * Copyright (C) 2013-2015 Christian Hergert <christian@hergert.me>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additionally, this file does not claim copyright over the expansion
+ * of macros in your source program.
  */
 
 #ifndef EGG_COUNTER_H
