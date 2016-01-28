a708e4896b9c8f56ddb1dbe4de5916bba284df8d
Christian Hergert <chergert@redhat.com>
2016-01-28 14:50:48 +0100

    prefs: add section for diagnostics

--- libide/preferences/ide-preferences-builtin.c
+++ libide/preferences/ide-preferences-builtin.c
@@ -163,6 +163,8 @@ ide_preferences_builtin_register_code_insight (IdePreferences *preferences)
   ide_preferences_add_switch (preferences, "code-insight", "completion", "org.gnome.builder.code-insight", "word-completion", NULL, NULL, _("Suggest words found in open files"), _("Suggests completions as you type based on words found in any open document"), NULL, 0);
   ide_preferences_add_switch (preferences, "code-insight", "completion", "org.gnome.builder.code-insight", "ctags-autocompletion", NULL, NULL, _("Suggest completions using Ctags"), _("Create and manages a Ctags database for completing class names, functions, and more"), NULL, 10);
   ide_preferences_add_switch (preferences, "code-insight", "completion", "org.gnome.builder.code-insight", "clang-autocompletion", NULL, NULL, _("Suggest completions using Clang (Experimental)"), _("Use Clang to suggest completions for C and C++ languages"), NULL, 20);
+
+  ide_preferences_add_list_group (preferences, "code-insight", "diagnostics", _("Diagnostics"), 200);
 }
 
 static void
