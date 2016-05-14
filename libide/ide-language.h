4d1d836b73e20531cca80a60353f4b1ede429d5e
Christian Hergert <chergert@redhat.com>
2016-05-14 15:36:17 +0300

    language: add language helper to generate license headers
    This can be used to add the comments to a text blurb for a license header.
    Generally you'll want to pair this with a template include.

---
+++ libide/ide-language.h
@@ -0,0 +1,31 @@
+/* ide-language.h
+ *
+ * Copyright (C) 2016 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IDE_LANGUAGE_H
+#define IDE_LANGUAGE_H
+
+#include <gtksourceview/gtksourcelanguage.h>
+
+G_BEGIN_DECLS
+
+gchar *ide_language_format_header (GtkSourceLanguage *language,
+                                   const gchar       *header);
+
+G_END_DECLS
+
+#endif /* IDE_LANGUAGE_H */
