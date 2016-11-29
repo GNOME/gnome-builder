210ce78808ce3e796e0a413934b6853c85a96629
Christian Hergert <chergert@redhat.com>
2016-11-28 18:01:44 -0800

    git: add missing G_LOG_DOMAIN

--- plugins/git/ide-git-remote-callbacks.c
+++ plugins/git/ide-git-remote-callbacks.c
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define G_LOG_DOMAIN "ide-git-remote-callbacks"
+
 #include <egg-animation.h>
 #include <glib/gi18n.h>
 #include <ide.h>
