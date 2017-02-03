8436de56a14fb32b7de88175d3035948264f85e2
Christian Hergert <chergert@redhat.com>
2017-02-03 12:42:08 -0800

    configuration: whitespace cleanup

--- libide/buildsystem/ide-configuration.h
+++ libide/buildsystem/ide-configuration.h
@@ -80,7 +80,7 @@ IdeBuildCommandQueue *ide_configuration_get_prebuild         (IdeConfiguration  
 IdeBuildCommandQueue *ide_configuration_get_postbuild        (IdeConfiguration  *self);
 const gchar          *ide_configuration_get_app_id           (IdeConfiguration  *self);
 void                  ide_configuration_set_app_id           (IdeConfiguration  *self,
-                                                              const gchar        *app_id);
+                                                              const gchar       *app_id);
 const gchar          *ide_configuration_get_internal_string  (IdeConfiguration  *self,
                                                               const gchar       *key);
 void                  ide_configuration_set_internal_string  (IdeConfiguration  *self,
