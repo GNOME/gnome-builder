813c2ff2090b707afe3a1f438d0d1140415f2e89
Sebastien Lafargue <slafargue@gnome.org>
2017-02-19 22:24:17 +0100

    buildsystem: we certainly don't want to see this 'unused' warning at each build but tracing need this variable so let's use G_GNUC_UNUSED

--- libide/buildsystem/ide-build-stage-launcher.c
+++ libide/buildsystem/ide-build-stage-launcher.c
@@ -113,7 +113,7 @@ ide_build_stage_launcher_run (IdeBuildStage         *stage,
                               gpointer               user_data)
 {
   IdeBuildStageLauncher *self = (IdeBuildStageLauncher *)stage;
-  IdeBuildStageLauncherPrivate *priv = ide_build_stage_launcher_get_instance_private (self);
+  IdeBuildStageLauncherPrivate *priv = ide_build_stage_launcher_get_instance_private (self); G_GNUC_UNUSED
   g_autoptr(GTask) task = NULL;
   g_autoptr(GError) error = NULL;
   g_autoptr(IdeSubprocess) subprocess = NULL;
