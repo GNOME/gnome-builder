4a52b0acfde5c57f2972bcc341a47119138355f3
Christian Hergert <chergert@redhat.com>
2017-02-20 15:04:23 -0800

    build: remove libidemm
    Nothing is using libidemm and we won't be needing it for the things we
    have upcoming. Babysitting gtkmm/glibmm and it's lack of support for
    proper out-of-tree builds is getting annoying, so until something decides
    to implement stuff in C++, I'm just going to drop this.
    
    Having all of this in a single commit is convenience in case we want to
    revert it at some point in the future.
    
    Additionally, it's reliance on automake is holding back our ability to
    move to Meson/Ninja in the future.

--- autogen.sh
+++ autogen.sh
@@ -19,14 +19,6 @@ touch INSTALL
 
 aclocal --install -I build/autotools || exit 1
 gtkdocize || exit 1
-
-if command -v mm-common-prepare 2>/dev/null; then
-    mm-common-prepare --copy --force $srcdir/libidemm
-else
-    touch $srcdir/libidemm/generate-binding.am
-    touch $srcdir/libidemm/compile-binding.am
-fi
-
 autoreconf --force --install -Wno-portability || exit 1
 
 cd $olddir
--- libidemm/tools/m4/convert.m4
+++
@@ -1,7 +0,0 @@
-include(convert_base.m4)
-include(convert_glib.m4)
-include(convert_libide.m4)
-include(convert_glibmm.m4)
-include(convert_gtkmm.m4)
-include(convert_gio.m4)
-
