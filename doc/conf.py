e8a985c2afc8953e0eefbacfa4b2f5e15af6c086
Christian Hergert <chergert@redhat.com>
2017-02-22 00:24:38 -0800

    doc: add gnome overrides

--- doc/conf.py
+++ doc/conf.py
@@ -120,7 +120,13 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
+
+html_context = {
+    'css_files': [
+        '_static/theme_overrides.css', # GNOME specific overrides
+    ],
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
