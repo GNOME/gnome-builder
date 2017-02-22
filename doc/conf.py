2b9ca57f0aebe40045be3164063e1c68233b9fcf
Christian Hergert <chergert@redhat.com>
2017-02-22 00:19:28 -0800

    doc: add favicon

--- doc/conf.py
+++ doc/conf.py
@@ -149,7 +149,7 @@ html_theme = 'alabaster'
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-# html_favicon = None
+html_favicon = 'favicon'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
