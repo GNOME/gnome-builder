GNOME/gnome-builder

commit ca5a035ea3535a7872344b49b1f415cabaa96fab

flatpak: drop bind mount for /run/build/$project
This is where flatpak-builder builds things, but it is terribly
inconvenient for us to do so. It breaks mtimes, it causes make distclean
failures, and is generally just a PITA.

We lose predictable build source locations for debug symbols, but we can
come up with a different strategy for "release quality builds" that get
shipped to users (ie: all flatpak-builder).

plugins/flatpak/gbp-flatpak-runtime.c
@@ -212,8 +212,7 @@ gbp_flatpak_runtime_create_launcher (IdeRuntime  *runtime,
           g_autofree gchar *bind_mount_option = NULL;
           g_autofree gchar *build_dir_option = NULL;
           filesystem_option = g_strdup_printf ("--filesystem=%s", project_path);
-          bind_mount_option = g_strdup_printf ("--bind-mount=/run/build/%s=%s", project_name, builddir);
-          build_dir_option = g_strdup_printf ("--build-dir=/run/build/%s", project_name);
+          build_dir_option = g_strdup_printf ("--build-dir=%s", builddir);
           ide_subprocess_launcher_push_argv (ret, "--nofilesystem=host");
           ide_subprocess_launcher_push_argv (ret, filesystem_option);
           ide_subprocess_launcher_push_argv (ret, bind_mount_option);

commit 4acc48940279ba32e38e0f8a1064682b41b530ee

flatpak: allow access to build directory
The flatpak build process was broken by commit ca5a035ea because flatpak
doesn't have access to the build directory on the host. This commit
fixes the issue.

plugins/flatpak/gbp-flatpak-runtime.c
@@ -209,12 +209,15 @@ gbp_flatpak_runtime_create_launcher (IdeRuntime  *runtime,
       ide_subprocess_launcher_push_argv (ret, "--share=network");
       if (!ide_str_empty0 (project_path))
         {
-          g_autofree gchar *filesystem_option = NULL;
+          g_autofree gchar *filesystem_option_src = NULL;
+          g_autofree gchar *filesystem_option_build = NULL;
           g_autofree gchar *build_dir_option = NULL;
-          filesystem_option = g_strdup_printf ("--filesystem=%s", project_path);
+          filesystem_option_src = g_strdup_printf ("--filesystem=%s", project_path);
+          filesystem_option_build = g_strdup_printf ("--filesystem=%s", builddir);
           build_dir_option = g_strdup_printf ("--build-dir=%s", builddir);
           ide_subprocess_launcher_push_argv (ret, "--nofilesystem=host");
-          ide_subprocess_launcher_push_argv (ret, filesystem_option);
+          ide_subprocess_launcher_push_argv (ret, filesystem_option_src);
+          ide_subprocess_launcher_push_argv (ret, filesystem_option_build);
           ide_subprocess_launcher_push_argv (ret, build_dir_option);
         }
       if (env_vars != NULL)