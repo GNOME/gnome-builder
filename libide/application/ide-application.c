83711946f3bfd04be998ea7abf77c10559fe7d82
Christian Hergert <chergert@redhat.com>
2017-02-28 17:44:30 -0800

    Revert "theme: set cursor theme when we are in flatpak"
    This reverts commit 5119aa3f6041700e9c3b9df3a2ea850c91c48102.
    
    We don't need this anymore now that we have cursor name.

--- libide/application/ide-application.c
+++ libide/application/ide-application.c
@@ -119,7 +119,6 @@ ide_application_register_theme_overrides (IdeApplication *self)
   if (ide_is_flatpak () && g_getenv ("GTK_THEME") == NULL)
     {
       g_object_set (gtk_settings,
-                    "gtk-cursor-theme-name", "Adwaita",
                     "gtk-theme-name", "Adwaita",
                     NULL);
     }
