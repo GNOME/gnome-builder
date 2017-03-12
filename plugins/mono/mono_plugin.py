60959dc931847675112915f4b6f464b20c48e21f
Christian Hergert <chergert@redhat.com>
2017-03-11 17:54:17 -0800

    mono: add simple mono plugin
    This just adds an error regex to extract build warnings when
    projects are compiled with gmcs/mcs.

---
+++ plugins/mono/mono_plugin.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import gi
+
+from gi.repository import Ide
+from gi.repository import GLib
+
+_ERROR_REGEX = ("(?<filename>[a-zA-Z0-9\\-\\.\\/]+.cs)" +
+                "\\((?<line>\\d+),(?<column>\\d+)\\): " +
+                "(?<level>[\\w\\s]+) " +
+                "(?<code>CS[0-9]+): " +
+                "(?<message>.*)")
+
+class MonoPipelineAddin(Ide.Object, Ide.BuildPipelineAddin):
+
+    def do_load(self, pipeline):
+        self.error_format = pipeline.add_error_format(_ERROR_REGEX, GLib.RegexCompileFlags.OPTIMIZE)
+
+    def do_unload(self, pipeline):
+        pipeline.remove_error_format(self.error_format)
