a641b33b204da8c9ce8ee9f41c27737547939962
Christian Hergert <chergert@redhat.com>
2017-03-17 14:50:31 -0700

    greeter: default to projects directory when opening
    If we are opening a project, we should place the user in the projects
    directory, which is the most likely place for their project to exist.

--- libide/greeter/ide-greeter-perspective.c
+++ libide/greeter/ide-greeter-perspective.c
@@ -655,6 +655,8 @@ static void
 ide_greeter_perspective_open_clicked (IdeGreeterPerspective *self,
                                       GtkButton             *open_button)
 {
+  g_autoptr(GSettings) settings = NULL;
+  g_autofree gchar *projects_dir = NULL;
   GtkFileChooserDialog *dialog;
   GtkWidget *toplevel;
   PeasEngine *engine;
@@ -763,6 +765,10 @@ ide_greeter_perspective_open_clicked (IdeGreeterPerspective *self,
                            G_CONNECT_SWAPPED);
 
   gtk_file_chooser_set_filter (GTK_FILE_CHOOSER (dialog), all_filter);
+
+  settings = g_settings_new ("org.gnome.builder");
+  projects_dir = g_settings_get_string (settings, "projects-directory");
+  gtk_file_chooser_set_current_folder (GTK_FILE_CHOOSER (dialog), projects_dir);
 
   gtk_window_present (GTK_WINDOW (dialog));
 }
