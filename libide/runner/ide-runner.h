9e13c0a4f495015becbca326b525c9951930486b
Christian Hergert <chergert@redhat.com>
2017-03-20 14:28:22 -0700

    runner: whitespace cleanup

--- libide/runner/ide-runner.h
+++ libide/runner/ide-runner.h
@@ -61,41 +61,40 @@ struct _IdeRunnerClass
   gpointer _reserved8;
 };
 
-IdeRunner      *ide_runner_new             (IdeContext           *context);
-void            ide_runner_force_quit      (IdeRunner            *self);
-IdeEnvironment *ide_runner_get_environment (IdeRunner            *self);
-void            ide_runner_run_async       (IdeRunner            *self,
-                                            GCancellable         *cancellable,
-                                            GAsyncReadyCallback   callback,
-                                            gpointer              user_data);
-gboolean        ide_runner_run_finish      (IdeRunner            *self,
-                                            GAsyncResult         *result,
-                                            GError              **error);
-GSubprocessFlags
-                ide_runner_get_flags       (IdeRunner            *self);
-void            ide_runner_set_flags       (IdeRunner            *self,
-                                            GSubprocessFlags      flags);
-gboolean        ide_runner_get_clear_env   (IdeRunner            *self);
-void            ide_runner_set_clear_env   (IdeRunner            *self,
-                                            gboolean              clear_env);
-void            ide_runner_prepend_argv    (IdeRunner            *self,
-                                            const gchar          *param);
-void            ide_runner_append_argv     (IdeRunner            *self,
-                                            const gchar          *param);
-gchar         **ide_runner_get_argv        (IdeRunner            *self);
-void            ide_runner_set_argv        (IdeRunner            *self,
-                                            const gchar * const  *argv);
-gint            ide_runner_take_fd         (IdeRunner            *self,
-                                            gint                  source_fd,
-                                            gint                  dest_fd);
-GInputStream   *ide_runner_get_stdin       (IdeRunner            *self);
-GOutputStream  *ide_runner_get_stdout      (IdeRunner            *self);
-GOutputStream  *ide_runner_get_stderr      (IdeRunner            *self);
-gboolean        ide_runner_get_run_on_host (IdeRunner            *self);
-void            ide_runner_set_run_on_host (IdeRunner            *self,
-                                            gboolean              run_on_host);
-void            ide_runner_set_tty         (IdeRunner            *self,
-                                            int                   tty_fd);
+IdeRunner         *ide_runner_new             (IdeContext           *context);
+void               ide_runner_force_quit      (IdeRunner            *self);
+IdeEnvironment    *ide_runner_get_environment (IdeRunner            *self);
+void               ide_runner_run_async       (IdeRunner            *self,
+                                               GCancellable         *cancellable,
+                                               GAsyncReadyCallback   callback,
+                                               gpointer              user_data);
+gboolean           ide_runner_run_finish      (IdeRunner            *self,
+                                               GAsyncResult         *result,
+                                               GError              **error);
+GSubprocessFlags   ide_runner_get_flags       (IdeRunner            *self);
+void               ide_runner_set_flags       (IdeRunner            *self,
+                                               GSubprocessFlags      flags);
+gboolean           ide_runner_get_clear_env   (IdeRunner            *self);
+void               ide_runner_set_clear_env   (IdeRunner            *self,
+                                               gboolean              clear_env);
+void               ide_runner_prepend_argv    (IdeRunner            *self,
+                                               const gchar          *param);
+void               ide_runner_append_argv     (IdeRunner            *self,
+                                               const gchar          *param);
+gchar            **ide_runner_get_argv        (IdeRunner            *self);
+void               ide_runner_set_argv        (IdeRunner            *self,
+                                               const gchar * const  *argv);
+gint               ide_runner_take_fd         (IdeRunner            *self,
+                                               gint                  source_fd,
+                                               gint                  dest_fd);
+GInputStream      *ide_runner_get_stdin       (IdeRunner            *self);
+GOutputStream     *ide_runner_get_stdout      (IdeRunner            *self);
+GOutputStream     *ide_runner_get_stderr      (IdeRunner            *self);
+gboolean           ide_runner_get_run_on_host (IdeRunner            *self);
+void               ide_runner_set_run_on_host (IdeRunner            *self,
+                                               gboolean              run_on_host);
+void               ide_runner_set_tty         (IdeRunner            *self,
+                                               int                   tty_fd);
 
 G_END_DECLS
 
