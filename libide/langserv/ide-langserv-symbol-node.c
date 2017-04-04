40387c2d2aa81c4e592c096748fe4983d53309c5
Christian Hergert <chergert@redhat.com>
2017-04-04 14:56:15 -0700

    langserv: use decode helpers for kind property

--- libide/langserv/ide-langserv-symbol-node.c
+++ libide/langserv/ide-langserv-symbol-node.c
@@ -24,6 +24,7 @@
 #include "files/ide-file.h"
 #include "langserv/ide-langserv-symbol-node.h"
 #include "langserv/ide-langserv-symbol-node-private.h"
+#include "langserv/ide-langserv-util.h"
 
 typedef struct
 {
@@ -144,28 +145,7 @@ ide_langserv_symbol_node_new (GFile       *file,
 
   g_return_val_if_fail (G_IS_FILE (file), NULL);
 
-  switch (kind)
-    {
-    case 1:   kind = IDE_SYMBOL_FILE;         break;
-    case 2:   kind = IDE_SYMBOL_MODULE;       break;
-    case 3:   kind = IDE_SYMBOL_NAMESPACE;    break;
-    case 4:   kind = IDE_SYMBOL_PACKAGE;      break;
-    case 5:   kind = IDE_SYMBOL_CLASS;        break;
-    case 6:   kind = IDE_SYMBOL_METHOD;       break;
-    case 7:   kind = IDE_SYMBOL_PROPERTY;     break;
-    case 8:   kind = IDE_SYMBOL_FIELD;        break;
-    case 9:   kind = IDE_SYMBOL_CONSTRUCTOR;  break;
-    case 10:  kind = IDE_SYMBOL_ENUM;         break;
-    case 11:  kind = IDE_SYMBOL_INTERFACE;    break;
-    case 12:  kind = IDE_SYMBOL_FUNCTION;     break;
-    case 13:  kind = IDE_SYMBOL_VARIABLE;     break;
-    case 14:  kind = IDE_SYMBOL_CONSTANT;     break;
-    case 15:  kind = IDE_SYMBOL_STRING;       break;
-    case 16:  kind = IDE_SYMBOL_NUMBER;       break;
-    case 17:  kind = IDE_SYMBOL_BOOLEAN;      break;
-    case 18:  kind = IDE_SYMBOL_ARRAY;        break;
-    default:  kind = IDE_SYMBOL_NONE;         break;
-    }
+  kind = ide_langserv_decode_symbol_kind (kind);
 
   self = g_object_new (IDE_TYPE_LANGSERV_SYMBOL_NODE,
                        "flags", 0,
