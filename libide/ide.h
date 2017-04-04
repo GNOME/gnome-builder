45b91d2eae9acae4b6ab687a6730d3e6149a6c91
Christian Hergert <chergert@redhat.com>
2017-04-04 14:55:57 -0700

    langserv: add decode helpers for langserv symbol protocol

--- libide/ide.h
+++ libide/ide.h
@@ -87,6 +87,7 @@ G_BEGIN_DECLS
 #include "langserv/ide-langserv-diagnostic-provider.h"
 #include "langserv/ide-langserv-rename-provider.h"
 #include "langserv/ide-langserv-symbol-resolver.h"
+#include "langserv/ide-langserv-util.h"
 #include "local/ide-local-device.h"
 #include "logging/ide-log.h"
 #include "preferences/ide-preferences.h"
