379f8820bda2e3f2f4ed9640b6fd741747c0f90d
Christian Hergert <chergert@redhat.com>
2017-04-10 13:01:09 -0700

    Revert "Changed name of the window "Run Output" to "Application Output""
    This reverts commit 44098232028c14d8dabc8a5f187baf6065863df9.
    
    We shouldn't be changing strings.

--- plugins/terminal/gb-terminal-workbench-addin.c
+++ plugins/terminal/gb-terminal-workbench-addin.c
@@ -139,7 +139,7 @@ on_run_manager_run (GbTerminalWorkbenchAddin *self,
       panel = g_object_new (PNL_TYPE_DOCK_WIDGET,
                             "child", self->run_terminal,
                             "expand", TRUE,
-                            "title", _("Application Output"),
+                            "title", _("Run Output"),
                             "visible", TRUE,
                             NULL);
       ide_set_weak_pointer (&self->run_panel, panel);
