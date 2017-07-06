1439f3a4cc65541c11753721e4255bef2589391a
Christian Hergert <chergert@redhat.com>
2017-07-06 02:08:56 -0700

    editor: bring over more bits from previous tweak widget
    This brings over a bit of UI and restructures some things. Still needs
    work and planning, but let's me playaround with stuff in the mean time.

--- libide/editor/ide-editor-perspective.c
+++ libide/editor/ide-editor-perspective.c
@@ -25,6 +25,7 @@
 #include "diagnostics/ide-source-location.h"
 #include "editor/ide-editor-perspective.h"
 #include "editor/ide-editor-private.h"
+#include "editor/ide-editor-properties.h"
 #include "editor/ide-editor-sidebar.h"
 #include "editor/ide-editor-view.h"
 #include "workbench/ide-perspective.h"
@@ -33,15 +34,16 @@
 
 struct _IdeEditorPerspective
 {
-  IdeLayout         parent_instance;
+  IdeLayout            parent_instance;
 
   /* Template widgets */
-  IdeLayoutGrid    *grid;
-  IdeEditorSidebar *sidebar;
+  IdeLayoutGrid       *grid;
+  IdeEditorSidebar    *sidebar;
+  IdeEditorProperties *properties;
 
   /* State before entering focus mode */
-  guint             prefocus_had_left : 1;
-  guint             prefocus_had_bottom : 1;
+  guint                prefocus_had_left : 1;
+  guint                prefocus_had_bottom : 1;
 };
 
 typedef struct
@@ -81,10 +83,13 @@ ide_editor_perspective_class_init (IdeEditorPerspectiveClass *klass)
 
   container_class->add = ide_editor_perspective_add;
 
-  gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/builder/ui/ide-editor-perspective.ui");
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/org/gnome/builder/ui/ide-editor-perspective.ui");
   gtk_widget_class_bind_template_child (widget_class, IdeEditorPerspective, grid);
+  gtk_widget_class_bind_template_child (widget_class, IdeEditorPerspective, properties);
   gtk_widget_class_bind_template_child (widget_class, IdeEditorPerspective, sidebar);
 
+  g_type_ensure (IDE_TYPE_EDITOR_PROPERTIES);
   g_type_ensure (IDE_TYPE_EDITOR_SIDEBAR);
   g_type_ensure (IDE_TYPE_LAYOUT_GRID);
 }
@@ -446,6 +451,7 @@ ide_editor_perspective_restore_panel_state (IdeEditorPerspective *self)
   reveal = g_settings_get_boolean (settings, "right-visible");
   position = g_settings_get_int (settings, "right-position");
   dzl_dock_revealer_set_position (DZL_DOCK_REVEALER (pane), position);
+  set_reveal_child_without_transition (DZL_DOCK_REVEALER (pane), FALSE);
 
   pane = dzl_dock_bin_get_bottom_edge (DZL_DOCK_BIN (self));
   reveal = g_settings_get_boolean (settings, "bottom-visible");
