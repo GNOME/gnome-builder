4d89bf82e0f76ddbbf62711f84e99617bdf36f58
Christian Hergert <chergert@redhat.com>
2017-07-10 18:37:45 -0700

    menus: use DzlMenuButton for run menu button
    This is yet another menu we can consolidate. It also allows for
    plugins to specify their extensions to the menu without strictly
    implementing a run handler (event if it does currently require
    duplicating the icon/name entries).
    
    What might be a possibility going forward is to generate the
    GMenu entries based on the handler registration.

--- libide/runner/ide-run-button.c
+++ libide/runner/ide-run-button.c
@@ -33,88 +33,14 @@ struct _IdeRunButton
 {
   GtkBox                parent_instance;
 
-  GtkSizeGroup         *accel_size_group;
   GtkButton            *button;
   GtkImage             *button_image;
-  GtkListBox           *list_box;
-  GtkMenuButton        *menu_button;
-  GtkPopover           *popover;
+  DzlMenuButton        *menu_button;
   GtkButton            *stop_button;
   GtkShortcutsShortcut *run_shortcut;
 };
 
 G_DEFINE_TYPE (IdeRunButton, ide_run_button, GTK_TYPE_BOX)
-
-static GtkWidget *
-create_row (const IdeRunHandlerInfo *info,
-            IdeRunButton            *self)
-{
-  GtkListBoxRow *row;
-  GtkLabel *label;
-  GtkImage *image;
-  GtkBox *box;
-
-  g_assert (info != NULL);
-  g_assert (IDE_IS_RUN_BUTTON (self));
-
-  row = g_object_new (GTK_TYPE_LIST_BOX_ROW,
-                      "selectable", FALSE,
-                      "visible", TRUE,
-                      NULL);
-
-  g_object_set_data_full (G_OBJECT (row), "IDE_RUN_HANDLER_ID", g_strdup (info->id), g_free);
-
-  box = g_object_new (GTK_TYPE_BOX,
-                      "visible", TRUE,
-                      NULL);
-  gtk_container_add (GTK_CONTAINER (row), GTK_WIDGET (box));
-
-  image = g_object_new (GTK_TYPE_IMAGE,
-                        "hexpand", FALSE,
-                        "icon-name", info->icon_name,
-                        "visible", TRUE,
-                        NULL);
-  gtk_container_add (GTK_CONTAINER (box), GTK_WIDGET (image));
-
-  label = g_object_new (GTK_TYPE_LABEL,
-                        "label", info->title,
-                        "hexpand", TRUE,
-                        "xalign", 0.0f,
-                        "visible", TRUE,
-                        NULL);
-  gtk_container_add (GTK_CONTAINER (box), GTK_WIDGET (label));
-
-  if (info->accel != NULL)
-    {
-      g_autofree gchar *xaccel = NULL;
-      guint accel_key = 0;
-      GdkModifierType accel_mod = 0;
-
-      gtk_accelerator_parse (info->accel, &accel_key, &accel_mod);
-      xaccel = gtk_accelerator_get_label (accel_key, accel_mod);
-      label = g_object_new (GTK_TYPE_LABEL,
-                            "label", xaccel,
-                            "visible", TRUE,
-                            "xalign", 0.0f,
-                            NULL);
-      dzl_gtk_widget_add_style_class (GTK_WIDGET (label), "dim-label");
-      dzl_gtk_widget_add_style_class (GTK_WIDGET (label), "accel");
-      gtk_container_add_with_properties (GTK_CONTAINER (box), GTK_WIDGET (label),
-                                         "pack-type", GTK_PACK_END,
-                                         NULL);
-      gtk_size_group_add_widget (self->accel_size_group, GTK_WIDGET (label));
-    }
-
-  return GTK_WIDGET (row);
-}
-
-static void
-ide_run_button_clear (IdeRunButton *self)
-{
-  g_assert (IDE_IS_RUN_BUTTON (self));
-
-  gtk_container_foreach (GTK_CONTAINER (self->list_box), (GtkCallback)gtk_widget_destroy, NULL);
-}
 
 static void
 ide_run_button_handler_set (IdeRunButton  *self,
@@ -150,24 +76,11 @@ ide_run_button_load (IdeRunButton *self,
                      IdeContext   *context)
 {
   IdeRunManager *run_manager;
-  const GList *list;
-  const GList *iter;
 
   g_assert (IDE_IS_RUN_BUTTON (self));
   g_assert (IDE_IS_CONTEXT (context));
 
   run_manager = ide_context_get_run_manager (context);
-  list = _ide_run_manager_get_handlers (run_manager);
-
-  for (iter = list; iter; iter = iter->next)
-    {
-      const IdeRunHandlerInfo *info = iter->data;
-      GtkWidget *row;
-
-      row = create_row (info, self);
-
-      gtk_container_add (GTK_CONTAINER (self->list_box), row);
-    }
 
   g_object_bind_property (run_manager, "busy", self->button, "visible",
                           G_BINDING_SYNC_CREATE | G_BINDING_INVERT_BOOLEAN);
@@ -184,35 +97,6 @@ ide_run_button_load (IdeRunButton *self,
 }
 
 static void
-ide_run_button_row_activated (IdeRunButton  *self,
-                              GtkListBoxRow *row,
-                              GtkListBox    *list_box)
-{
-  IdeContext *context;
-  const gchar *id;
-
-  g_assert (IDE_IS_RUN_BUTTON (self));
-  g_assert (GTK_IS_LIST_BOX_ROW (row));
-  g_assert (GTK_IS_LIST_BOX (list_box));
-
-  context = ide_widget_get_context (GTK_WIDGET (self));
-  id = g_object_get_data (G_OBJECT (row), "IDE_RUN_HANDLER_ID");
-
-  if (id != NULL && context != NULL)
-    {
-      IdeRunManager *run_manager;
-
-      /* First change the run action to the selected handler. */
-      run_manager = ide_context_get_run_manager (context);
-      ide_run_manager_set_handler (run_manager, id);
-      gtk_popover_popdown (self->popover);
-
-      /* Now run the action */
-      dzl_gtk_widget_action (GTK_WIDGET (self), "run-manager", "run-with-handler", g_variant_new_string (id));
-    }
-}
-
-static void
 ide_run_button_context_set (GtkWidget  *widget,
                             IdeContext *context)
 {
@@ -220,8 +104,6 @@ ide_run_button_context_set (GtkWidget  *widget,
 
   g_assert (IDE_IS_RUN_BUTTON (self));
   g_assert (!context || IDE_IS_CONTEXT (context));
-
-  ide_run_button_clear (self);
 
   if (context != NULL)
     ide_run_button_load (self, context);
@@ -273,12 +155,9 @@ ide_run_button_class_init (IdeRunButtonClass *klass)
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/builder/ui/ide-run-button.ui");
-  gtk_widget_class_bind_template_child (widget_class, IdeRunButton, accel_size_group);
   gtk_widget_class_bind_template_child (widget_class, IdeRunButton, button);
   gtk_widget_class_bind_template_child (widget_class, IdeRunButton, button_image);
-  gtk_widget_class_bind_template_child (widget_class, IdeRunButton, list_box);
   gtk_widget_class_bind_template_child (widget_class, IdeRunButton, menu_button);
-  gtk_widget_class_bind_template_child (widget_class, IdeRunButton, popover);
   gtk_widget_class_bind_template_child (widget_class, IdeRunButton, run_shortcut);
   gtk_widget_class_bind_template_child (widget_class, IdeRunButton, stop_button);
 }
@@ -287,12 +166,6 @@ static void
 ide_run_button_init (IdeRunButton *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
-
-  g_signal_connect_object (self->list_box,
-                           "row-activated",
-                           G_CALLBACK (ide_run_button_row_activated),
-                           self,
-                           G_CONNECT_SWAPPED);
 
   g_signal_connect_object (self->button,
                            "query-tooltip",
