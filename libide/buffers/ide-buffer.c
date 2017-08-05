64c0884884988f64d0ff31b52cad8ae38a3cfd68
Christian Hergert <chergert@redhat.com>
2017-08-05 15:29:51 -0700

    buffer: move tags initialize into separate function
    This is a bunch of mundane stuff that is unrelated to the other init code,
    so lets just move it somewhere else for the sake of readability.

--- libide/buffers/ide-buffer.c
+++ libide/buffers/ide-buffer.c
@@ -1181,10 +1181,8 @@ ide_buffer_addin_removed (PeasExtensionSet *set,
 }
 
 static void
-ide_buffer_constructed (GObject *object)
+ide_buffer_init_tags (IdeBuffer *self)
 {
-  IdeBuffer *self = (IdeBuffer *)object;
-  IdeBufferPrivate *priv = ide_buffer_get_instance_private (self);
   GtkTextTagTable *tag_table;
   GtkSourceStyleScheme *style_scheme;
   g_autoptr(GtkTextTag) deprecated_tag = NULL;
@@ -1197,9 +1195,6 @@ ide_buffer_constructed (GObject *object)
   GdkRGBA warning_rgba;
 
   g_assert (IDE_IS_BUFFER (self));
-  g_assert (IDE_IS_CONTEXT (priv->context));
-
-  G_OBJECT_CLASS (ide_buffer_parent_class)->constructed (object);
 
   tag_table = gtk_text_buffer_get_tag_table (GTK_TEXT_BUFFER (self));
   style_scheme = gtk_source_buffer_get_style_scheme (GTK_SOURCE_BUFFER (self));
@@ -1259,11 +1254,25 @@ ide_buffer_constructed (GObject *object)
                               "underline", PANGO_UNDERLINE_SINGLE,
                               NULL);
 
-  g_signal_connect_object (gtk_text_buffer_get_tag_table (GTK_TEXT_BUFFER (self)),
+  g_signal_connect_object (tag_table,
                            "tag-added",
                            G_CALLBACK (ide_buffer_on_tag_added),
                            self,
                            G_CONNECT_SWAPPED);
+}
+
+static void
+ide_buffer_constructed (GObject *object)
+{
+  IdeBuffer *self = (IdeBuffer *)object;
+  IdeBufferPrivate *priv = ide_buffer_get_instance_private (self);
+
+  g_assert (IDE_IS_BUFFER (self));
+  g_assert (IDE_IS_CONTEXT (priv->context));
+
+  G_OBJECT_CLASS (ide_buffer_parent_class)->constructed (object);
+
+  ide_buffer_init_tags (self);
 
   priv->highlight_engine = ide_highlight_engine_new (self);
   ide_highlight_engine_pause (priv->highlight_engine);
