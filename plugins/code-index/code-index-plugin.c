35f35a6956688bf5e6142a93da235ff7a3d678dc
Anoop Chandu <anoopchandu96@gmail.com>
2017-08-24 00:50:42 +0530

    code-index: Adding plugin files
    https://bugzilla.gnome.org/show_bug.cgi?id=786700

---
+++ plugins/code-index/code-index-plugin.c
@@ -0,0 +1,40 @@
+/* code-index-plugin.c
+ *
+ * Copyright (C) 2017 Anoop Chandu <anoopchandu96@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libpeas/peas.h>
+#include <ide.h>
+
+#include "ide-code-index-service.h"
+#include "ide-code-index-search-provider.h"
+#include "ide-code-index-symbol-resolver.h"
+
+void
+peas_register_types (PeasObjectModule *module)
+{
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_SERVICE,
+                                              IDE_TYPE_CODE_INDEX_SERVICE);
+
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_SEARCH_PROVIDER,
+                                              IDE_TYPE_CODE_INDEX_SEARCH_PROVIDER);
+
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_SYMBOL_RESOLVER,
+                                              IDE_TYPE_CODE_INDEX_SYMBOL_RESOLVER);
+}
