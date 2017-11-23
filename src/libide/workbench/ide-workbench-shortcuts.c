d0b0bbf8196ed28ae5a07aa17e300793b64d6454
Christian Hergert <chergert@redhat.com>
2017-11-23 02:43:30 -0800

    shortcut: capture ctrl+. shortcut early
    This was stolen for the emoji picker so we can't use DISPATCH
    anymore or the editor will activate that. Instead, we can just
    CAPTURE the event before it reaches the textview.

--- src/libide/workbench/ide-workbench-shortcuts.c
+++ src/libide/workbench/ide-workbench-shortcuts.c
@@ -105,7 +105,7 @@ _ide_workbench_init_shortcuts (IdeWorkbench *self)
   dzl_shortcut_controller_add_command_action (controller,
                                               I_("org.gnome.builder.workbench.global-search"),
                                               "<Control>period",
-                                              DZL_SHORTCUT_PHASE_DISPATCH | DZL_SHORTCUT_PHASE_GLOBAL,
+                                              DZL_SHORTCUT_PHASE_CAPTURE | DZL_SHORTCUT_PHASE_GLOBAL,
                                               I_("win.global-search"));
 
   dzl_shortcut_controller_add_command_action (controller,
