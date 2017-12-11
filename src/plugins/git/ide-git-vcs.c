56446357b078550df8af24d0a412641368d4a401
Christian Hergert <chergert@redhat.com>
2017-12-11 15:33:58 -0800

    git: include index in search result
    We want both index and workdir, as files that have been
    "git add" will not show up without it.

--- src/plugins/git/ide-git-vcs.c
+++ src/plugins/git/ide-git-vcs.c
@@ -632,7 +632,7 @@ ide_git_vcs_list_status_worker (GTask        *task,
 
   strv[0] = relative;
   options = ggit_status_options_new (GGIT_STATUS_OPTION_DEFAULT,
-                                     GGIT_STATUS_SHOW_WORKDIR_ONLY,
+                                     GGIT_STATUS_SHOW_INDEX_AND_WORKDIR,
                                      (const gchar **)strv);
 
   store = g_list_store_new (IDE_TYPE_VCS_FILE_INFO);
