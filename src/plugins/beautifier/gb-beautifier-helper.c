a4a645dedb5a20b76747d47c78f08a48b8e3a681
Christian Hergert <chergert@redhat.com>
2017-12-12 21:25:27 -0800

    beautifier: add log domain

--- src/plugins/beautifier/gb-beautifier-helper.c
+++ src/plugins/beautifier/gb-beautifier-helper.c
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define G_LOG_DOMAIN "gb-beautifier-helper"
+
 #include <glib.h>
 #include <gtksourceview/gtksource.h>
 #include <ide.h>
