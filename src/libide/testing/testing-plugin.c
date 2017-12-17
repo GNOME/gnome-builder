34494a876f4c78db24abfd80d367d48663007a02
Christian Hergert <chergert@redhat.com>
2017-12-17 02:38:45 -0800

    copyright: use © over (C)

--- src/libide/testing/testing-plugin.c
+++ src/libide/testing/testing-plugin.c
@@ -1,6 +1,6 @@
 /* testing-plugin.c
  *
- * Copyright (C) 2017 Christian Hergert <chergert@redhat.com>
+ * Copyright © 2017 Christian Hergert <chergert@redhat.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
