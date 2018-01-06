e9c2c9a2357807abf74712c1fa13147dc05e964f
Christian Hergert <chergert@redhat.com>
2018-01-05 20:17:29 -0800

    messages: add a messages panel, hidden by default
    This adds a new utilities panel that will show log messages
    from Builder and it's plugins. Not much currently proxies
    messages to the context yet, but we'll improve that going
    forward so that they show up here.

---
+++ src/plugins/messages/gbp-messages-editor-addin.c
@@ -0,0 +1,86 @@
+/* gbp-messages-editor-addin.c
+ *
+ * Copyright © 2018 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define G_LOG_DOMAIN "gbp-messages-editor-addin"
+
+#include <ide.h>
+
+#include "gbp-messages-editor-addin.h"
+#include "gbp-messages-panel.h"
+
+struct _GbpMessagesEditorAddin
+{
+  GObject           parent_instance;
+  GbpMessagesPanel *panel;
+};
+
+static void
+gbp_messages_editor_addin_load (IdeEditorAddin       *addin,
+                                IdeEditorPerspective *editor)
+{
+  GbpMessagesEditorAddin *self = (GbpMessagesEditorAddin *)addin;
+  GtkWidget *utilities;
+
+  g_assert (GBP_IS_MESSAGES_EDITOR_ADDIN (self));
+  g_assert (IDE_IS_EDITOR_PERSPECTIVE (editor));
+
+  utilities = ide_editor_perspective_get_utilities (editor);
+
+  /* hidden by default */
+  self->panel = g_object_new (GBP_TYPE_MESSAGES_PANEL, NULL);
+  g_signal_connect (self->panel,
+                    "destroy",
+                    G_CALLBACK (gtk_widget_destroyed),
+                    &self->panel);
+  gtk_container_add (GTK_CONTAINER (utilities), GTK_WIDGET (self->panel));
+}
+
+static void
+gbp_messages_editor_addin_unload (IdeEditorAddin       *addin,
+                                  IdeEditorPerspective *editor)
+{
+  GbpMessagesEditorAddin *self = (GbpMessagesEditorAddin *)addin;
+
+  g_assert (GBP_IS_MESSAGES_EDITOR_ADDIN (self));
+  g_assert (IDE_IS_EDITOR_PERSPECTIVE (editor));
+
+  if (self->panel != NULL)
+    gtk_widget_destroy (GTK_WIDGET (self->panel));
+
+  g_assert (self->panel == NULL);
+}
+
+static void
+editor_addin_iface_init (IdeEditorAddinInterface *iface)
+{
+  iface->load = gbp_messages_editor_addin_load;
+  iface->unload = gbp_messages_editor_addin_unload;
+}
+
+G_DEFINE_TYPE_WITH_CODE (GbpMessagesEditorAddin, gbp_messages_editor_addin, G_TYPE_OBJECT,
+                         G_IMPLEMENT_INTERFACE (IDE_TYPE_EDITOR_ADDIN, editor_addin_iface_init))
+
+static void
+gbp_messages_editor_addin_class_init (GbpMessagesEditorAddinClass *klass)
+{
+}
+
+static void
+gbp_messages_editor_addin_init (GbpMessagesEditorAddin *self)
+{
+}
---
+++ src/plugins/messages/gbp-messages-panel.c
@@ -0,0 +1,102 @@
+/* gbp-messages-panel.c
+ *
+ * Copyright © 2018 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define G_LOG_DOMAIN "gbp-messages-panel"
+
+#include <ide.h>
+
+#include "gbp-messages-panel.h"
+
+struct _GbpMessagesPanel
+{
+  DzlDockWidget parent_instance;
+
+  GtkScrollbar *scrollbar;
+  IdeTerminal  *terminal;
+};
+
+G_DEFINE_TYPE (GbpMessagesPanel, gbp_messages_panel, DZL_TYPE_DOCK_WIDGET)
+
+static void
+gbp_messages_panel_log_cb (GbpMessagesPanel *self,
+                           GLogLevelFlags    log_level,
+                           const gchar      *message,
+                           IdeContext       *context)
+{
+  g_assert (GBP_IS_MESSAGES_PANEL (self));
+  g_assert (message != NULL);
+  g_assert (IDE_IS_CONTEXT (context));
+
+  vte_terminal_feed (VTE_TERMINAL (self->terminal), message, -1);
+  vte_terminal_feed (VTE_TERMINAL (self->terminal), "\r\n", 2);
+  gtk_widget_show (GTK_WIDGET (self));
+}
+
+#if 0
+static gboolean
+do_log (gpointer data)
+{
+  ide_context_warning (data, "(some log message here)");
+  return G_SOURCE_CONTINUE;
+}
+#endif
+
+static void
+gbp_messages_panel_set_context (GtkWidget  *widget,
+                                IdeContext *context)
+{
+  GbpMessagesPanel *self = (GbpMessagesPanel *)widget;
+
+  g_assert (GBP_IS_MESSAGES_PANEL (self));
+  g_assert (!context || IDE_IS_CONTEXT (context));
+
+#if 0
+  g_timeout_add (1000, do_log, context);
+#endif
+
+  if (context != NULL)
+    g_signal_connect_object (context,
+                             "log",
+                             G_CALLBACK (gbp_messages_panel_log_cb),
+                             self,
+                             G_CONNECT_SWAPPED);
+}
+
+static void
+gbp_messages_panel_class_init (GbpMessagesPanelClass *klass)
+{
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  gtk_widget_class_set_template_from_resource (widget_class,
+                                               "/org/gnome/builder/plugins/messages-plugin/gbp-messages-panel.ui");
+  gtk_widget_class_bind_template_child (widget_class, GbpMessagesPanel, scrollbar);
+  gtk_widget_class_bind_template_child (widget_class, GbpMessagesPanel, terminal);
+}
+
+static void
+gbp_messages_panel_init (GbpMessagesPanel *self)
+{
+  GtkAdjustment *vadj;
+
+  gtk_widget_init_template (GTK_WIDGET (self));
+
+  ide_widget_set_context_handler (GTK_WIDGET (self), gbp_messages_panel_set_context);
+
+  vadj = gtk_scrollable_get_vadjustment (GTK_SCROLLABLE (self->terminal));
+  gtk_range_set_adjustment (GTK_RANGE (self->scrollbar), vadj);
+}
