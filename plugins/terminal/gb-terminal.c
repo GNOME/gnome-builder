050527277a5029d5a38069fabe116d7d78cd7a20
Christian Hergert <chergert@redhat.com>
2018-01-21 18:44:09 -0800

    terminal: fix block selection

--- plugins/terminal/gb-terminal.c
+++ plugins/terminal/gb-terminal.c
@@ -193,9 +193,9 @@ gb_terminal_button_press_event (GtkWidget      *widget,
                                       gtk_get_current_event_time (),
                                       NULL);
             }
-        }
 
-      return GDK_EVENT_STOP;
+          return GDK_EVENT_STOP;
+        }
     }
 
   return GTK_WIDGET_CLASS (gb_terminal_parent_class)->button_press_event (widget, button);
