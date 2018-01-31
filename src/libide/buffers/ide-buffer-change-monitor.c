e872ba7a67d653f3cd2530e1634a67dfc08889fc
Christian Hergert <chergert@redhat.com>
2018-01-30 16:35:12 -0800

    buffer: code style cleanup

--- src/libide/buffers/ide-buffer-change-monitor.c
+++ src/libide/buffers/ide-buffer-change-monitor.c
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define G_LOG_DOMAIN "ide-buffer-change-monitor"
+
 #include <glib/gi18n.h>
 
 #include "buffers/ide-buffer.h"
--- src/libide/buffers/ide-buffer.h
+++ src/libide/buffers/ide-buffer.h
@@ -55,14 +55,7 @@ struct _IdeBufferClass
                         const GtkTextIter *location);
 
   /*< private >*/
-  gpointer _reserved1;
-  gpointer _reserved2;
-  gpointer _reserved3;
-  gpointer _reserved4;
-  gpointer _reserved5;
-  gpointer _reserved6;
-  gpointer _reserved7;
-  gpointer _reserved8;
+  gpointer _reserved[8];
 };
 
 IDE_AVAILABLE_IN_ALL
