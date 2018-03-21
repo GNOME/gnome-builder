f7935016c0d6a9b89dcb5e24d4256234a3696c03
Christian Hergert <chergert@redhat.com>
2018-03-20 21:38:35 -0700

    buffer-manager: fix potential NULL dereference
    As part of the fix in c954dea92d31b1ad7e2cdaa2c7a4ca9af34ec2d8
    we introduced this issue.

--- src/libide/buffers/ide-buffer-manager.c
+++ src/libide/buffers/ide-buffer-manager.c
@@ -1273,7 +1273,7 @@ ide_buffer_manager_save_file_async (IdeBufferManager     *self,
                                 g_object_ref (task));
 
 unmodified:
-  unregister_auto_save (self, state->buffer);
+  unregister_auto_save (self, buffer);
 
   IDE_EXIT;
 }
