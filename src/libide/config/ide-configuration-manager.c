866385b44d9078432e480d4893a02c5a4322abe9
Christian Hergert <chergert@redhat.com>
2018-04-08 22:49:55 -0700

    config: avoid signal processing from threaded loaders
    If we're loading a configuration on a thread, we risk accessing various
    subsystems from that thread. This ensures we don't connect to monitor
    for changes until the configuration has been added to the manager in the
    main thread.

--- src/libide/config/ide-configuration-manager.c
+++ src/libide/config/ide-configuration-manager.c
@@ -26,6 +26,7 @@
 
 #include "application/ide-application.h"
 #include "config/ide-configuration-manager.h"
+#include "config/ide-configuration-private.h"
 #include "config/ide-configuration.h"
 #include "config/ide-configuration-provider.h"
 #include "buildconfig/ide-buildconfig-configuration.h"
@@ -503,6 +504,8 @@ ide_configuration_manager_config_added (IdeConfigurationManager  *self,
 
   if (self->current == NULL)
     ide_configuration_manager_set_current (self, config);
+
+  _ide_configuration_attach (config);
 
   IDE_EXIT;
 }
