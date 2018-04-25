dae46c5f9cc9a49cdfb8eb0e06592a7480c68a0e
Christian Hergert <chergert@redhat.com>
2018-04-24 23:25:11 -0700

    source-range: add ide_source_range_new_from_variant()

--- src/libide/diagnostics/ide-source-range.h
+++ src/libide/diagnostics/ide-source-range.h
@@ -27,20 +27,22 @@ G_BEGIN_DECLS
 #define IDE_TYPE_SOURCE_RANGE (ide_source_range_get_type())
 
 IDE_AVAILABLE_IN_ALL
-GType              ide_source_range_get_type  (void);
+GType              ide_source_range_get_type        (void);
 IDE_AVAILABLE_IN_ALL
-IdeSourceRange    *ide_source_range_ref        (IdeSourceRange       *self);
+IdeSourceRange    *ide_source_range_ref              (IdeSourceRange       *self);
 IDE_AVAILABLE_IN_ALL
-void               ide_source_range_unref      (IdeSourceRange       *self);
+void               ide_source_range_unref            (IdeSourceRange       *self);
 IDE_AVAILABLE_IN_ALL
-IdeSourceLocation *ide_source_range_get_begin  (IdeSourceRange       *self);
+IdeSourceLocation *ide_source_range_get_begin        (IdeSourceRange       *self);
 IDE_AVAILABLE_IN_ALL
-IdeSourceLocation *ide_source_range_get_end    (IdeSourceRange       *self);
+IdeSourceLocation *ide_source_range_get_end          (IdeSourceRange       *self);
 IDE_AVAILABLE_IN_ALL
-IdeSourceRange    *ide_source_range_new        (IdeSourceLocation    *begin,
-                                                IdeSourceLocation    *end);
+IdeSourceRange    *ide_source_range_new              (IdeSourceLocation    *begin,
+                                                      IdeSourceLocation    *end);
 IDE_AVAILABLE_IN_3_30
-GVariant          *ide_source_range_to_variant (const IdeSourceRange *self);
+IdeSourceRange    *ide_source_range_new_from_variant (GVariant             *variant);
+IDE_AVAILABLE_IN_3_30
+GVariant          *ide_source_range_to_variant       (const IdeSourceRange *self);
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC (IdeSourceRange, ide_source_range_unref)
 
