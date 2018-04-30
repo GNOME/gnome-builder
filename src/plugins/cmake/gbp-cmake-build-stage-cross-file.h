8aaff2b7029a647c6d51fcf6de85a7786de5c1b8
Corentin Noël <corentin.noel@collabora.co.uk>
2018-04-30 16:43:09 +0100

    cmake: provide toolchains from the source directory

---
+++ src/plugins/cmake/gbp-cmake-build-stage-cross-file.h
@@ -0,0 +1,35 @@
+/* gbp-cmake-build-stage-cross-file.h
+ *
+ * Copyright 2018 Corentin Noël <corentin.noel@collabora.com>
+ * Copyright 2018 Collabora Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <ide.h>
+
+G_BEGIN_DECLS
+
+#define GBP_TYPE_CMAKE_BUILD_STAGE_CROSS_FILE (gbp_cmake_build_stage_cross_file_get_type())
+
+G_DECLARE_FINAL_TYPE (GbpCMakeBuildStageCrossFile, gbp_cmake_build_stage_cross_file, GBP, CMAKE_BUILD_STAGE_CROSS_FILE, IdeBuildStage)
+
+GbpCMakeBuildStageCrossFile *gbp_cmake_build_stage_cross_file_new      (IdeContext                  *context,
+                                                                        IdeToolchain                *toolchain);
+gchar                       *gbp_cmake_build_stage_cross_file_get_path (GbpCMakeBuildStageCrossFile *stage,
+                                                                        IdeBuildPipeline            *pipeline);
+
+G_END_DECLS
