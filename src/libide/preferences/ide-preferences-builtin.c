54c3d0d0919c5b1cbc08aeaef5f15dfc399c1256
Christian Hergert <chergert@redhat.com>
2018-05-30 23:52:35 -0700

    clang: switch to extention-type for enabling provider
    We don't need to use our old format for this. Instead, we can just use the
    plugin type (although that does mean we will be enabled by default once
    this lands).

--- src/libide/preferences/ide-preferences-builtin.c
+++ src/libide/preferences/ide-preferences-builtin.c
@@ -202,7 +202,6 @@ ide_preferences_builtin_register_code_insight (DzlPreferences *preferences)
   dzl_preferences_add_switch (preferences, "code-insight", "completion", "org.gnome.builder.code-insight", "word-completion", NULL, NULL, _("Suggest words found in open files"), _("Suggests completions as you type based on words found in any open document"), NULL, 0);
   dzl_preferences_add_spin_button (preferences, "code-insight", "completion", "org.gnome.builder.editor", "minimum-word-size", NULL, _("Minimum word size"), _("Minimum word size for word completion"), NULL, 5);
   dzl_preferences_add_switch (preferences, "code-insight", "completion", "org.gnome.builder.code-insight", "ctags-autocompletion", NULL, NULL, _("Suggest completions using Ctags"), _("Create and manages a Ctags database for completing class names, functions, and more"), NULL, 10);
-  dzl_preferences_add_switch (preferences, "code-insight", "completion", "org.gnome.builder.code-insight", "clang-autocompletion", NULL, NULL, _("Suggest completions using Clang (Experimental)"), _("Use Clang to suggest completions for C and C++ languages"), NULL, 20);
 
   dzl_preferences_add_list_group (preferences, "code-insight", "diagnostics", _("Diagnostics"), GTK_SELECTION_NONE, 200);
 }
