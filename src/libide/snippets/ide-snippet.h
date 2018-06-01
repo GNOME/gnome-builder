d0b8371ef79bdbe4b3efb93f4c9aa9f9205fa86a
Christian Hergert <chergert@redhat.com>
2018-06-01 04:38:42 -0700

    snippets: add completion provider as a plugin
    We still keep all the snippet plumbing internal, but this moves the
    provider into the src/plugins/ directory so we dont have to mix plumbing
    and plugin stuff in the same directory in libide. They all get dynamically
    linked together anyway.

--- src/libide/snippets/ide-snippet.h
+++ src/libide/snippets/ide-snippet.h
@@ -27,45 +27,45 @@ G_BEGIN_DECLS
 
 #define IDE_TYPE_SNIPPET (ide_snippet_get_type())
 
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 G_DECLARE_FINAL_TYPE (IdeSnippet, ide_snippet, IDE, SNIPPET, GObject)
 
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 IdeSnippet              *ide_snippet_new              (const gchar     *trigger,
                                                        const gchar     *language);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 IdeSnippet              *ide_snippet_copy             (IdeSnippet      *self);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 const gchar             *ide_snippet_get_trigger      (IdeSnippet      *self);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 void                     ide_snippet_set_trigger      (IdeSnippet      *self,
                                                        const gchar     *trigger);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 const gchar             *ide_snippet_get_language     (IdeSnippet      *self);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 void                     ide_snippet_set_language     (IdeSnippet      *self,
                                                        const gchar     *language);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 const gchar             *ide_snippet_get_description  (IdeSnippet      *self);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 void                     ide_snippet_set_description  (IdeSnippet      *self,
                                                        const gchar     *description);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 void                     ide_snippet_add_chunk        (IdeSnippet      *self,
                                                        IdeSnippetChunk *chunk);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 guint                    ide_snippet_get_n_chunks     (IdeSnippet      *self);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 gint                     ide_snippet_get_tab_stop     (IdeSnippet      *self);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 IdeSnippetChunk         *ide_snippet_get_nth_chunk    (IdeSnippet      *self,
                                                        guint            n);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 void                     ide_snippet_get_chunk_range  (IdeSnippet      *self,
                                                        IdeSnippetChunk *chunk,
                                                        GtkTextIter     *begin,
                                                        GtkTextIter     *end);
-IDE_AVAILABLE_IN_ALL
+IDE_AVAILABLE_IN_3_30
 IdeSnippetContext       *ide_snippet_get_context      (IdeSnippet      *self);
 
 G_END_DECLS
---
+++ src/plugins/snippets/snippets-plugin.c
@@ -0,0 +1,30 @@
+/* snippets-plugin.c
+ *
+ * Copyright 2018 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ide.h>
+#include <libpeas/peas.h>
+
+#include "ide-snippet-completion-provider.h"
+
+void
+gbp_snippets_register_types (PeasObjectModule *module)
+{
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_COMPLETION_PROVIDER,
+                                              IDE_TYPE_SNIPPET_COMPLETION_PROVIDER);
+}
