45efccf3fdd3d0e1ac20b4b63ce10dabeb4b4a2f
Clipsey <TheisCHR@live.dk>
2018-06-15 22:04:47 +0200

    removed --skip-configuration for now

--- src/plugins/d-langserv/d_langserver_plugin.py
+++ src/plugins/d-langserv/d_langserver_plugin.py
@@ -90,7 +90,6 @@ class DService(Ide.Object, Ide.Service):
             launcher.push_argv(pname)
 
             # require DLang support
-            launcher.push_argv("--skip-configuration")
             launcher.push_argv("--require")
             launcher.push_argv("D")
 
