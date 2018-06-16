425de982f1a6997a81849aae817cc64425015bdb
Patrick Griffis <tingping@tingping.se>
2018-06-16 17:27:09 -0400

    meson-templates: Update Python template to use Gtk.Template

--- src/plugins/meson-templates/resources/src/window.py
+++ src/plugins/meson-templates/resources/src/window.py
@@ -1,15 +1,13 @@
 {{include "license.py"}}
 
 from gi.repository import Gtk
-from .gi_composites import GtkTemplate
 
 
-@GtkTemplate(ui='{{appid_path}}/{{ui_file}}')
+@Gtk.Template(resource_path='{{appid_path}}/{{ui_file}}')
 class {{PreFix}}Window(Gtk.ApplicationWindow):
     __gtype_name__ = '{{PreFix}}Window'
 
-    label = GtkTemplate.Child()
+    label = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.init_template()
