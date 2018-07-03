e652061be7e120fbf46fa44d566ee08c02cca84f
Christian Hergert <chergert@redhat.com>
2018-07-02 21:52:28 -0700

    wip: devhelp: stub out hover provider for devhelp

---
+++ src/plugins/devhelp/gbp-devhelp-hover-provider.h
@@ -0,0 +1,29 @@
+/* gbp-devhelp-hover-provider.h
+ *
+ * Copyright 2018 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <ide.h>
+
+G_BEGIN_DECLS
+
+#define GBP_TYPE_DEVHELP_HOVER_PROVIDER (gbp_devhelp_hover_provider_get_type())
+
+G_DECLARE_FINAL_TYPE (GbpDevhelpHoverProvider, gbp_devhelp_hover_provider, GBP, DEVHELP_HOVER_PROVIDER, GObject)
+
+G_END_DECLS
