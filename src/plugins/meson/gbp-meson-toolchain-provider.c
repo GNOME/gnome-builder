ac806295c02f7911305d2420112becc7ad22c082
Christian Hergert <chergert@redhat.com>
2018-07-27 19:19:16 -0700

    meson: simplify task state ownership for toolchain lookup
    We cleaned this up elsewhere, but the point is that it's really difficult
    to ensure ownership is proper when using backpoints to the task. Instead,
    let the task own the task data and use the task as the closure ownership
    toplevel.

--- src/plugins/meson/gbp-meson-toolchain-provider.c
+++ src/plugins/meson/gbp-meson-toolchain-provider.c
@@ -37,36 +37,31 @@ typedef struct
 {
   GList     *folders;
   GPtrArray *found_files;
-  IdeTask   *task;
 } FileSearching;
 
 static FileSearching *
-meson_toolchain_provider_file_searching_new (void)
+file_searching_new (void)
 {
   FileSearching *file_searching;
 
   file_searching = g_slice_new0 (FileSearching);
-  file_searching->task = NULL;
-  file_searching->folders = NULL;
-  file_searching->found_files = g_ptr_array_new ();
-  IDE_PTR_ARRAY_SET_FREE_FUNC (file_searching->found_files, g_object_unref);
+  file_searching->found_files = g_ptr_array_new_with_free_func (g_object_unref);
 
   return file_searching;
 }
 
 static void
-meson_toolchain_provider_file_searching_free (FileSearching *file_searching)
+file_searching_free (FileSearching *fs)
 {
-  if (file_searching->task)
-    g_object_unref (file_searching->task);
+  g_clear_pointer (&fs->found_files, g_ptr_array_unref);
 
-  if (file_searching->found_files)
-    g_ptr_array_unref (file_searching->found_files);
+  if (fs->folders)
+    {
+      g_list_free_full (fs->folders, g_object_unref);
+      fs->folders = NULL;
+    }
 
-  if (file_searching->folders)
-    g_list_free_full (file_searching->folders, g_object_unref);
-
-  g_slice_free (FileSearching, file_searching);
+  g_slice_free (FileSearching, fs);
 }
 
 static void
@@ -77,8 +72,8 @@ gbp_meson_toolchain_provider_load_worker (IdeTask      *task,
 {
   GbpMesonToolchainProvider *self = source_object;
   g_autoptr(GPtrArray) toolchains = NULL;
-  IdeContext *context;
   GPtrArray *files = task_data;
+  IdeContext *context;
 
   g_assert (IDE_IS_TASK (task));
   g_assert (GBP_IS_MESON_TOOLCHAIN_PROVIDER (self));
@@ -136,32 +131,12 @@ gbp_meson_toolchain_provider_load_worker (IdeTask      *task,
 }
 
 static void
-meson_toolchain_provider_search_finish (FileSearching *file_searching,
-                                        GError        *error)
-{
-  g_autoptr(IdeTask) task = NULL;
-  g_autoptr(GPtrArray) ret = NULL;
-
-  task = g_steal_pointer (&file_searching->task);
-  ret = g_steal_pointer (&file_searching->found_files);
-  meson_toolchain_provider_file_searching_free (file_searching);
-
-  if (error != NULL)
-    {
-      ide_task_return_error (task, error);
-      return;
-    }
-
-  ide_task_set_task_data (task, g_steal_pointer (&ret), g_ptr_array_unref);
-  ide_task_run_in_thread (task, gbp_meson_toolchain_provider_load_worker);
-}
-
-static void
 add_all_files (GFile     *array,
                GPtrArray *dest_array)
 {
   g_assert (G_IS_FILE (array));
   g_assert (dest_array != NULL);
+
   g_ptr_array_add (dest_array, g_object_ref (array));
 }
 
@@ -171,58 +146,78 @@ meson_toolchain_provider_search_iterate (GObject      *object,
                                          gpointer      user_data)
 {
   GFile *file = (GFile *)object;
-  g_autoptr(GError) error = NULL;
+  g_autoptr(IdeTask) task = user_data;
   g_autoptr(GPtrArray) ret = NULL;
-  FileSearching *file_searching = user_data;
+  g_autoptr(GError) error = NULL;
+  FileSearching *fs;
+
+  IDE_ENTRY;
 
   g_assert (G_IS_FILE (file));
   g_assert (G_IS_ASYNC_RESULT (result));
-  g_assert (file_searching != NULL);
-  g_assert (IDE_IS_TASK (file_searching->task));
+  g_assert (IDE_IS_TASK (task));
+
+  fs = ide_task_get_task_data (task);
+
+  g_assert (fs != NULL);
+  g_assert (fs->found_files != NULL);
+  g_assert (fs->folders != NULL);
 
   ret = ide_g_file_find_finish (file, result, &error);
   IDE_PTR_ARRAY_SET_FREE_FUNC (ret, g_object_unref);
 
   if (ret == NULL)
     {
-      meson_toolchain_provider_search_finish (file_searching, g_steal_pointer (&error));
-      return;
+      ide_task_return_error (task, g_steal_pointer (&error));
+      IDE_EXIT;
     }
 
-  g_ptr_array_foreach (ret, (GFunc)add_all_files, file_searching->found_files);
-  file_searching->folders = g_list_remove (file_searching->folders, file);
-  if (file_searching->folders != NULL)
+  g_ptr_array_foreach (ret, (GFunc)add_all_files, fs->found_files);
+  fs->folders = g_list_remove (fs->folders, file);
+
+  if (fs->folders != NULL)
     {
-      ide_g_file_find_async (file_searching->folders->data,
+      GCancellable *cancellable = ide_task_get_cancellable (task);
+
+      ide_g_file_find_async (fs->folders->data,
                              "*",
-                             ide_task_get_cancellable (file_searching->task),
+                             cancellable,
                              meson_toolchain_provider_search_iterate,
-                             file_searching);
+                             g_steal_pointer (&task));
+
+      IDE_EXIT;
     }
-  else
-    meson_toolchain_provider_search_finish (file_searching, NULL);
+
+  ide_task_set_task_data (task, g_steal_pointer (&ret), g_ptr_array_unref);
+  ide_task_run_in_thread (task, gbp_meson_toolchain_provider_load_worker);
+
+  IDE_EXIT;
 }
 
 static void
-meson_toolchain_provider_search_init (GbpMesonToolchainProvider *self,
-                                      GCancellable              *cancellable,
-                                      GAsyncReadyCallback        callback,
-                                      gpointer                   user_data)
+gbp_meson_toolchain_provider_load_async (IdeToolchainProvider     *provider,
+                                         GCancellable             *cancellable,
+                                         GAsyncReadyCallback       callback,
+                                         gpointer                  user_data)
 {
-  GList *folders = NULL;
+  GbpMesonToolchainProvider *self = (GbpMesonToolchainProvider *)provider;
   g_autoptr(GFile) project_folder = NULL;
-  g_autoptr (IdeTask) task = NULL;
+  g_autoptr(IdeTask) task = NULL;
   g_autofree gchar *user_folder_path = NULL;
   const gchar * const *system_data_dirs;
-  IdeContext *context;
   IdeBuildSystem *build_system;
-  FileSearching *file_searching;
+  FileSearching *fs;
+  IdeContext *context;
+  GList *folders = NULL;
 
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
   g_assert (GBP_IS_MESON_TOOLCHAIN_PROVIDER (self));
   g_assert (!cancellable || G_IS_CANCELLABLE (cancellable));
 
   task = ide_task_new (self, cancellable, callback, user_data);
-  ide_task_set_source_tag (task, meson_toolchain_provider_search_init);
+  ide_task_set_source_tag (task, gbp_meson_toolchain_provider_load_async);
   ide_task_set_priority (task, G_PRIORITY_LOW);
 
   context = ide_object_get_context (IDE_OBJECT (self));
@@ -233,16 +228,17 @@ meson_toolchain_provider_search_init (GbpMesonToolchainProvider *self,
       ide_task_return_new_error (task,
                                  G_IO_ERROR,
                                  G_IO_ERROR_NOT_SUPPORTED,
-                                 "Not using meson, ignoring toolchain provider");
-      return;
+                                 "Not using meson, ignoring meson toolchain provider");
+      IDE_EXIT;
     }
 
   system_data_dirs = g_get_system_data_dirs ();
 
   for (guint i = 0; system_data_dirs[i] != NULL; i++)
     {
-      g_autofree gchar *subfolder = g_build_filename (system_data_dirs[i], "meson", "cross", NULL);
+      g_autofree gchar *subfolder = NULL;
 
+      subfolder = g_build_filename (system_data_dirs[i], "meson", "cross", NULL);
       folders = g_list_append (folders, g_file_new_for_path (subfolder));
     }
 
@@ -250,36 +246,22 @@ meson_toolchain_provider_search_init (GbpMesonToolchainProvider *self,
   folders = g_list_append (folders, g_file_new_for_path (user_folder_path));
 
   project_folder = g_file_get_parent (ide_context_get_project_file (context));
-  folders = g_list_append (folders, g_steal_pointer(&project_folder));
+  folders = g_list_append (folders, g_steal_pointer (&project_folder));
 
-  file_searching = meson_toolchain_provider_file_searching_new ();
-  file_searching->task = g_steal_pointer (&task);
-  file_searching->folders = folders;
+  fs = file_searching_new ();
+  fs->folders = g_steal_pointer (&folders);
+  ide_task_set_task_data (task, fs, file_searching_free);
+
+  g_assert (fs != NULL);
+  g_assert (fs->folders != NULL);
 
   /* Unfortunately there is no file extension for this */
-  ide_g_file_find_with_depth_async (g_list_first (folders)->data,
+  ide_g_file_find_with_depth_async (fs->folders->data,
                                     "*",
                                     MESON_TOOLCHAIN_FIND_MAX_DEPTH,
                                     cancellable,
                                     meson_toolchain_provider_search_iterate,
-                                    file_searching);
-}
-
-static void
-gbp_meson_toolchain_provider_load_async (IdeToolchainProvider     *provider,
-                                         GCancellable             *cancellable,
-                                         GAsyncReadyCallback       callback,
-                                         gpointer                  user_data)
-{
-  GbpMesonToolchainProvider *self = (GbpMesonToolchainProvider *)provider;
-
-  IDE_ENTRY;
-
-  g_assert (IDE_IS_MAIN_THREAD ());
-  g_assert (GBP_IS_MESON_TOOLCHAIN_PROVIDER (self));
-  g_assert (!cancellable || G_IS_CANCELLABLE (cancellable));
-
-  meson_toolchain_provider_search_init (self, cancellable, callback, user_data);
+                                    g_steal_pointer (&task));
 
   IDE_EXIT;
 }
