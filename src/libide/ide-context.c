73705a2dd6736e3eb48145b9cd5373a79e199ad6
Christian Hergert <chergert@redhat.com>
2018-08-03 16:47:17 -0700

    context: release run manager on finalize

--- src/libide/ide-context.c
+++ src/libide/ide-context.c
@@ -595,6 +595,7 @@ ide_context_finalize (GObject *object)
   g_clear_object (&self->project);
   g_clear_object (&self->project_file);
   g_clear_object (&self->recent_manager);
+  g_clear_object (&self->run_manager);
   g_clear_object (&self->runtime_manager);
   g_clear_object (&self->session);
   g_clear_object (&self->toolchain_manager);
