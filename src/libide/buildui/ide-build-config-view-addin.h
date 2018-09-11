f25782ebcd960f638badc535b2ba67250a923395
Christian Hergert <chergert@redhat.com>
2018-09-11 13:45:02 -0700

    buildui: start on plugin for builtin build config

---
+++ src/libide/buildui/ide-build-config-view-addin.h
@@ -0,0 +1,31 @@
+/* ide-build-config-view-addin.h
+ *
+ * Copyright 2018 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define IDE_TYPE_BUILD_CONFIG_VIEW_ADDIN (ide_build_config_view_addin_get_type())
+
+G_DECLARE_FINAL_TYPE (IdeBuildConfigViewAddin, ide_build_config_view_addin, IDE, BUILD_CONFIG_VIEW_ADDIN, IdeObject)
+
+G_END_DECLS
