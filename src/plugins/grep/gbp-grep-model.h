d10efc3bfee35251f95a4762da0f481690c0e964
Christian Hergert <chergert@redhat.com>
2018-10-24 15:08:32 -0700

    grep: start on grep search model for find in project
    This knows how to use git-grep vs grep like our todo plugin. It still
    needs a number of UI pieces and finishing of the treemodel to handle the
    low-cardinality storage of checkmark fields. We also need to do the replace
    UI to reuse ProjectEdit workflow.

---
+++ src/plugins/grep/gbp-grep-model.h
@@ -0,0 +1,58 @@
+/* gbp-grep-model.h
+ *
+ * Copyright 2018 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <ide.h>
+
+G_BEGIN_DECLS
+
+#define GBP_TYPE_GREP_MODEL (gbp_grep_model_get_type())
+
+G_DECLARE_FINAL_TYPE (GbpGrepModel, gbp_grep_model, GBP, GREP_MODEL, IdeObject)
+
+GbpGrepModel *gbp_grep_model_new                    (IdeContext           *context);
+GFile        *gbp_grep_model_get_directory          (GbpGrepModel         *self);
+void          gbp_grep_model_set_directory          (GbpGrepModel         *self,
+                                                     GFile                *directory);
+gboolean      gbp_grep_model_get_use_regex          (GbpGrepModel         *self);
+void          gbp_grep_model_set_use_regex          (GbpGrepModel         *self,
+                                                     gboolean              use_regex);
+gboolean      gbp_grep_model_get_recursive          (GbpGrepModel         *self);
+void          gbp_grep_model_set_recursive          (GbpGrepModel         *self,
+                                                     gboolean              recursive);
+gboolean      gbp_grep_model_get_case_sensitive     (GbpGrepModel         *self);
+void          gbp_grep_model_set_case_sensitive     (GbpGrepModel         *self,
+                                                     gboolean              case_sensitive);
+gboolean      gbp_grep_model_get_at_word_boundaries (GbpGrepModel         *self);
+void          gbp_grep_model_set_at_word_boundaries (GbpGrepModel         *self,
+                                                     gboolean              at_word_boundaries);
+const gchar  *gbp_grep_model_get_query              (GbpGrepModel         *self);
+void          gbp_grep_model_set_query              (GbpGrepModel         *self,
+                                                     const gchar          *query);
+void          gbp_grep_model_scan_async             (GbpGrepModel         *self,
+                                                     GCancellable         *cancellable,
+                                                     GAsyncReadyCallback   callback,
+                                                     gpointer              user_data);
+gboolean      gbp_grep_model_scan_finish            (GbpGrepModel         *self,
+                                                     GAsyncResult         *result,
+                                                     GError              **error);
+
+G_END_DECLS
