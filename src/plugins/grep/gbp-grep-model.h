9a9eec802b033e3e07132f2a83fcb89e7c9fcc34
Christian Hergert <chergert@redhat.com>
2018-10-29 12:55:51 -0700

    grep: add helper to get line info

--- src/plugins/grep/gbp-grep-model.h
+++ src/plugins/grep/gbp-grep-model.h
@@ -26,39 +26,57 @@ G_BEGIN_DECLS
 
 #define GBP_TYPE_GREP_MODEL (gbp_grep_model_get_type())
 
+typedef struct
+{
+  gint match_begin;
+  gint match_end;
+} GbpGrepModelMatch;
+
+typedef struct
+{
+  const gchar *start_of_line;
+  const gchar *start_of_message;
+  gchar       *path;
+  GArray      *matches;
+  guint        line;
+} GbpGrepModelLine;
+
 G_DECLARE_FINAL_TYPE (GbpGrepModel, gbp_grep_model, GBP, GREP_MODEL, IdeObject)
 
-GbpGrepModel *gbp_grep_model_new                    (IdeContext           *context);
-GFile        *gbp_grep_model_get_directory          (GbpGrepModel         *self);
-void          gbp_grep_model_set_directory          (GbpGrepModel         *self,
-                                                     GFile                *directory);
-gboolean      gbp_grep_model_get_use_regex          (GbpGrepModel         *self);
-void          gbp_grep_model_set_use_regex          (GbpGrepModel         *self,
-                                                     gboolean              use_regex);
-gboolean      gbp_grep_model_get_recursive          (GbpGrepModel         *self);
-void          gbp_grep_model_set_recursive          (GbpGrepModel         *self,
-                                                     gboolean              recursive);
-gboolean      gbp_grep_model_get_case_sensitive     (GbpGrepModel         *self);
-void          gbp_grep_model_set_case_sensitive     (GbpGrepModel         *self,
-                                                     gboolean              case_sensitive);
-gboolean      gbp_grep_model_get_at_word_boundaries (GbpGrepModel         *self);
-void          gbp_grep_model_set_at_word_boundaries (GbpGrepModel         *self,
-                                                     gboolean              at_word_boundaries);
-const gchar  *gbp_grep_model_get_query              (GbpGrepModel         *self);
-void          gbp_grep_model_set_query              (GbpGrepModel         *self,
-                                                     const gchar          *query);
-GPtrArray    *gbp_grep_model_create_edits           (GbpGrepModel         *self);
-void          gbp_grep_model_select_all             (GbpGrepModel         *self);
-void          gbp_grep_model_select_none            (GbpGrepModel         *self);
-void          gbp_grep_model_toggle_mode            (GbpGrepModel         *self);
-void          gbp_grep_model_toggle_row             (GbpGrepModel         *self,
-                                                     GtkTreeIter          *iter);
-void          gbp_grep_model_scan_async             (GbpGrepModel         *self,
-                                                     GCancellable         *cancellable,
-                                                     GAsyncReadyCallback   callback,
-                                                     gpointer              user_data);
-gboolean      gbp_grep_model_scan_finish            (GbpGrepModel         *self,
-                                                     GAsyncResult         *result,
-                                                     GError              **error);
+GbpGrepModel *gbp_grep_model_new                    (IdeContext              *context);
+GFile        *gbp_grep_model_get_directory          (GbpGrepModel            *self);
+void          gbp_grep_model_set_directory          (GbpGrepModel            *self,
+                                                     GFile                   *directory);
+gboolean      gbp_grep_model_get_use_regex          (GbpGrepModel            *self);
+void          gbp_grep_model_set_use_regex          (GbpGrepModel            *self,
+                                                     gboolean                 use_regex);
+gboolean      gbp_grep_model_get_recursive          (GbpGrepModel            *self);
+void          gbp_grep_model_set_recursive          (GbpGrepModel            *self,
+                                                     gboolean                 recursive);
+gboolean      gbp_grep_model_get_case_sensitive     (GbpGrepModel            *self);
+void          gbp_grep_model_set_case_sensitive     (GbpGrepModel            *self,
+                                                     gboolean                 case_sensitive);
+gboolean      gbp_grep_model_get_at_word_boundaries (GbpGrepModel            *self);
+void          gbp_grep_model_set_at_word_boundaries (GbpGrepModel            *self,
+                                                     gboolean                 at_word_boundaries);
+const gchar  *gbp_grep_model_get_query              (GbpGrepModel            *self);
+void          gbp_grep_model_set_query              (GbpGrepModel            *self,
+                                                     const gchar             *query);
+GPtrArray    *gbp_grep_model_create_edits           (GbpGrepModel            *self);
+void          gbp_grep_model_select_all             (GbpGrepModel            *self);
+void          gbp_grep_model_select_none            (GbpGrepModel            *self);
+void          gbp_grep_model_toggle_mode            (GbpGrepModel            *self);
+void          gbp_grep_model_toggle_row             (GbpGrepModel            *self,
+                                                     GtkTreeIter             *iter);
+void          gbp_grep_model_get_line               (GbpGrepModel            *self,
+                                                     GtkTreeIter             *iter,
+                                                     const GbpGrepModelLine **line);
+void          gbp_grep_model_scan_async             (GbpGrepModel            *self,
+                                                     GCancellable            *cancellable,
+                                                     GAsyncReadyCallback      callback,
+                                                     gpointer                 user_data);
+gboolean      gbp_grep_model_scan_finish            (GbpGrepModel            *self,
+                                                     GAsyncResult            *result,
+                                                     GError                 **error);
 
 G_END_DECLS
