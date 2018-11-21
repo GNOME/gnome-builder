ae9c2770046f7204f7c21c97e7d0fda46999d03c
Loic Blot <loic.blot@unix-experience.fr>
2018-11-21 15:54:22 +0100

    Add golang plugin
    * Handle *.go files
    * Runs `go build` in the project root
    * Needs GOROOT & GOPATH to be set in project env variables
    * Display go version in SDK section
    * Add project-tree build & rebuild enable support with callback functions

---
+++ src/plugins/golang/ide-golang-pipeline-addin.h
@@ -0,0 +1,29 @@
+/* ide-golang-pipeline-addin.h
+ *
+ * Copyright 2018 Loïc BLOT <loic.blot@unix-experience.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <libide-foundry.h>
+
+G_BEGIN_DECLS
+
+#define IDE_TYPE_GOLANG_PIPELINE_ADDIN (ide_golang_pipeline_addin_get_type())
+
+G_DECLARE_FINAL_TYPE (IdeGolangPipelineAddin, ide_golang_pipeline_addin, IDE, GOLANG_PIPELINE_ADDIN, IdeObject)
+
+G_END_DECLS
