6f9a0ab12b58d09692fc4febd6a49907b993fda2
Christian Hergert <chergert@redhat.com>
2019-01-09 16:54:10 -0800

    webkit: add libide-webkit static library
    Adds a libide-webkit static library for other plugins and Builder to use.
    We now require webkit on all platforms.

--- src/libide/webkit/ide-webkit-plugin.c
+++ src/libide/webkit/ide-webkit-plugin.c
@@ -1,4 +1,4 @@
-/* ide-webkit.c
+/* ide-webkit-plugin.c
  *
  * Copyright 2016-2019 Christian Hergert <chergert@redhat.com>
  *
@@ -18,14 +18,19 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#define G_LOG_DOMAIN "ide-webkit-plugin"
+
 #include "config.h"
 
+#include <libpeas/peas.h>
 #include <webkit2/webkit2.h>
+#include <girepository.h>
 
-#include "object-modules.h"
+_IDE_EXTERN void _ide_webkit_register_types (PeasObjectModule *module);
 
 void
-ide_webkit_register_types (PeasObjectModule *module)
+_ide_webkit_register_types (PeasObjectModule *module)
 {
   g_type_ensure (WEBKIT_TYPE_WEB_VIEW);
+  g_irepository_require (NULL, "WebKit2", "4.0", 0, NULL);
 }
