51f181194ed0a491e01355ef612e2f07e0a7d140
Muhammad Rivan Febrian <rivanfebrian123@gmail.com>
2019-01-31 11:10:16 +0000

    Update window.cpp

--- src/plugins/meson-templates/resources/src/window.cpp
+++ src/plugins/meson-templates/resources/src/window.cpp
@@ -2,17 +2,18 @@
 
 #include "{{prefix}}-window.h"
 
-{{Prefix}}Window::{{Prefix}}Window()
-	: Glib::ObjectBase("{{Prefix}}Window")
-	, Gtk::Window()
-	, headerbar(nullptr)
-	, label(nullptr)
-{
-	builder = Gtk::Builder::create_from_resource("{{appid_path}}/{{ui_file}}");
-	builder->get_widget("headerbar", headerbar);
-	builder->get_widget("label", label);
-	add(*label);
-	label->show();
-	set_titlebar(*headerbar);
-	headerbar->show();
+{{Prefix}}Window::{{Prefix}}Window(BaseObjectType *base,
+                           const Glib::RefPtr<Gtk::Builder> &builder)
+                           : Gtk::ApplicationWindow(base) {
+    builder->get_widget("label", label);
+}
+
+//static function we've declared in '...-window.h'
+{{Prefix}}Window *{{Prefix}}Window::create() {
+    auto builder = Gtk::Builder::create_from_resource("{{appid_path}}/{{ui_file}}");
+
+    TrycppWindow *parent_instance;
+    builder->get_widget_derived("{{Prefix}}Window", parent_instance);
+    
+    return parent_instance;
 }
