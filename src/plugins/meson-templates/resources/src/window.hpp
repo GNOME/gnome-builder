2d3cdbbce00b7e50d9b7f7b86faad43691862262
Muhammad Rivan Febrian <rivanfebrian123@gmail.com>
2019-01-31 11:12:41 +0000

    Update window.hpp

--- src/plugins/meson-templates/resources/src/window.hpp
+++ src/plugins/meson-templates/resources/src/window.hpp
@@ -2,18 +2,21 @@
 
 #pragma once
 
-#include <gtkmm/builder.h>
-#include <gtkmm/headerbar.h>
-#include <gtkmm/label.h>
-#include <gtkmm/window.h>
+#include <gtkmm.h>
+#include <glibmm/i18n.h>
 
-class {{Prefix}}Window : public Gtk::Window
-{
+class {{Prefix}}Window: public Gtk::ApplicationWindow {
 public:
-	{{Prefix}}Window();
+  // constructors
+  {{Prefix}}Window(BaseObjectType*, const Glib::RefPtr<Gtk::Builder>&);
+	static {{Prefix}}Window *create();
 
+protected:
+  // you can add some widgets here
+  Gtk::Label *label;
+  
 private:
-	Gtk::HeaderBar *headerbar;
-	Gtk::Label *label;
-	Glib::RefPtr<Gtk::Builder> builder;
+  // put your functions here.
+  // you can connect widgets' signal by put the belowing in {{Prefix}}Window(...) constructor:
+  // my_widget->signal_something().connect(sigc::mem_fun(*this, &{{Prefix}}Window::on_my_widget_something));
 };
