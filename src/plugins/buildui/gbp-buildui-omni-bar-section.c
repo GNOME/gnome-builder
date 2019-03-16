063560591eb750f5060de23aff75a28b393dafa9
Christian Hergert <chergert@redhat.com>
2019-03-15 18:34:09 -0700

    buildui: fix i18n of Missing to avoid <b> in translatable string

--- src/plugins/buildui/gbp-buildui-omni-bar-section.c
+++ src/plugins/buildui/gbp-buildui-omni-bar-section.c
@@ -110,10 +110,14 @@ gbp_buildui_omni_bar_section_notify_pipeline (GbpBuilduiOmniBarSection *self,
   gtk_actionable_set_action_target (GTK_ACTIONABLE (self->configure_button), "s", config_id);
 
   if (runtime_name != NULL)
-    gtk_label_set_label (self->popover_runtime_label, runtime_name);
+    {
+      gtk_label_set_label (self->popover_runtime_label, runtime_name);
+    }
   else
-    /* translators: use <b>translation</b> to wrap the text in bold font */
-    gtk_label_set_markup (self->popover_runtime_label, _("<b>Missing</b>"));
+    {
+      g_autofree gchar *markup = g_strdup_printf ("<b>%s</b>", _("Missing"));
+      gtk_label_set_markup (self->popover_runtime_label, markup);
+    }
 }
 
 static void
