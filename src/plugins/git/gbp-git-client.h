3b90278ed1c66beb7f300184bf5ee4a22369e59e
Christian Hergert <chergert@redhat.com>
2019-04-05 18:29:47 -0700

    git: add GbpGitClient
    The GbpGitClient object prepares and maintains state using the
    IdeSubprocessSupervisor to manage gnome-builder-git subprocesses. It
    also provides access to the IpcGitService peer running in the daemon.

---
+++ src/plugins/git/gbp-git-client.h
@@ -0,0 +1,42 @@
+/* gbp-git-client.h
+ *
+ * Copyright 2019 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <libide-core.h>
+
+#include "daemon/ipc-git-service.h"
+
+G_BEGIN_DECLS
+
+#define GBP_TYPE_GIT_CLIENT (gbp_git_client_get_type())
+
+G_DECLARE_FINAL_TYPE (GbpGitClient, gbp_git_client, GBP, GIT_CLIENT, IdeObject)
+
+GbpGitClient  *gbp_git_client_from_context       (IdeContext           *context);
+void           gbp_git_client_get_service_async  (GbpGitClient         *self,
+                                                  GCancellable         *cancellable,
+                                                  GAsyncReadyCallback   callback,
+                                                  gpointer              user_data);
+IpcGitService *gbp_git_client_get_service_finish (GbpGitClient         *self,
+                                                  GAsyncResult         *result,
+                                                  GError              **error);
+
+G_END_DECLS
