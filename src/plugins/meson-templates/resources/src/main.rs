ca3df787af5fa16c4345cced88c21781f2d32d77
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2019-08-25 02:54:12 +0000

    Meson Templates: Add Rust GNOME Application template
    Currently, gtk-rs doesn't fully support subclassing which explains the various changes on window.ui

---
+++ src/plugins/meson-templates/resources/src/main.rs
@@ -0,0 +1,34 @@
+use gtk::prelude::*;
+use gio::prelude::*;
+use gettextrs::*;
+use std::env;
+
+mod window;
+use crate::window::Window;
+
+fn main() {
+    gtk::init().unwrap_or_else(|_| panic!("Failed to initialize GTK."));
+
+    setlocale(LocaleCategory::LcAll, "");
+    bindtextdomain("{{name}}", config::LOCALEDIR);
+    textdomain("{{name}}");
+
+    let res = gio::Resource::load(config::PKGDATADIR.to_owned() + "/{{name}}.gresource")
+                                .expect("Could not load resources");
+    gio::resources_register(&res);
+
+    let app = gtk::Application::new(Some("{{appid}}"), Default::default()).unwrap();
+    app.connect_activate(move |app| {
+        let window = Window::new();
+
+        window.widget.set_application(Some(app));
+        app.add_window(&window.widget);
+        window.widget.present();
+    });
+
+
+    let args: Vec<String> = env::args().collect();
+    app.run(&args);
+
+}
+
