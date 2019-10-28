e1fa30665111703fcf79399c19d234c941293137
Diego Escalante Urrelo <diegoe@gnome.org>
2019-10-27 20:55:22 -0500

    build: handle a few temporary quirks in flatpak
    flatpak 1.5.0 does not build because of a change in glib:
    https://github.com/flatpak/flatpak/pull/3164
    
    To avoid the above problem we pin flatpak to the commit which resolved
    the issue. Said commit is not yet available in an official release.
    
    flatpak itself was also failing to build because of a missing ifdef:
    https://github.com/flatpak/flatpak/pull/3198

--- build-aux/flatpak/local-build.sh
+++ build-aux/flatpak/local-build.sh
@@ -7,6 +7,12 @@ rm -rf _repo ; mkdir _repo
 STATE_DIR=~/.cache/gnome-builder/flatpak-builder
 BRANCH=master
 
-flatpak-builder --ccache --state-dir=$STATE_DIR --force-clean _build org.gnome.Builder.json --repo=_repo --default-branch=$BRANCH
-flatpak build-bundle _repo org.gnome.Builder.flatpak org.gnome.Builder $BRANCH
+flatpak-builder \
+    --ccache --force-clean \
+    --repo=_repo --state-dir=$STATE_DIR \
+    --default-branch=$BRANCH \
+    _build org.gnome.Builder.json
+
+flatpak build-bundle \
+    _repo org.gnome.Builder.flatpak org.gnome.Builder $BRANCH
 
