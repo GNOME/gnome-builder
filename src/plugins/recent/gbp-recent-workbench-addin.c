5a9856e27ea21c01b46d944c2068a0c77e569473
Christian Hergert <chergert@redhat.com>
2020-05-11 15:33:56 -0700

    recent: track projects within projects directory
    This may be in a directory that is not a child of the home directory. We
    should track those as well.
    
    Fixes #1207

--- src/plugins/recent/gbp-recent-workbench-addin.c
+++ src/plugins/recent/gbp-recent-workbench-addin.c
@@ -41,15 +41,18 @@ directory_is_ignored (GFile *file)
   g_autofree gchar *relative_path = NULL;
   g_autoptr(GFile) downloads_dir = NULL;
   g_autoptr(GFile) home_dir = NULL;
+  g_autoptr(GFile) projects_dir = NULL;
   GFileType file_type;
 
   g_assert (G_IS_FILE (file));
 
+  projects_dir = g_file_new_for_path (ide_get_projects_dir ());
   home_dir = g_file_new_for_path (g_get_home_dir ());
   relative_path = g_file_get_relative_path (home_dir, file);
   file_type = g_file_query_file_type (file, G_FILE_QUERY_INFO_NOFOLLOW_SYMLINKS, NULL);
 
-  if (!g_file_has_prefix (file, home_dir))
+  if (!g_file_has_prefix (file, home_dir) &&
+      !g_file_has_prefix (file, projects_dir))
     return TRUE;
 
   downloads_dir = g_file_new_for_path (g_get_user_special_dir (G_USER_DIRECTORY_DOWNLOAD));
