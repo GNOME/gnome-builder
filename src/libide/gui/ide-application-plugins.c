00eb15b38557560ef8b473f2dfb70d573ecf3d0f
Christian Hergert <chergert@redhat.com>
2020-11-23 16:19:20 -0800

    gui: make WebKit2 loading optional

--- src/libide/gui/ide-application-plugins.c
+++ src/libide/gui/ide-application-plugins.c
@@ -415,7 +415,9 @@ _ide_application_load_plugins (IdeApplication *self)
       !g_irepository_require (NULL, "Dazzle", "1.0", 0, &error) ||
       !g_irepository_require (NULL, "Jsonrpc", "1.0", 0, &error) ||
       !g_irepository_require (NULL, "Template", "1.0", 0, &error) ||
+#ifdef HAVE_WEBKIT
       !g_irepository_require (NULL, "WebKit2", "4.0", 0, &error) ||
+#endif
       !g_irepository_require (NULL, "Ide", PACKAGE_ABI_S, 0, &error))
     g_critical ("Cannot enable Python 3 plugins: %s", error->message);
   else
