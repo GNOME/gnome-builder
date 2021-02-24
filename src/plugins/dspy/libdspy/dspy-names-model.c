d375be337c8d0f497a76d28fffde36dc8cd872d2
Christian Hergert <chergert@redhat.com>
2021-02-23 22:06:55 -0800

    dspy: use g_memdup2()

--- src/plugins/dspy/libdspy/dspy-names-model.c
+++ src/plugins/dspy/libdspy/dspy-names-model.c
@@ -385,7 +385,7 @@ dspy_names_model_init_async (GAsyncInitable      *initable,
   task = g_task_new (self, cancellable, callback, user_data);
   g_task_set_priority (task, io_priority);
   g_task_set_source_tag (task, dspy_names_model_init_async);
-  g_task_set_task_data (task, g_memdup (&n_active, sizeof n_active), g_free);
+  g_task_set_task_data (task, g_memdup2 (&n_active, sizeof n_active), g_free);
 
   if (self->connection == NULL)
     g_task_return_new_error (task,
