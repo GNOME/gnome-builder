01fc356ae50f07f230735a74bac1232b44347a47
vanadiae <vanadiae35@gmail.com>
2021-07-03 17:20:29 +0200

    version: Use new versioning scheme (3.42 -> 41)
    As I was introducing new APIs (and changing substantially new ones), I
    thought it could be useful to change now to the new versioning scheme,
    as there's only one function for the moment that was made available in
    3.42 (i.e. now 41).
    
    So this commit changes from version 3.42 to 41 for this release cycle.

--- src/libide/gui/ide-application.h
+++ src/libide/gui/ide-application.h
@@ -92,7 +92,7 @@ IdeWorkbench  *ide_application_find_workbench_for_file  (IdeApplication         
 IDE_AVAILABLE_IN_3_34
 gpointer       ide_application_find_addin_by_module_name (IdeApplication           *self,
                                                           const gchar              *module_name);
-IDE_AVAILABLE_IN_3_42
+IDE_AVAILABLE_IN_41
 char          *ide_application_create_cancel_action      (IdeApplication           *self,
                                                           GCancellable             *cancellable);
 
