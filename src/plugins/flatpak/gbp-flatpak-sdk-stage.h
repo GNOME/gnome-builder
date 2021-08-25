8e2ec93d624a17178a58167db0e42d616d755007
Christian Hergert <chergert@redhat.com>
2021-08-24 21:42:14 -0700

    flatpak: add stage to install SDK runtimes

---
+++ src/plugins/flatpak/gbp-flatpak-sdk-stage.h
@@ -0,0 +1,33 @@
+/* gbp-flatpak-sdk-stage.h
+ *
+ * Copyright 2021 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <libide-foundry.h>
+
+G_BEGIN_DECLS
+
+#define GBP_TYPE_FLATPAK_SDK_STAGE (gbp_flatpak_sdk_stage_get_type())
+
+G_DECLARE_FINAL_TYPE (GbpFlatpakSdkStage, gbp_flatpak_sdk_stage, GBP, FLATPAK_SDK_STAGE, IdePipelineStage)
+
+GbpFlatpakSdkStage *gbp_flatpak_sdk_stage_new (const char * const *sdks);
+
+G_END_DECLS
