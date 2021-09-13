a5091ea85d118f312c7309b07adecf26da2b45f5
Christian Hergert <chergert@redhat.com>
2021-09-13 10:36:11 -0700

    meson-templates: fix accidental changes to templates
    See https://gitlab.gnome.org/GNOME/gnome-builder/-/commit/54e74c9b085b3677a455bfb13724bf1e59c609d8#note_1268117

--- src/plugins/meson-templates/resources/src/window.h
+++ src/plugins/meson-templates/resources/src/window.h
@@ -8,6 +8,6 @@ G_BEGIN_DECLS
 
 #define {{PREFIX}}_TYPE_WINDOW ({{prefix_}}_window_get_type())
 
-G_DECLARE_TYPE ({{PreFix}}Window, {{prefix_}}_window, {{PREFIX}}, WINDOW, GtkApplicationWindow)
+G_DECLARE_FINAL_TYPE ({{PreFix}}Window, {{prefix_}}_window, {{PREFIX}}, WINDOW, GtkApplicationWindow)
 
 G_END_DECLS
