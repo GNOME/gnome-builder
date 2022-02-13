a0eb111b9bc0cb4052ec763ad0b08559a2779561
Rafael Fontenelle <rffontenelle@gmail.com>
2022-02-13 19:18:03 +0000

    meson-template: fix typo

--- src/plugins/meson-templates/resources/src/main-gtk4.py
+++ src/plugins/meson-templates/resources/src/main-gtk4.py
@@ -53,7 +53,7 @@ class Application(Gtk.Application):
         action.connect("activate", callback)
         self.add_action(action)
         if shortcuts:
-            self.set_accels_for_action(f"app.{name}", shorcuts)
+            self.set_accels_for_action(f"app.{name}", shortcuts)
 
 
 def main(version):
