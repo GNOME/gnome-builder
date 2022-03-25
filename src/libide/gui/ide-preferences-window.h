1605922cc5f3b838c351d5d7d6ca2641614b81fe
Christian Hergert <chergert@redhat.com>
2022-03-25 16:46:19 -0700

    libide/gui: start on new preferences window
    The goal for this long term is to have a preferences window which can
    look more like the Control Center in terms of how it works but support
    both application preferences as well as project properties. They will
    be displayed at separate times, through separate flows however. Some
    settings which can have project overrides will be displayed redundantly
    in the project properties window.

--- src/libide/gui/ide-preferences-window.h
+++ src/libide/gui/ide-preferences-window.h
@@ -1,6 +1,6 @@
 /* ide-preferences-window.h
  *
- * Copyright 2017-2019 Christian Hergert <chergert@redhat.com>
+ * Copyright 2017-2022 Christian Hergert <chergert@redhat.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,15 +20,95 @@
 
 #pragma once
 
-#include <dazzle.h>
-#include <handy.h>
+#include <adwaita.h>
+
 #include <libide-core.h>
 
 G_BEGIN_DECLS
 
+typedef struct _IdePreferenceItemEntry IdePreferenceItemEntry;
+
+typedef void (*IdePreferenceCallback) (const char                   *page_name,
+                                       const IdePreferenceItemEntry *entry,
+                                       AdwPreferencesGroup          *group,
+                                       gpointer                      user_data);
+
+typedef struct
+{
+  const char *parent;
+  const char *section;
+  const char *name;
+  const char *icon_name;
+  int priority;
+  const char *title;
+
+  /*< private >*/
+  gconstpointer reserved1;
+  gconstpointer reserved2;
+  gconstpointer reserved3;
+  gconstpointer reserved4;
+} IdePreferencePageEntry;
+
+typedef struct
+{
+  const char *page;
+  const char *name;
+  int priority;
+  const char *title;
+
+  /*< private >*/
+  gconstpointer reserved1;
+  gconstpointer reserved2;
+  gconstpointer reserved3;
+  gconstpointer reserved4;
+} IdePreferenceGroupEntry;
+
+struct _IdePreferenceItemEntry
+{
+  const char *page;
+  const char *group;
+  const char *name;
+  int priority;
+  IdePreferenceCallback callback;
+
+  /*< private >*/
+  gconstpointer reserved1;
+  gconstpointer reserved2;
+  gconstpointer reserved3;
+  gconstpointer reserved4;
+};
+
 #define IDE_TYPE_PREFERENCES_WINDOW (ide_preferences_window_get_type())
 
-IDE_AVAILABLE_IN_3_32
-G_DECLARE_FINAL_TYPE (IdePreferencesWindow, ide_preferences_window, IDE, PREFERENCES_WINDOW, HdyApplicationWindow)
+IDE_AVAILABLE_IN_ALL
+G_DECLARE_FINAL_TYPE (IdePreferencesWindow, ide_preferences_window, IDE, PREFERENCES_WINDOW, AdwApplicationWindow)
+
+IDE_AVAILABLE_IN_ALL
+GtkWidget *ide_preferences_window_new        (void);
+IDE_AVAILABLE_IN_ALL
+void       ide_preferences_window_add_pages  (IdePreferencesWindow         *self,
+                                              const IdePreferencePageEntry *pages,
+                                              gsize                         n_pages,
+                                              const char                   *translation_domain);
+IDE_AVAILABLE_IN_ALL
+void       ide_preferences_window_add_groups (IdePreferencesWindow          *self,
+                                              const IdePreferenceGroupEntry *groups,
+                                              gsize                          n_groups,
+                                              const char                   *translation_domain);
+IDE_AVAILABLE_IN_ALL
+void       ide_preferences_window_add_items  (IdePreferencesWindow          *self,
+                                              const IdePreferenceItemEntry  *items,
+                                              gsize                          n_items,
+                                              gpointer                       user_data,
+                                              GDestroyNotify                 user_data_destroy);
+IDE_AVAILABLE_IN_ALL
+void       ide_preferences_window_add_item   (IdePreferencesWindow          *self,
+                                              const char                    *page,
+                                              const char                    *group,
+                                              const char                    *name,
+                                              int                            priority,
+                                              IdePreferenceCallback          callback,
+                                              gpointer                       user_data,
+                                              GDestroyNotify                 user_data_destroy);
 
 G_END_DECLS
