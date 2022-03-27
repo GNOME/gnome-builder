507745a950e1afaed83a14f67def3141f13a98c7
Christian Hergert <chergert@redhat.com>
2022-03-27 00:28:50 -0700

    libide/sourceview: fix typo

--- src/libide/sourceview/ide-source-view.h
+++ src/libide/sourceview/ide-source-view.h
@@ -33,7 +33,7 @@ G_BEGIN_DECLS
 
 #define IDE_TYPE_SOURCE_VIEW (ide_source_view_get_type())
 
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 G_DECLARE_DERIVABLE_TYPE (IdeSourceView, ide_source_view, IDE, SOURCE_VIEW, GtkSourceView)
 
 typedef enum
@@ -71,91 +71,91 @@ struct _IdeSourceViewClass
   void (*remove_cursors)        (IdeSourceView           *self);
 };
 
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 guint                       ide_source_view_get_visual_column              (IdeSourceView              *self,
                                                                             const GtkTextIter          *location);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_get_visual_position            (IdeSourceView              *self,
                                                                             guint                      *line,
                                                                             guint                      *line_column);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 IdeFileSettings            *ide_source_view_get_file_settings              (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 const PangoFontDescription *ide_source_view_get_font_desc                  (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 PangoFontDescription       *ide_source_view_get_scaled_font_desc           (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_highlight_current_line     (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_insert_matching_brace      (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_get_iter_at_visual_column      (IdeSourceView              *self,
                                                                             guint                       column,
                                                                             GtkTextIter                *location);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_overwrite_braces           (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_overwrite                  (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 guint                       ide_source_view_get_scroll_offset              (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_show_grid_lines            (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_show_line_changes          (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_show_line_diagnostics      (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_show_line_numbers          (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_get_show_relative_line_numbers (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_get_visible_rect               (IdeSourceView              *self,
                                                                             GdkRectangle               *visible_rect);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_font_desc                  (IdeSourceView              *self,
                                                                             const PangoFontDescription *font_desc);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_font_name                  (IdeSourceView              *self,
                                                                             const gchar                *font_name);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_highlight_current_line     (IdeSourceView              *self,
                                                                             gboolean                    highlight_current_line);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_insert_matching_brace      (IdeSourceView              *self,
                                                                             gboolean                    insert_matching_brace);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_overwrite_braces           (IdeSourceView              *self,
                                                                             gboolean                    overwrite_braces);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_scroll_offset              (IdeSourceView              *self,
                                                                             guint                       scroll_offset);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_show_grid_lines            (IdeSourceView              *self,
                                                                             gboolean                    show_grid_lines);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_show_line_changes          (IdeSourceView              *self,
                                                                             gboolean                    show_line_changes);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_show_line_diagnostics      (IdeSourceView              *self,
                                                                             gboolean                    show_line_diagnostics);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_show_line_numbers          (IdeSourceView              *self,
                                                                             gboolean                    show_line_numbers);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_show_relative_line_numbers (IdeSourceView              *self,
                                                                             gboolean                    show_relative_line_numbers);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_move_mark_onscreen             (IdeSourceView              *self,
                                                                             GtkTextMark                *mark);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 gboolean                    ide_source_view_place_cursor_onscreen          (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_scroll_mark_onscreen           (IdeSourceView              *self,
                                                                             GtkTextMark                *mark,
                                                                             IdeSourceScrollAlign        use_align,
                                                                             gdouble                     alignx,
                                                                             gdouble                     aligny);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_scroll_to_mark                 (IdeSourceView              *self,
                                                                             GtkTextMark                *mark,
                                                                             gdouble                     within_margin,
@@ -163,7 +163,7 @@ void                        ide_source_view_scroll_to_mark                 (IdeS
                                                                             gdouble                     xalign,
                                                                             gdouble                     yalign,
                                                                             gboolean                    animate_scroll);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_scroll_to_iter                 (IdeSourceView              *self,
                                                                             const GtkTextIter          *iter,
                                                                             gdouble                     within_margin,
@@ -171,9 +171,9 @@ void                        ide_source_view_scroll_to_iter                 (IdeS
                                                                             gdouble                     xalign,
                                                                             gdouble                     yalign,
                                                                             gboolean                    animate_scroll);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_scroll_to_insert               (IdeSourceView              *self);
-IDE_AVAILABLE_ALL
+IDE_AVAILABLE_IN_ALL
 void                        ide_source_view_set_gutter                     (IdeSourceView              *self,
                                                                             IdeGutter                  *gutter);
 
