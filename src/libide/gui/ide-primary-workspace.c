30a9ebbf221f344e6d28d4c72e489874396eb77e
Christian Hergert <chergert@redhat.com>
2022-03-29 00:36:12 -0700

    libide/gui: remove unused bits from primary workspace

--- src/libide/gui/ide-primary-workspace.c
+++ src/libide/gui/ide-primary-workspace.c
@@ -28,7 +28,6 @@
 #include "ide-omni-bar.h"
 #include "ide-primary-workspace.h"
 #include "ide-run-button.h"
-#include "ide-surface.h"
 #include "ide-window-settings-private.h"
 
 /**
@@ -44,8 +43,6 @@
  * when opening a project.
  *
  * Returns: (transfer full): an #IdePrimaryWorkspace
- *
- * Since: 3.32
  */
 
 struct _IdePrimaryWorkspace
@@ -54,10 +51,8 @@ struct _IdePrimaryWorkspace
 
   /* Template widgets */
   IdeHeaderBar       *header_bar;
-  DzlMenuButton      *surface_menu_button;
   IdeRunButton       *run_button;
   GtkLabel           *project_title;
-  DzlShortcutTooltip *search_tooltip;
 };
 
 G_DEFINE_FINAL_TYPE (IdePrimaryWorkspace, ide_primary_workspace, IDE_TYPE_WORKSPACE)
@@ -80,30 +75,9 @@ ide_primary_workspace_context_set (IdeWorkspace *workspace,
   project_info = ide_workbench_get_project_info (workbench);
 
   if (project_info)
-    g_object_bind_property (project_info, "name", self->project_title, "label",
+    g_object_bind_property (project_info, "name",
+                            self->project_title, "label",
                             G_BINDING_SYNC_CREATE);
-}
-
-static void
-ide_primary_workspace_surface_set (IdeWorkspace *workspace,
-                                   IdeSurface   *surface)
-{
-  IdePrimaryWorkspace *self = (IdePrimaryWorkspace *)workspace;
-
-  g_assert (IDE_IS_PRIMARY_WORKSPACE (self));
-  g_assert (!surface || IDE_IS_SURFACE (surface));
-
-  if (DZL_IS_DOCK_ITEM (surface))
-    {
-      g_autofree gchar *icon_name = NULL;
-
-      icon_name = dzl_dock_item_get_icon_name (DZL_DOCK_ITEM (surface));
-      g_object_set (self->surface_menu_button,
-                    "icon-name", icon_name,
-                    NULL);
-    }
-
-  IDE_WORKSPACE_CLASS (ide_primary_workspace_parent_class)->surface_set (workspace, surface);
 }
 
 static void
@@ -114,15 +88,12 @@ ide_primary_workspace_class_init (IdePrimaryWorkspaceClass *klass)
 
   ide_workspace_class_set_kind (workspace_class, "primary");
 
-  workspace_class->surface_set = ide_primary_workspace_surface_set;
   workspace_class->context_set = ide_primary_workspace_context_set;
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/libide-gui/ui/ide-primary-workspace.ui");
   gtk_widget_class_bind_template_child (widget_class, IdePrimaryWorkspace, header_bar);
   gtk_widget_class_bind_template_child (widget_class, IdePrimaryWorkspace, project_title);
   gtk_widget_class_bind_template_child (widget_class, IdePrimaryWorkspace, run_button);
-  gtk_widget_class_bind_template_child (widget_class, IdePrimaryWorkspace, search_tooltip);
-  gtk_widget_class_bind_template_child (widget_class, IdePrimaryWorkspace, surface_menu_button);
 
   g_type_ensure (IDE_TYPE_RUN_BUTTON);
 }
