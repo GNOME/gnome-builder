c2e101acd0c6a3037c52e763d5e4187950633c82
Christian Hergert <chergert@redhat.com>
2022-03-28 17:40:37 -0700

    libide/gui: use IdePreferencesWindow for preferences addin

--- src/libide/gui/ide-preferences-addin.c
+++ src/libide/gui/ide-preferences-addin.c
@@ -38,17 +38,11 @@ ide_preferences_addin_default_init (IdePreferencesAddinInterface *iface)
  *
  * This interface method is called when a preferences addin is initialized. It
  * could be initialized from multiple preferences implementations, so consumers
- * should use the #DzlPreferences interface to add their preferences controls
- * to the container.
- *
- * Such implementations might include a preferences dialog window, or a
- * preferences widget which could be rendered as a perspective.
- *
- * Since: 3.32
+ * should use the #IdePreferencesWindow to add their preferences controls.
  */
 void
-ide_preferences_addin_load (IdePreferencesAddin *self,
-                            DzlPreferences      *preferences)
+ide_preferences_addin_load (IdePreferencesAddin  *self,
+                            IdePreferencesWindow *preferences)
 {
   g_return_if_fail (IDE_IS_PREFERENCES_ADDIN (self));
   g_return_if_fail (DZL_IS_PREFERENCES (preferences));
@@ -65,12 +59,10 @@ ide_preferences_addin_load (IdePreferencesAddin *self,
  * This interface method is called when the preferences addin should remove all
  * controls added to @preferences. This could happen during desctruction of
  * @preferences, or when the plugin is unloaded.
- *
- * Since: 3.32
  */
 void
-ide_preferences_addin_unload (IdePreferencesAddin *self,
-                              DzlPreferences      *preferences)
+ide_preferences_addin_unload (IdePreferencesAddin  *self,
+                              IdePreferencesWindow *preferences)
 {
   g_return_if_fail (IDE_IS_PREFERENCES_ADDIN (self));
   g_return_if_fail (DZL_IS_PREFERENCES (preferences));
