97a3b003ed70c1eeb8aa6b49c2da25b420a5b4a0
Christian Hergert <chergert@redhat.com>
2022-03-31 03:59:46 -0700

    libide/gui: add recoloring helpers
    From text editor, to do the recoloring generation from schemes.

---
+++ src/libide/gui/ide-recoloring-private.h
@@ -0,0 +1,32 @@
+/* ide-recoloring-private.h
+ *
+ * Copyright 2020 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <gtksourceview/gtksource.h>
+
+G_BEGIN_DECLS
+
+char                 *_ide_recoloring_generate_css         (GtkSourceStyleScheme *style_scheme);
+gboolean              _ide_source_style_scheme_is_dark     (GtkSourceStyleScheme *style_scheme);
+GtkSourceStyleScheme *_ide_source_style_scheme_get_variant (GtkSourceStyleScheme *style_scheme,
+                                                            const char           *variant);
+
+G_END_DECLS
