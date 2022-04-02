7d8a98b4e789b1c112c8c1e4c0ea9a36eeea4894
Christian Hergert <chergert@redhat.com>
2022-04-01 21:07:59 -0700

    plugins/editorui: start on new editorui plugin
    This is a new plugin to replace the editor plugin that will bridge to
    the new libide-editor. I'm trying to follow the convention of <name>ui
    for plugins that wrap our internal libide libraries and bridge them
    into the plugin layers.

---
+++ src/plugins/editorui/editorui-plugin.c
@@ -0,0 +1,33 @@
+/* editorui-plugin.c
+ *
+ * Copyright 2018-2022 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "editorui-plugin"
+
+#include "config.h"
+
+#include "gbp-editorui-resources.h"
+
+#include <libpeas/peas.h>
+
+_IDE_EXTERN void
+_gbp_editorui_register_types (PeasObjectModule *module)
+{
+  g_resources_register (gbp_editorui_get_resource ());
+}
