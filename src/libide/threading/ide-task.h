a7fedf24f8ee6214b581c2e658d44a0bacc72d43
Christian Hergert <chergert@redhat.com>
2022-04-04 14:52:54 -0700

    libide/threading: make IdeTask a final type
    We originally thought we might subclass IdeTask to do specific work, but
    that didn't pan out and there are better patterns for that rather than
    subclassing the task itself.
    
    So might as well make the type final and remove our priv indirection as
    it makes it easier to debug from gdb too.

--- src/libide/threading/ide-task.h
+++ src/libide/threading/ide-task.h
@@ -29,8 +29,8 @@ G_BEGIN_DECLS
 
 #define IDE_TYPE_TASK (ide_task_get_type())
 
-IDE_AVAILABLE_IN_3_32
-G_DECLARE_DERIVABLE_TYPE (IdeTask, ide_task, IDE, TASK, GObject)
+IDE_AVAILABLE_IN_ALL
+G_DECLARE_FINAL_TYPE (IdeTask, ide_task, IDE, TASK, GObject)
 
 typedef void (*IdeTaskThreadFunc) (IdeTask      *task,
                                    gpointer      source_object,
@@ -46,121 +46,114 @@ typedef enum
   IDE_TASK_KIND_LAST
 } IdeTaskKind;
 
-struct _IdeTaskClass
-{
-  GObjectClass parent;
-
-  gpointer _reserved[16];
-};
-
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 IdeTask      *ide_task_new                       (gpointer              source_object,
                                                   GCancellable         *cancellable,
                                                   GAsyncReadyCallback   callback,
                                                   gpointer              user_data);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_chain                     (IdeTask              *self,
                                                   IdeTask              *other_task);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 GCancellable *ide_task_get_cancellable           (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gboolean      ide_task_get_completed             (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 IdeTaskKind   ide_task_get_kind                  (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 const gchar  *ide_task_get_name                  (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gint          ide_task_get_priority              (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gint          ide_task_get_complete_priority     (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gpointer      ide_task_get_source_object         (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gpointer      ide_task_get_source_tag            (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gpointer      ide_task_get_task_data             (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gboolean      ide_task_had_error                 (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gboolean      ide_task_is_valid                  (gpointer              self,
                                                   gpointer              source_object);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gboolean      ide_task_propagate_boolean         (IdeTask              *self,
                                                   GError              **error);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gpointer      ide_task_propagate_boxed           (IdeTask              *self,
                                                   GError              **error);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gssize        ide_task_propagate_int             (IdeTask              *self,
                                                   GError              **error);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gpointer      ide_task_propagate_object          (IdeTask              *self,
                                                   GError              **error);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gpointer      ide_task_propagate_pointer         (IdeTask              *self,
                                                   GError              **error);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_boolean            (IdeTask              *self,
                                                   gboolean              result);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_boxed              (IdeTask              *self,
                                                   GType                 result_type,
                                                   gpointer              result);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_error              (IdeTask              *self,
                                                   GError               *error);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gboolean      ide_task_return_error_if_cancelled (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_int                (IdeTask              *self,
                                                   gssize                result);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 gboolean      ide_task_get_return_on_cancel      (IdeTask              *self);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_new_error          (IdeTask              *self,
                                                   GQuark                error_domain,
                                                   gint                  error_code,
                                                   const gchar          *format,
                                                   ...) G_GNUC_PRINTF (4, 5);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_object             (IdeTask              *self,
                                                   gpointer              instance);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_return_pointer            (IdeTask              *self,
                                                   gpointer              data,
                                                   GDestroyNotify        destroy);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_run_in_thread             (IdeTask              *self,
                                                   IdeTaskThreadFunc     thread_func);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_check_cancellable     (IdeTask              *self,
                                                   gboolean              check_cancellable);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_kind                  (IdeTask              *self,
                                                   IdeTaskKind           kind);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_name                  (IdeTask              *self,
                                                   const gchar          *name);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_priority              (IdeTask              *self,
                                                   gint                  priority);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_complete_priority     (IdeTask              *self,
                                                   gint                  complete_priority);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_release_on_propagate  (IdeTask              *self,
                                                   gboolean              release_on_propagate);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_return_on_cancel      (IdeTask              *self,
                                                   gboolean              return_on_cancel);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_source_tag            (IdeTask              *self,
                                                   gpointer              source_tag);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_set_task_data             (IdeTask              *self,
                                                   gpointer              task_data,
                                                   GDestroyNotify        task_data_destroy);
-IDE_AVAILABLE_IN_3_32
+IDE_AVAILABLE_IN_ALL
 void          ide_task_report_new_error          (gpointer              source_object,
                                                   GAsyncReadyCallback   callback,
                                                   gpointer              callback_data,
