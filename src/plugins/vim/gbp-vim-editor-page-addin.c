9efc8a19bf0e47538e3accb940e4d0d677962a42
Christian Hergert <chergert@redhat.com>
2022-04-05 10:42:40 -0700

    plugins/vim: toggle vim input-method based on setting
    We can use the GbpVimWorkspaceAddin:active property to determine if the
    GtkSourceVimIMContext should be enabled with the key controller. Some fixes
    are needed in GtkSourceView for this which were found by using this method
    of enabling/disabling the "client widget".

--- src/plugins/vim/gbp-vim-editor-page-addin.c
+++ src/plugins/vim/gbp-vim-editor-page-addin.c
@@ -28,130 +28,34 @@
 #include "gbp-vim-editor-page-addin.h"
 #include "gbp-vim-workspace-addin.h"
 
+static void gbp_vim_editor_page_addin_load                       (IdeEditorPageAddin    *addin,
+                                                                  IdeEditorPage         *page);
+static void gbp_vim_editor_page_addin_unload                     (IdeEditorPageAddin    *addin,
+                                                                  IdeEditorPage         *page);
+static void gbp_vim_editor_page_addin_notify_active_cb           (GbpVimEditorPageAddin *self,
+                                                                  GParamSpec            *pspec,
+                                                                  GbpVimWorkspaceAddin  *workspace_addin);
+static void gbp_vim_editor_page_addin_notify_command_bar_text_cb (GbpVimEditorPageAddin *self,
+                                                                  GParamSpec            *pspec,
+                                                                  GtkSourceVimIMContext *im_context);
+static void gbp_vim_editor_page_addin_notify_command_text_cb     (GbpVimEditorPageAddin *self,
+                                                                  GParamSpec            *pspec,
+                                                                  GtkSourceVimIMContext *im_context);
+static void gbp_vim_editor_page_addin_update                     (GbpVimEditorPageAddin *self);
+
 struct _GbpVimEditorPageAddin
 {
   GObject               parent_instance;
+
+  /* Unowned pointers */
   IdeEditorPage        *page;
   GbpVimWorkspaceAddin *workspace_addin;
+
+  /* Owned references */
+  GtkEventController   *key_controller;
+
+  guint                 enabled : 1;
 };
-
-static void
-gbp_vim_editor_page_addin_update (GbpVimEditorPageAddin *self)
-{
-  IdeWorkspaceAddin *workspace_addin;
-  IdeWorkspace *workspace;
-
-  IDE_ENTRY;
-
-  g_assert (IDE_IS_EDITOR_PAGE_ADDIN (self));
-  g_assert (IDE_IS_EDITOR_PAGE (self->page));
-
-  self->workspace_addin = NULL;
-
-  workspace = ide_widget_get_workspace (GTK_WIDGET (self->page));
-  workspace_addin = ide_workspace_addin_find_by_module_name (workspace, "vim");
-
-  if (workspace_addin == NULL || !GBP_IS_VIM_WORKSPACE_ADDIN (workspace_addin))
-    IDE_GOTO (failure);
-
-  self->workspace_addin = GBP_VIM_WORKSPACE_ADDIN (workspace_addin);
-
-failure:
-  IDE_EXIT;
-}
-
-static void
-notify_command_bar_text_cb (GbpVimEditorPageAddin *self,
-                            GParamSpec            *pspec,
-                            GtkSourceVimIMContext *im_context)
-{
-  const char *command_bar;
-
-  g_assert (GBP_IS_VIM_EDITOR_PAGE_ADDIN (self));
-  g_assert (GTK_SOURCE_IS_VIM_IM_CONTEXT (im_context));
-
-  command_bar = gtk_source_vim_im_context_get_command_bar_text (im_context);
-
-  if (self->workspace_addin)
-    gbp_vim_workspace_addin_set_command_bar (self->workspace_addin, command_bar);
-}
-
-static void
-notify_command_text_cb (GbpVimEditorPageAddin *self,
-                        GParamSpec            *pspec,
-                        GtkSourceVimIMContext *im_context)
-{
-  const char *command;
-
-  g_assert (GBP_IS_VIM_EDITOR_PAGE_ADDIN (self));
-  g_assert (GTK_SOURCE_IS_VIM_IM_CONTEXT (im_context));
-
-  command = gtk_source_vim_im_context_get_command_text (im_context);
-
-  if (self->workspace_addin)
-    gbp_vim_workspace_addin_set_command (self->workspace_addin, command);
-}
-
-static void
-gbp_vim_editor_page_addin_load (IdeEditorPageAddin *addin,
-                                IdeEditorPage      *page)
-{
-  GbpVimEditorPageAddin *self = (GbpVimEditorPageAddin *)addin;
-  GtkEventController *key;
-  IdeSourceView *view;
-  GtkIMContext *im_context;
-
-  IDE_ENTRY;
-
-  g_assert (IDE_IS_EDITOR_PAGE_ADDIN (self));
-  g_assert (IDE_IS_EDITOR_PAGE (page));
-
-  self->page = page;
-
-  gbp_vim_editor_page_addin_update (self);
-
-  view = ide_editor_page_get_view (page);
-  key = gtk_event_controller_key_new ();
-  im_context = gtk_source_vim_im_context_new ();
-  gtk_im_context_set_client_widget (im_context, GTK_WIDGET (view));
-  gtk_event_controller_set_propagation_phase (key, GTK_PHASE_CAPTURE);
-  gtk_event_controller_key_set_im_context (GTK_EVENT_CONTROLLER_KEY (key), im_context);
-  gtk_widget_add_controller (GTK_WIDGET (view), key);
-
-  /* TODO: Make this toggleable via GSettings, and bind a property from
-   * the workspace addin to enable this.
-   */
-
-  g_signal_connect_object (im_context,
-                           "notify::command-bar-text",
-                           G_CALLBACK (notify_command_bar_text_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
-
-  g_signal_connect_object (im_context,
-                           "notify::command-text",
-                           G_CALLBACK (notify_command_text_cb),
-                           self,
-                           G_CONNECT_SWAPPED);
-
-  g_object_unref (im_context);
-
-  IDE_EXIT;
-}
-
-static void
-gbp_vim_editor_page_addin_unload (IdeEditorPageAddin *addin,
-                                  IdeEditorPage      *page)
-{
-  GbpVimEditorPageAddin *self = (GbpVimEditorPageAddin *)addin;
-
-  IDE_ENTRY;
-
-  g_assert (IDE_IS_EDITOR_PAGE_ADDIN (self));
-  g_assert (IDE_IS_EDITOR_PAGE (page));
-
-  IDE_EXIT;
-}
 
 static void
 editor_page_addin_iface_init (IdeEditorPageAddinInterface *iface)
@@ -171,4 +75,174 @@ gbp_vim_editor_page_addin_class_init (GbpVimEditorPageAddinClass *klass)
 static void
 gbp_vim_editor_page_addin_init (GbpVimEditorPageAddin *self)
 {
+}
+
+static void
+gbp_vim_editor_page_addin_update (GbpVimEditorPageAddin *self)
+{
+  IdeWorkspaceAddin *workspace_addin;
+  IdeSourceView *view;
+  IdeWorkspace *workspace;
+  GtkIMContext *im_context;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_EDITOR_PAGE_ADDIN (self));
+  g_assert (IDE_IS_EDITOR_PAGE (self->page));
+
+  view = ide_editor_page_get_view (self->page);
+  im_context = gtk_event_controller_key_get_im_context (GTK_EVENT_CONTROLLER_KEY (self->key_controller));
+
+  if (self->workspace_addin != NULL)
+    {
+      g_signal_handlers_disconnect_by_func (self->workspace_addin,
+                                            G_CALLBACK (gbp_vim_editor_page_addin_notify_active_cb),
+                                            self);
+      self->workspace_addin = NULL;
+    }
+
+  workspace = ide_widget_get_workspace (GTK_WIDGET (self->page));
+  workspace_addin = ide_workspace_addin_find_by_module_name (workspace, "vim");
+
+  if (workspace_addin == NULL || !GBP_IS_VIM_WORKSPACE_ADDIN (workspace_addin))
+    IDE_GOTO (disabled);
+
+  self->workspace_addin = GBP_VIM_WORKSPACE_ADDIN (workspace_addin);
+
+  g_signal_connect_object (self->workspace_addin,
+                           "notify::active",
+                           G_CALLBACK (gbp_vim_editor_page_addin_notify_active_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  if (!gbp_vim_workspace_addin_get_active (self->workspace_addin))
+    IDE_GOTO (disabled);
+
+  if (!self->enabled)
+    {
+      self->enabled = TRUE;
+      gtk_im_context_set_client_widget (im_context, GTK_WIDGET (view));
+      gtk_widget_add_controller (GTK_WIDGET (view), g_object_ref (self->key_controller));
+    }
+
+  IDE_EXIT;
+
+disabled:
+  if (self->enabled)
+    {
+      self->enabled = FALSE;
+      gtk_im_context_set_client_widget (im_context, NULL);
+      gtk_widget_remove_controller (GTK_WIDGET (view), self->key_controller);
+      gtk_event_controller_reset (self->key_controller);
+    }
+
+  IDE_EXIT;
+}
+
+static void
+gbp_vim_editor_page_addin_notify_active_cb (GbpVimEditorPageAddin *self,
+                                            GParamSpec            *pspec,
+                                            GbpVimWorkspaceAddin  *workspace_addin)
+{
+  g_assert (GBP_IS_VIM_EDITOR_PAGE_ADDIN (self));
+  g_assert (GBP_IS_VIM_WORKSPACE_ADDIN (workspace_addin));
+
+  gbp_vim_editor_page_addin_update (self);
+}
+
+static void
+gbp_vim_editor_page_addin_notify_command_bar_text_cb (GbpVimEditorPageAddin *self,
+                                                      GParamSpec            *pspec,
+                                                      GtkSourceVimIMContext *im_context)
+{
+  const char *command_bar;
+
+  g_assert (GBP_IS_VIM_EDITOR_PAGE_ADDIN (self));
+  g_assert (GTK_SOURCE_IS_VIM_IM_CONTEXT (im_context));
+
+  command_bar = gtk_source_vim_im_context_get_command_bar_text (im_context);
+
+  if (self->workspace_addin)
+    gbp_vim_workspace_addin_set_command_bar (self->workspace_addin, command_bar);
+}
+
+static void
+gbp_vim_editor_page_addin_notify_command_text_cb (GbpVimEditorPageAddin *self,
+                                                  GParamSpec            *pspec,
+                                                  GtkSourceVimIMContext *im_context)
+{
+  const char *command;
+
+  g_assert (GBP_IS_VIM_EDITOR_PAGE_ADDIN (self));
+  g_assert (GTK_SOURCE_IS_VIM_IM_CONTEXT (im_context));
+
+  command = gtk_source_vim_im_context_get_command_text (im_context);
+
+  if (self->workspace_addin)
+    gbp_vim_workspace_addin_set_command (self->workspace_addin, command);
+}
+
+static void
+gbp_vim_editor_page_addin_load (IdeEditorPageAddin *addin,
+                                IdeEditorPage      *page)
+{
+  GbpVimEditorPageAddin *self = (GbpVimEditorPageAddin *)addin;
+  GtkIMContext *im_context;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_EDITOR_PAGE_ADDIN (self));
+  g_assert (IDE_IS_EDITOR_PAGE (page));
+
+  self->page = page;
+
+  self->key_controller = gtk_event_controller_key_new ();
+  im_context = gtk_source_vim_im_context_new ();
+  gtk_event_controller_set_propagation_phase (self->key_controller, GTK_PHASE_CAPTURE);
+  gtk_event_controller_key_set_im_context (GTK_EVENT_CONTROLLER_KEY (self->key_controller), im_context);
+
+  g_signal_connect_object (im_context,
+                           "notify::command-bar-text",
+                           G_CALLBACK (gbp_vim_editor_page_addin_notify_command_bar_text_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  g_signal_connect_object (im_context,
+                           "notify::command-text",
+                           G_CALLBACK (gbp_vim_editor_page_addin_notify_command_text_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
+
+  g_object_unref (im_context);
+
+  gbp_vim_editor_page_addin_update (self);
+
+  IDE_EXIT;
+}
+
+static void
+gbp_vim_editor_page_addin_unload (IdeEditorPageAddin *addin,
+                                  IdeEditorPage      *page)
+{
+  GbpVimEditorPageAddin *self = (GbpVimEditorPageAddin *)addin;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_EDITOR_PAGE_ADDIN (self));
+  g_assert (IDE_IS_EDITOR_PAGE (page));
+
+  if (self->enabled)
+    {
+      IdeSourceView *view = ide_editor_page_get_view (page);
+
+      self->enabled = FALSE;
+      gtk_widget_remove_controller (GTK_WIDGET (view), self->key_controller);
+    }
+
+  g_clear_object (&self->key_controller);
+
+  self->page = NULL;
+  self->workspace_addin = NULL;
+
+  IDE_EXIT;
 }
