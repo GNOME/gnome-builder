de4b365af17d0b11977325657e1b4d978a214214
Christian Hergert <chergert@redhat.com>
2022-04-07 16:16:35 -0700

    libide/gui: add basic spin button preferences support

--- src/libide/gui/ide-preferences-window.h
+++ src/libide/gui/ide-preferences-window.h
@@ -128,5 +128,10 @@ void                ide_preferences_window_check      (const char               
                                                        const IdePreferenceItemEntry  *entry,
                                                        AdwPreferencesGroup           *group,
                                                        gpointer                       user_data);
+IDE_AVAILABLE_IN_ALL
+void                ide_preferences_window_spin       (const char                    *page_name,
+                                                       const IdePreferenceItemEntry  *entry,
+                                                       AdwPreferencesGroup           *group,
+                                                       gpointer                       user_data);
 
 G_END_DECLS
