c9f331b8279c9d27582faa65aa19bf1abfc52dcb
Christian Hergert <chergert@redhat.com>
2022-04-19 16:31:39 -0700

    plugins/pygi: port python GI imports to C
    Honestly this could have been fine in Python, but I have a distaste for
    async Gio-based Python code, even though I want async everywhere.

---
+++ src/plugins/python-gi-imports-completion/pygi-plugin.c
@@ -0,0 +1,37 @@
+/* pygi-plugin.c
+ *
+ * Copyright 2022 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "pygi-plugin"
+
+#include "config.h"
+
+#include <libpeas/peas.h>
+
+#include <libide-sourceview.h>
+
+#include "gbp-pygi-completion-provider.h"
+
+_IDE_EXTERN void
+_gbp_pygi_register_types (PeasObjectModule *module)
+{
+  peas_object_module_register_extension_type (module,
+                                              GTK_SOURCE_TYPE_COMPLETION_PROVIDER,
+                                              GBP_TYPE_PYGI_COMPLETION_PROVIDER);
+}
