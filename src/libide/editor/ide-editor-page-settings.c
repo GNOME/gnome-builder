269861d692900ea662ea3f933225216b324514a0
Christian Hergert <chergert@redhat.com>
2022-04-20 17:27:37 -0700

    libide/editor: use line-height instead of line-spacing
    Now that we have GTK 4 we can use line-height CSS instead of pixels above
    or below lines.

--- src/libide/editor/ide-editor-page-settings.c
+++ src/libide/editor/ide-editor-page-settings.c
@@ -187,6 +187,9 @@ _ide_editor_page_settings_init (IdeEditorPage *self)
   g_settings_bind (editor_settings, "enable-snippets",
                    self->view, "enable-snippets",
                    G_SETTINGS_BIND_GET);
+  g_settings_bind (editor_settings, "line-height",
+                   self->view, "line-height",
+                   G_SETTINGS_BIND_GET);
 
   g_settings_bind_with_mapping (editor_settings, "font-name",
                                 self->view, "font-desc",
