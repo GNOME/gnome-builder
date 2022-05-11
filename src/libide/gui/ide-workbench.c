4c8fd0a569feb79d2f7b5b0712381a4203d6712a
Christian Hergert <chergert@redhat.com>
2022-05-11 11:12:44 -0700

    plugins/debuggerui: menu actions to break on warning/critical
    This allows setting the G_DEBUG=fatal-warnings or G_DEBUG=fatal-criticals
    environment variables with check menu items in the run menu. A new
    "Debugger" sections section is added to contain this.
    
    Even though we show these as checks, it's mostly to just not have to add
    a third "Don't break on log" menu item as we can't really toggle them
    individually.
    
    This also makes the IdeDebugManager a GActionGroup like we do for other
    manager objects with two new GAction for the breakpoint settings.

--- src/libide/gui/ide-workbench.c
+++ src/libide/gui/ide-workbench.c
@@ -724,6 +724,7 @@ insert_action_groups_foreach_cb (IdeWorkspace *workspace,
   } groups[] = {
     { "config-manager", IDE_TYPE_CONFIG_MANAGER },
     { "build-manager", IDE_TYPE_BUILD_MANAGER },
+    { "debug-manager", IDE_TYPE_DEBUG_MANAGER },
     { "device-manager", IDE_TYPE_DEVICE_MANAGER },
     { "run-manager", IDE_TYPE_RUN_MANAGER },
     { "test-manager", IDE_TYPE_TEST_MANAGER },
