d0bdcc4c452f1e25da1200b4a93d5e51238d056a
Christian Hergert <chergert@redhat.com>
2022-05-11 17:42:44 -0700

    plugins/find-other-file: port to GTK 4
    This uses a new design for the find-other-file which places the menu
    button in the statusbar with a popover that is displayed when activated.
    
    Plugins can further extend the list of files. In the future we may also
    want to have a way to perform certain actions on the similar files such
    as open the current file in a designer instead of as a source file.
    
    But that can come later.

---
+++ src/plugins/find-other-file/gbp-find-other-file-workspace-addin.c
@@ -0,0 +1,236 @@
+/* gbp-find-other-file-workspace-addin.c
+ *
+ * Copyright 2022 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "gbp-find-other-file-workspace-addin"
+
+#include "config.h"
+
+#include <glib/gi18n.h>
+
+#include <libide-gui.h>
+#include <libide-editor.h>
+#include <libide-projects.h>
+
+#include "gbp-find-other-file-popover.h"
+#include "gbp-find-other-file-workspace-addin.h"
+
+struct _GbpFindOtherFileWorkspaceAddin
+{
+  GObject                  parent_instance;
+  IdeWorkspace            *workspace;
+  GtkMenuButton           *menu_button;
+  GtkLabel                *label;
+  GtkImage                *image;
+  GbpFindOtherFilePopover *popover;
+};
+
+static void
+find_other_file_action (GSimpleAction *action,
+                        GVariant      *param,
+                        gpointer       user_data)
+{
+  GbpFindOtherFileWorkspaceAddin *self = user_data;
+
+  g_assert (GBP_IS_FIND_OTHER_FILE_WORKSPACE_ADDIN (self));
+
+  if (self->menu_button != NULL &&
+      gtk_widget_get_visible (GTK_WIDGET (self->menu_button)))
+    gtk_menu_button_popup (self->menu_button);
+}
+
+static GActionEntry actions[] = {
+  { "find-other-file", find_other_file_action },
+};
+
+static void
+gbp_find_other_file_workspace_addin_clear (GbpFindOtherFileWorkspaceAddin *self)
+{
+  g_assert (GBP_IS_FIND_OTHER_FILE_WORKSPACE_ADDIN (self));
+
+  gtk_widget_hide (GTK_WIDGET (self->menu_button));
+  gbp_find_other_file_popover_set_model (self->popover, NULL);
+}
+
+static void
+gbp_find_other_file_workspace_addin_list_similar_cb (GObject      *object,
+                                                     GAsyncResult *result,
+                                                     gpointer      user_data)
+{
+  IdeProject *project = (IdeProject *)object;
+  g_autoptr(GbpFindOtherFileWorkspaceAddin) self = user_data;
+  g_autoptr(GListModel) model = NULL;
+  g_autoptr(GError) error = NULL;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_PROJECT (project));
+  g_assert (G_IS_ASYNC_RESULT (result));
+  g_assert (GBP_IS_FIND_OTHER_FILE_WORKSPACE_ADDIN (self));
+
+  /* Maybe we were disposed already */
+  if (self->workspace == NULL)
+    IDE_EXIT;
+
+  if (!(model = ide_project_list_similar_finish (project, result, &error)))
+    {
+      if (!ide_error_ignore (error))
+        g_warning ("%s", error->message);
+      gbp_find_other_file_workspace_addin_clear (self);
+      IDE_EXIT;
+    }
+
+  gbp_find_other_file_popover_set_model (self->popover, model);
+  gtk_widget_show (GTK_WIDGET (self->menu_button));
+
+  IDE_EXIT;
+}
+
+static void
+gbp_find_other_file_workspace_addin_page_changed (IdeWorkspaceAddin *addin,
+                                                  IdePage           *page)
+{
+  GbpFindOtherFileWorkspaceAddin *self = (GbpFindOtherFileWorkspaceAddin *)addin;
+  IdeProject *project;
+  IdeContext *context;
+  GFile *file;
+
+  IDE_ENTRY;
+
+  g_assert (GBP_IS_FIND_OTHER_FILE_WORKSPACE_ADDIN (self));
+  g_assert (!page || IDE_IS_PAGE (page));
+
+  if (!IDE_IS_EDITOR_PAGE (page))
+    {
+      gbp_find_other_file_workspace_addin_clear (self);
+      IDE_EXIT;
+    }
+
+  context = ide_workspace_get_context (self->workspace);
+  project = ide_project_from_context (context);
+  file = ide_editor_page_get_file (IDE_EDITOR_PAGE (page));
+
+  ide_project_list_similar_async (project,
+                                  file,
+                                  NULL,
+                                  gbp_find_other_file_workspace_addin_list_similar_cb,
+                                  g_object_ref (self));
+
+  IDE_EXIT;
+}
+
+static void
+gbp_find_other_file_workspace_addin_load (IdeWorkspaceAddin *addin,
+                                          IdeWorkspace      *workspace)
+{
+  GbpFindOtherFileWorkspaceAddin *self = (GbpFindOtherFileWorkspaceAddin *)addin;
+  PanelStatusbar *statusbar;
+  GtkBox *box;
+
+  IDE_ENTRY;
+
+  g_assert (GBP_IS_FIND_OTHER_FILE_WORKSPACE_ADDIN (self));
+  g_assert (IDE_IS_WORKSPACE (workspace));
+
+  self->workspace = workspace;
+
+  self->popover = g_object_new (GBP_TYPE_FIND_OTHER_FILE_POPOVER,
+                                NULL);
+  box = g_object_new (GTK_TYPE_BOX,
+                      "orientation", GTK_ORIENTATION_HORIZONTAL,
+                      "spacing", 6,
+                      NULL);
+  self->image = g_object_new (GTK_TYPE_IMAGE,
+                              "icon-name", "folder-symbolic",
+                              "pixel-size", 16,
+                              NULL);
+  gtk_box_append (box, GTK_WIDGET (self->image));
+#if 0
+  self->label = g_object_new (GTK_TYPE_LABEL,
+                              "ellipsize", PANGO_ELLIPSIZE_END,
+                              NULL);
+  gtk_box_append (box, GTK_WIDGET (self->label));
+#endif
+  self->menu_button = g_object_new (GTK_TYPE_MENU_BUTTON,
+                                    "focus-on-click", FALSE,
+                                    "popover", self->popover,
+                                    "direction", GTK_ARROW_UP,
+                                    "child", box,
+                                    "visible", FALSE,
+                                    "tooltip-text", _("Similar Files"),
+                                    NULL);
+
+  statusbar = ide_workspace_get_statusbar (workspace);
+  panel_statusbar_add_suffix (statusbar, 10000, GTK_WIDGET (self->menu_button));
+
+  g_action_map_add_action_entries (G_ACTION_MAP (workspace),
+                                   actions,
+                                   G_N_ELEMENTS (actions),
+                                   self);
+
+  IDE_EXIT;
+}
+
+static void
+gbp_find_other_file_workspace_addin_unload (IdeWorkspaceAddin *addin,
+                                            IdeWorkspace      *workspace)
+{
+  GbpFindOtherFileWorkspaceAddin *self = (GbpFindOtherFileWorkspaceAddin *)addin;
+  PanelStatusbar *statusbar;
+
+  IDE_ENTRY;
+
+  g_assert (GBP_IS_FIND_OTHER_FILE_WORKSPACE_ADDIN (self));
+  g_assert (IDE_IS_WORKSPACE (workspace));
+
+  for (guint i = 0; i < G_N_ELEMENTS (actions); i++)
+    g_action_map_remove_action (G_ACTION_MAP (workspace), actions[i].name);
+
+  statusbar = ide_workspace_get_statusbar (workspace);
+  panel_statusbar_remove (statusbar, GTK_WIDGET (self->menu_button));
+  self->menu_button = NULL;
+  self->popover = NULL;
+  self->label = NULL;
+  self->image = NULL;
+
+  self->workspace = NULL;
+
+  IDE_EXIT;
+}
+
+static void
+workspace_addin_iface_init (IdeWorkspaceAddinInterface *iface)
+{
+  iface->load = gbp_find_other_file_workspace_addin_load;
+  iface->unload = gbp_find_other_file_workspace_addin_unload;
+  iface->page_changed = gbp_find_other_file_workspace_addin_page_changed;
+}
+
+G_DEFINE_FINAL_TYPE_WITH_CODE (GbpFindOtherFileWorkspaceAddin, gbp_find_other_file_workspace_addin, G_TYPE_OBJECT,
+                               G_IMPLEMENT_INTERFACE (IDE_TYPE_WORKSPACE_ADDIN, workspace_addin_iface_init))
+
+static void
+gbp_find_other_file_workspace_addin_class_init (GbpFindOtherFileWorkspaceAddinClass *klass)
+{
+}
+
+static void
+gbp_find_other_file_workspace_addin_init (GbpFindOtherFileWorkspaceAddin *self)
+{
+}
---
+++ src/plugins/find-other-file/gbp-found-file.c
@@ -0,0 +1,220 @@
+/* gbp-found-file.c
+ *
+ * Copyright 2022 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "gbp-found-file"
+
+#include "config.h"
+
+#include "gbp-found-file.h"
+
+struct _GbpFoundFile
+{
+  GObject parent_instance;
+  GFile *file;
+  GFileInfo *info;
+  char *relative;
+};
+
+enum {
+  PROP_0,
+  PROP_GICON,
+  PROP_DISPLAY_NAME,
+  PROP_FILE,
+  N_PROPS
+};
+
+G_DEFINE_FINAL_TYPE (GbpFoundFile, gbp_found_file, G_TYPE_OBJECT)
+
+static GParamSpec *properties [N_PROPS];
+
+static void
+gbp_found_file_query_info_cb (GObject      *object,
+                              GAsyncResult *result,
+                              gpointer      user_data)
+{
+  GFile *file = (GFile *)object;
+  g_autoptr(GbpFoundFile) self = user_data;
+
+  g_assert (G_IS_FILE (file));
+  g_assert (G_IS_ASYNC_RESULT (result));
+  g_assert (GBP_IS_FOUND_FILE (self));
+
+  if (!(self->info = g_file_query_info_finish (file, result, NULL)))
+    return;
+
+  g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_DISPLAY_NAME]);
+  g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_GICON]);
+}
+
+static void
+gbp_found_file_constructed (GObject *object)
+{
+  GbpFoundFile *self = (GbpFoundFile *)object;
+
+  G_OBJECT_CLASS (gbp_found_file_parent_class)->constructed (object);
+
+  if (self->file == NULL)
+    return;
+
+  g_file_query_info_async (self->file,
+                           G_FILE_ATTRIBUTE_STANDARD_DISPLAY_NAME","
+                           G_FILE_ATTRIBUTE_STANDARD_SYMBOLIC_ICON,
+                           G_FILE_QUERY_INFO_NONE,
+                           G_PRIORITY_LOW,
+                           NULL,
+                           gbp_found_file_query_info_cb,
+                           g_object_ref (self));
+}
+
+static void
+gbp_found_file_dispose (GObject *object)
+{
+  GbpFoundFile *self = (GbpFoundFile *)object;
+
+  g_clear_object (&self->file);
+  g_clear_object (&self->info);
+  g_clear_pointer (&self->relative, g_free);
+
+  G_OBJECT_CLASS (gbp_found_file_parent_class)->dispose (object);
+}
+
+static void
+gbp_found_file_get_property (GObject    *object,
+                             guint       prop_id,
+                             GValue     *value,
+                             GParamSpec *pspec)
+{
+  GbpFoundFile *self = GBP_FOUND_FILE (object);
+
+  switch (prop_id)
+    {
+    case PROP_DISPLAY_NAME:
+      if (self->relative != NULL)
+        g_value_set_string (value, self->relative);
+      else if (self->info != NULL)
+        g_value_set_string (value, g_file_info_get_attribute_string (self->info, G_FILE_ATTRIBUTE_STANDARD_DISPLAY_NAME));
+      break;
+
+    case PROP_FILE:
+      g_value_set_object (value, self->file);
+      break;
+
+    case PROP_GICON:
+      if (self->info != NULL)
+        g_value_set_object (value, g_file_info_get_attribute_object (self->info, G_FILE_ATTRIBUTE_STANDARD_SYMBOLIC_ICON));
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+gbp_found_file_set_property (GObject      *object,
+                             guint         prop_id,
+                             const GValue *value,
+                             GParamSpec   *pspec)
+{
+  GbpFoundFile *self = GBP_FOUND_FILE (object);
+
+  switch (prop_id)
+    {
+    case PROP_FILE:
+      self->file = g_value_dup_object (value);
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+    }
+}
+
+static void
+gbp_found_file_class_init (GbpFoundFileClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->constructed = gbp_found_file_constructed;
+  object_class->dispose = gbp_found_file_dispose;
+  object_class->get_property = gbp_found_file_get_property;
+  object_class->set_property = gbp_found_file_set_property;
+
+  properties [PROP_FILE] =
+    g_param_spec_object ("file", NULL, NULL,
+                         G_TYPE_FILE,
+                         (G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY | G_PARAM_STATIC_STRINGS));
+
+  properties [PROP_DISPLAY_NAME] =
+    g_param_spec_string ("display-name", NULL, NULL,
+                         NULL,
+                         (G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+
+  properties [PROP_GICON] =
+    g_param_spec_object ("gicon", NULL, NULL,
+                         G_TYPE_ICON,
+                         (G_PARAM_READABLE | G_PARAM_STATIC_STRINGS));
+
+  g_object_class_install_properties (object_class, N_PROPS, properties);
+}
+
+static void
+gbp_found_file_init (GbpFoundFile *self)
+{
+}
+
+GbpFoundFile *
+gbp_found_file_new (GFile *workdir,
+                    GFile *file)
+{
+  GbpFoundFile *ret;
+
+  g_return_val_if_fail (G_IS_FILE (file), NULL);
+
+  ret = g_object_new (GBP_TYPE_FOUND_FILE,
+                      "file", file,
+                      NULL);
+  ret->relative = g_file_get_relative_path (workdir, file);
+
+  return ret;
+}
+
+void
+gbp_found_file_open (GbpFoundFile *self,
+                     IdeWorkspace *workspace)
+{
+  g_autoptr(IdePanelPosition) position = NULL;
+  IdeWorkbench *workbench;
+
+  IDE_ENTRY;
+
+  g_return_if_fail (GBP_IS_FOUND_FILE (self));
+  g_return_if_fail (IDE_IS_WORKSPACE (workspace));
+
+  workbench = ide_workspace_get_workbench (workspace);
+  position = ide_panel_position_new ();
+
+  ide_workbench_open_async (workbench,
+                            self->file,
+                            NULL,
+                            IDE_BUFFER_OPEN_FLAGS_NONE,
+                            position,
+                            NULL, NULL, NULL);
+
+  IDE_EXIT;
+}
