d58a3de73030be643f0080991913eedc597bfebe
Christian Hergert <chergert@redhat.com>
2022-05-25 15:14:25 -0700

    plugin/buildstream: port buildstream plugin to C
    More work towards #1670

---
+++ src/plugins/buildstream/buildstream-plugin.c
@@ -0,0 +1,47 @@
+/* buildstream-plugin.c
+ *
+ * Copyright 2016-2022 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "buildstream-plugin"
+
+#include "config.h"
+
+#include <libpeas/peas.h>
+
+#include <libide-editor.h>
+#include <libide-gui.h>
+#include <libide-tree.h>
+
+#include "gbp-buildstream-build-system.h"
+#include "gbp-buildstream-build-system-discovery.h"
+#include "gbp-buildstream-pipeline-addin.h"
+
+_IDE_EXTERN void
+_gbp_buildstream_register_types (PeasObjectModule *module)
+{
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_BUILD_SYSTEM,
+                                              GBP_TYPE_BUILDSTREAM_BUILD_SYSTEM);
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_BUILD_SYSTEM_DISCOVERY,
+                                              GBP_TYPE_BUILDSTREAM_BUILD_SYSTEM_DISCOVERY);
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_PIPELINE_ADDIN,
+                                              GBP_TYPE_BUILDSTREAM_PIPELINE_ADDIN);
+}
