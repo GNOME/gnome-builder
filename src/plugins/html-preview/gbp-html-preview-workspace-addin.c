903ae2dbd239558cca1d8d916ac45e08d807eb70
Christian Hergert <chergert@redhat.com>
2022-06-06 13:31:36 -0700

    libide/webkit: allow transforms of buffer content
    This will be needed for things like reStructuredText and markdown preview.

--- src/plugins/html-preview/gbp-html-preview-workspace-addin.c
+++ src/plugins/html-preview/gbp-html-preview-workspace-addin.c
@@ -226,7 +226,7 @@ live_preview_action (GbpHtmlPreviewWorkspaceAddin *self,
   g_assert (IDE_IS_EDITOR_PAGE (self->editor_page));
 
   buffer = g_signal_group_dup_target (self->buffer_signals);
-  page = ide_webkit_page_new_for_buffer (GTK_TEXT_BUFFER (buffer));
+  page = ide_webkit_page_new_for_buffer (GTK_TEXT_BUFFER (buffer), NULL, NULL, NULL);
   position = ide_page_get_position (IDE_PAGE (self->editor_page));
 
   if (!ide_panel_position_get_column (position, &column))
