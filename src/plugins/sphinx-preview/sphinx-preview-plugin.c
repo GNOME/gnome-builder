c3b8b283447ee7556ff9e76c2da91d56664121cd
Christian Hergert <chergert@redhat.com>
2022-06-06 21:05:14 -0700

    plugins/sphinx-preview: add sphinx preview plugin
    There is still more work to be done here for things like rst, but this
    gets the harder stuff working which is a sphinx compiler and transforms.
    
    Thanks to IdeHtmlGenerator, we can do this in a much cleaner fashion than
    we used to.

---
+++ src/plugins/sphinx-preview/sphinx-preview-plugin.c
@@ -0,0 +1,48 @@
+/* sphinx-preview-plugin.c
+ *
+ * Copyright 2022 Christian Hergert <chergert@redhat.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define G_LOG_DOMAIN "sphinx-preview-plugin"
+
+#include "config.h"
+
+#include <libpeas/peas.h>
+
+#include <libide-gui.h>
+
+#include "gbp-sphinx-preview-workspace-addin.h"
+
+_IDE_EXTERN void
+_gbp_sphinx_preview_register_types (PeasObjectModule *module)
+{
+  const char *path;
+
+  if (!(path = g_find_program_in_path ("sphinx-build")))
+    {
+      /* We always have it in the Flatpak, so just complain for other types
+       * of installations which are incomplete.
+       */
+      g_debug ("sphinx-build not found in PATH. Refusing to register addins.");
+      return;
+    }
+
+  peas_object_module_register_extension_type (module,
+                                              IDE_TYPE_WORKSPACE_ADDIN,
+                                              GBP_TYPE_SPHINX_PREVIEW_WORKSPACE_ADDIN);
+}
