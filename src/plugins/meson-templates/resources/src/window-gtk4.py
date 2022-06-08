a0bb6899cbb5ad855ba74bfa04ceba62127ac0aa
Christian Hergert <chergert@redhat.com>
2022-06-07 20:13:24 -0700

    plugins/meson-templates: inherit AdwApplicationWindow
    If using libadwaita, inherit from AdwApplicationWindow instead.
    
    Clearly this all points out that we need a way to add patterns to an
    application eventually.

--- src/plugins/meson-templates/resources/src/window-gtk4.py
+++ src/plugins/meson-templates/resources/src/window-gtk4.py
@@ -1,10 +1,12 @@
 {{include "license.py"}}
 
+{{if is_adwaita}}
+from gi.repository import Adw
+{{end}}
 from gi.repository import Gtk
 
-
 @Gtk.Template(resource_path='{{appid_path}}/{{ui_file}}')
-class {{PreFix}}Window(Gtk.ApplicationWindow):
+class {{PreFix}}Window({{if is_adwaita}}Adw{{else}}Gtk{{end}}.ApplicationWindow):
     __gtype_name__ = '{{PreFix}}Window'
 
     label = Gtk.Template.Child()
