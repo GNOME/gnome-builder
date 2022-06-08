ce3b0389a6fac6e1c3e86b89dc20c991d855abe3
Christian Hergert <chergert@redhat.com>
2022-06-07 17:02:56 -0700

    plugins/meson-templates: fix template types/alignments
    app id should be const, and the alignment was off.

--- src/plugins/meson-templates/resources/src/application.h
+++ src/plugins/meson-templates/resources/src/application.h
@@ -14,7 +14,7 @@ G_BEGIN_DECLS
 
 G_DECLARE_FINAL_TYPE ({{PreFix}}Application, {{prefix_}}_application, {{PREFIX}}, APPLICATION, {{if is_adwaita}}AdwApplication{{else}}GtkApplication{{end}})
 
-{{PreFix}}Application *{{prefix_}}_application_new (gchar *application_id,
+{{PreFix}}Application *{{prefix_}}_application_new (const char        *application_id,
 {{Spaces}}{{spaces}}                               GApplicationFlags  flags);
 
 G_END_DECLS
