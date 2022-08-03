db129944e92d8aeb68020847ccf123250666157d
Christian Hergert <chergert@redhat.com>
2022-08-02 23:29:17 -0700

    libide/tweaks: recurse into factories
    The recursion is where items are inflated.

--- src/libide/tweaks/ide-tweaks-panel-list.c
+++ src/libide/tweaks/ide-tweaks-panel-list.c
@@ -74,7 +74,7 @@ panel_list_visitor (IdeTweaksItem *item,
 
   if (IDE_IS_TWEAKS_FACTORY (item) &&
       _ide_tweaks_factory_is_one_of (IDE_TWEAKS_FACTORY (item), &page_type, 1))
-    return IDE_TWEAKS_ITEM_VISIT_ACCEPT;
+    return IDE_TWEAKS_ITEM_VISIT_RECURSE;
 
   return IDE_TWEAKS_ITEM_VISIT_SKIP;
 }
