6ba1b683c930d8dae3b09c77cc4941ca4e3f4231
Christian Hergert <chergert@redhat.com>
2022-09-20 21:13:30 -0700

    plugins/codeui: implement actions to goto declaration/definition
    This was also still missing from the GTK 4 port.

--- src/plugins/codeui/gbp-codeui-editor-page-addin.c
+++ src/plugins/codeui/gbp-codeui-editor-page-addin.c
@@ -40,11 +40,17 @@ struct _GbpCodeuiEditorPageAddin
   gulong         notify_has_selection;
 };
 
-static void rename_symbol_action (GbpCodeuiEditorPageAddin *self,
-                                  GVariant                 *params);
+static void goto_declaration_action (GbpCodeuiEditorPageAddin *self,
+                                     GVariant                 *params);
+static void goto_definition_action  (GbpCodeuiEditorPageAddin *self,
+                                     GVariant                 *params);
+static void rename_symbol_action    (GbpCodeuiEditorPageAddin *self,
+                                     GVariant                 *params);
 
 IDE_DEFINE_ACTION_GROUP (GbpCodeuiEditorPageAddin, gbp_codeui_editor_page_addin, {
   { "rename-symbol", rename_symbol_action },
+  { "goto-declaration", goto_declaration_action },
+  { "goto-definition", goto_definition_action },
 })
 
 static void
@@ -52,6 +58,7 @@ gbp_codeui_editor_page_addin_update_state (GbpCodeuiEditorPageAddin *self)
 {
   IdeRenameProvider *provider;
   gboolean has_selection;
+  gboolean has_resolvers;
 
   IDE_ENTRY;
 
@@ -59,10 +66,13 @@ gbp_codeui_editor_page_addin_update_state (GbpCodeuiEditorPageAddin *self)
   g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
   g_assert (IDE_IS_BUFFER (self->buffer));
 
+  has_resolvers = ide_buffer_has_symbol_resolvers (self->buffer);
   provider = ide_buffer_get_rename_provider (self->buffer);
   has_selection = gtk_text_buffer_get_has_selection (GTK_TEXT_BUFFER (self->buffer));
 
   gbp_codeui_editor_page_addin_set_action_enabled (self, "rename-symbol", has_selection && provider);
+  gbp_codeui_editor_page_addin_set_action_enabled (self, "goto-declaration", has_resolvers);
+  gbp_codeui_editor_page_addin_set_action_enabled (self, "goto-definition", has_resolvers);
 
   IDE_EXIT;
 }
@@ -190,6 +200,139 @@ rename_symbol_action (GbpCodeuiEditorPageAddin *self,
   gtk_window_set_transient_for (GTK_WINDOW (dialog), toplevel);
   gtk_window_set_modal (GTK_WINDOW (dialog), TRUE);
   gtk_window_present (GTK_WINDOW (dialog));
+
+  IDE_EXIT;
+}
+
+static void
+gbp_codeui_editor_page_addin_error (GbpCodeuiEditorPageAddin *self,
+                                    const GError             *error)
+{
+  IdeContext *context;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
+  g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
+  g_assert (error != NULL);
+
+  if (self->page == NULL)
+    return;
+
+  context = ide_widget_get_context (GTK_WIDGET (self->page));
+  ide_context_warning (context, "%s", error->message);
+}
+
+static void
+gbp_codeui_editor_page_addin_navigate_to (GbpCodeuiEditorPageAddin *self,
+                                          IdeLocation              *location)
+{
+  g_autoptr(PanelPosition) position = NULL;
+  IdeWorkspace *workspace;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
+  g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
+
+  if (self->page == NULL || location == NULL)
+    return;
+
+  workspace = ide_widget_get_workspace (GTK_WIDGET (self->page));
+  position = panel_widget_get_position (PANEL_WIDGET (self->page));
+
+  ide_editor_focus_location (workspace, position, location);
+}
+
+static void
+goto_declaration_cb (GObject      *object,
+                     GAsyncResult *result,
+                     gpointer      user_data)
+{
+  IdeBuffer *buffer = (IdeBuffer *)object;
+  g_autoptr(GbpCodeuiEditorPageAddin) self = user_data;
+  g_autoptr(IdeSymbol) symbol = NULL;
+  g_autoptr(GError) error = NULL;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
+  g_assert (IDE_IS_BUFFER (buffer));
+  g_assert (G_IS_ASYNC_RESULT (result));
+  g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
+
+  if (!(symbol = ide_buffer_get_symbol_at_location_finish (buffer, result, &error)))
+    gbp_codeui_editor_page_addin_error (self, error);
+  else
+    gbp_codeui_editor_page_addin_navigate_to (self,
+                                              ide_symbol_get_header_location (symbol) ?
+                                              ide_symbol_get_header_location (symbol) :
+                                              ide_symbol_get_location (symbol));
+
+  IDE_EXIT;
+}
+
+static void
+goto_declaration_action (GbpCodeuiEditorPageAddin *self,
+                         GVariant                 *params)
+{
+  GtkTextIter insert;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
+  g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
+
+  ide_buffer_get_selection_bounds (self->buffer, &insert, NULL);
+
+  ide_buffer_get_symbol_at_location_async (self->buffer,
+                                           &insert,
+                                           NULL,
+                                           goto_declaration_cb,
+                                           g_object_ref (self));
+
+  IDE_EXIT;
+}
+
+static void
+goto_definition_cb (GObject      *object,
+                    GAsyncResult *result,
+                    gpointer      user_data)
+{
+  IdeBuffer *buffer = (IdeBuffer *)object;
+  g_autoptr(GbpCodeuiEditorPageAddin) self = user_data;
+  g_autoptr(IdeSymbol) symbol = NULL;
+  g_autoptr(GError) error = NULL;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
+  g_assert (IDE_IS_BUFFER (buffer));
+  g_assert (G_IS_ASYNC_RESULT (result));
+  g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
+
+  if (!(symbol = ide_buffer_get_symbol_at_location_finish (buffer, result, &error)))
+    gbp_codeui_editor_page_addin_error (self, error);
+  else
+    gbp_codeui_editor_page_addin_navigate_to (self, ide_symbol_get_location (symbol));
+
+  IDE_EXIT;
+}
+
+static void
+goto_definition_action (GbpCodeuiEditorPageAddin *self,
+                        GVariant                 *params)
+{
+  GtkTextIter insert;
+
+  IDE_ENTRY;
+
+  g_assert (IDE_IS_MAIN_THREAD ());
+  g_assert (GBP_IS_CODEUI_EDITOR_PAGE_ADDIN (self));
+
+  ide_buffer_get_selection_bounds (self->buffer, &insert, NULL);
+
+  ide_buffer_get_symbol_at_location_async (self->buffer,
+                                           &insert,
+                                           NULL,
+                                           goto_definition_cb,
+                                           g_object_ref (self));
 
   IDE_EXIT;
 }
