10bbe817d43ec92b1650b843cfdb761bc43ff438
Christian Hergert <chergert@redhat.com>
2022-09-24 15:35:03 -0700

    plugins/deviced: add helper to get underlying device

--- src/plugins/deviced/gbp-deviced-device.h
+++ src/plugins/deviced/gbp-deviced-device.h
@@ -1,6 +1,6 @@
 /* gbp-deviced-device.h
  *
- * Copyright 2018-2019 Christian Hergert <chergert@redhat.com>
+ * Copyright 2018-2022 Christian Hergert <chergert@redhat.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -30,6 +30,7 @@ G_BEGIN_DECLS
 G_DECLARE_FINAL_TYPE (GbpDevicedDevice, gbp_deviced_device, GBP, DEVICED_DEVICE, IdeDevice)
 
 GbpDevicedDevice *gbp_deviced_device_new                   (DevdDevice             *device);
+DevdDevice       *gbp_deviced_device_get_device            (GbpDevicedDevice       *self);
 void              gbp_deviced_device_get_commit_async      (GbpDevicedDevice       *self,
                                                             const gchar            *commit_id,
                                                             GCancellable           *cancellable,
