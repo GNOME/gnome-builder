55a2eeb52f8580ff888d4366a6175c2c81ff508b
Christian Hergert <chergert@redhat.com>
2023-03-14 16:02:21 -0700

    libide/gtk: use GtkFilterListModel as basis for IdeUniqueListModel
    This is a listmodel that removes subsequent items that are the same as
    the previous item.
    
    It still has soem faults, such as not handling insertions right when it
    will match the exact next item (which is already shown). But we can deal
    with that later as it doesn't affect us currently for the use of this
    model in shortcuts.

--- src/libide/gtk/ide-unique-list-model.c
+++ src/libide/gtk/ide-unique-list-model.c
@@ -1,21 +1,24 @@
-/* ide-unique-list-model.c
+/*
+ * Copyright © 2018 Benjamin Otte
+ * Copyright © 2023 Christian Hergert
  *
- * Copyright 2023 Christian Hergert <chergert@redhat.com>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * Authors: Benjamin Otte <otte@gnome.org>
+ *          Christian Hergert <chergert@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #define G_LOG_DOMAIN "ide-unique-list-model"
@@ -26,23 +29,24 @@
 
 struct _IdeUniqueListModel
 {
-  GObject     parent_instance;
+  GObject           parent_instance;
 
-  GListModel *model;
-  GtkSorter  *sorter;
+  GtkSortListModel *sorted;
+  GtkSorter        *sorter;
 
-  GtkBitset  *unique;
-  GtkBitset  *pending;
+  GtkBitset        *unique;
+  GtkBitset        *pending;
 
-  guint       incremental_source;
-
-  guint       incremental : 1;
+  guint             incremental_source;
+  guint             incremental : 1;
 };
 
 enum {
   PROP_0,
   PROP_INCREMENTAL,
   PROP_MODEL,
+  PROP_N_ITEMS,
+  PROP_PENDING,
   PROP_SORTER,
   N_PROPS
 };
@@ -52,8 +56,8 @@ ide_unique_list_model_get_item_type (GListModel *model)
 {
   IdeUniqueListModel *self = IDE_UNIQUE_LIST_MODEL (model);
 
-  if (self->model != NULL)
-    return g_list_model_get_item_type (self->model);
+  if (self->sorted != NULL)
+    return g_list_model_get_item_type (G_LIST_MODEL (self->sorted));
 
   return G_TYPE_OBJECT;
 }
@@ -76,7 +80,7 @@ ide_unique_list_model_get_item (GListModel *model,
   if (unfiltered == 0 && position >= gtk_bitset_get_size (self->unique))
     return NULL;
 
-  return g_list_model_get_item (self->model, unfiltered);
+  return g_list_model_get_item (G_LIST_MODEL (self->sorted), unfiltered);
 }
 
 static void
@@ -92,18 +96,239 @@ G_DEFINE_FINAL_TYPE_WITH_CODE (IdeUniqueListModel, ide_unique_list_model, G_TYPE
 
 static GParamSpec *properties [N_PROPS];
 
+static gboolean
+ide_unique_list_model_run_filter_on_item (IdeUniqueListModel *self,
+                                          guint               position)
+{
+  gpointer previous;
+  gpointer item;
+  gboolean visible;
+
+  if (position == 0 || self->sorter == NULL)
+    return TRUE;
+
+  previous = g_list_model_get_item (G_LIST_MODEL (self->sorted), position - 1);
+  item = g_list_model_get_item (G_LIST_MODEL (self->sorted), position);
+
+  visible = gtk_sorter_compare (self->sorter, previous, item) != 0;
+
+  g_object_unref (previous);
+  g_object_unref (item);
+
+  return visible;
+}
+
+static void
+ide_unique_list_model_run_deduplicator (IdeUniqueListModel *self,
+                                        guint               n_steps)
+{
+  GtkBitsetIter iter;
+  guint i, pos;
+  gboolean more;
+
+  g_assert (IDE_IS_UNIQUE_LIST_MODEL (self));
+
+  if (self->pending == NULL)
+    return;
+
+  for (i = 0, more = gtk_bitset_iter_init_first (&iter, self->pending, &pos);
+       i < n_steps && more;
+       i++, more = gtk_bitset_iter_next (&iter, &pos))
+    {
+      if (ide_unique_list_model_run_filter_on_item (self, pos))
+        gtk_bitset_add (self->unique, pos);
+    }
+
+  if (more)
+    gtk_bitset_remove_range_closed (self->pending, 0, pos - 1);
+  else
+    g_clear_pointer (&self->pending, gtk_bitset_unref);
+
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_PENDING]);
+}
+
+static void
+ide_unique_list_model_stop_deduplicating (IdeUniqueListModel *self)
+{
+  gboolean notify_pending;
+
+  g_assert (IDE_IS_UNIQUE_LIST_MODEL (self));
+
+  notify_pending = self->pending != NULL;
+
+  g_clear_pointer (&self->pending, gtk_bitset_unref);
+  g_clear_handle_id (&self->incremental_source, g_source_remove);
+
+  if (notify_pending)
+    g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_PENDING]);
+}
+
+/**
+ * ide_unique_list_model_emit_items_changed_for_changes:
+ * @old: (transfer full): a #GtkBitset
+ */
+static void
+ide_unique_list_model_emit_items_changed_for_changes (IdeUniqueListModel *self,
+                                                      GtkBitset          *old)
+{
+  GtkBitset *changes;
+
+  g_assert (IDE_IS_UNIQUE_LIST_MODEL (self));
+  g_assert (old != NULL);
+
+  changes = gtk_bitset_copy (self->unique);
+
+  gtk_bitset_difference (changes, old);
+
+  if (!gtk_bitset_is_empty (changes))
+    {
+      guint min, max, removed, added;
+
+      min = gtk_bitset_get_minimum (changes);
+      max = gtk_bitset_get_maximum (changes);
+      removed = gtk_bitset_get_size_in_range (old, min, max);
+      added = gtk_bitset_get_size_in_range (self->unique, min, max);
+
+      g_list_model_items_changed (G_LIST_MODEL (self),
+                                  min == 0 ? 0 : gtk_bitset_get_size_in_range (self->unique, 0, min - 1),
+                                  removed,
+                                  added);
+
+      if (removed != added)
+        g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_N_ITEMS]);
+    }
+
+  gtk_bitset_unref (changes);
+  gtk_bitset_unref (old);
+}
+
+static gboolean
+ide_unique_list_model_deduplicate_source (gpointer user_data)
+{
+  IdeUniqueListModel *self = user_data;
+  GtkBitset *old;
+
+  g_assert (IDE_IS_UNIQUE_LIST_MODEL (self));
+  g_assert (self->unique != NULL);
+
+  old = gtk_bitset_copy (self->unique);
+  ide_unique_list_model_run_deduplicator (self, 512);
+
+  if (self->pending == NULL)
+    ide_unique_list_model_stop_deduplicating (self);
+
+  ide_unique_list_model_emit_items_changed_for_changes (self, old);
+
+  return G_SOURCE_CONTINUE;
+}
+
+/**
+ * ide_unique_list_model_start_deduplicating:
+ * @items: (transfer full): a #GtkBitset
+ */
+static void
+ide_unique_list_model_start_deduplicating (IdeUniqueListModel *self,
+                                           GtkBitset          *items)
+{
+  g_assert (IDE_IS_UNIQUE_LIST_MODEL (self));
+  g_assert (items != NULL);
+
+  if (self->pending != NULL)
+    {
+      gtk_bitset_union (self->pending, items);
+      gtk_bitset_unref (items);
+      g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_PENDING]);
+      return;
+    }
+
+  if (gtk_bitset_is_empty (items))
+    {
+      gtk_bitset_unref (items);
+      return;
+    }
+
+  self->pending = g_steal_pointer (&items);
+
+  if (!self->incremental)
+    {
+      ide_unique_list_model_run_deduplicator (self, G_MAXUINT);
+      g_assert (self->pending == NULL);
+      return;
+    }
+
+  g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_PENDING]);
+
+  g_assert (self->incremental_source == 0);
+
+  self->incremental_source = g_idle_add (ide_unique_list_model_deduplicate_source, self);
+  g_source_set_static_name (g_main_context_find_source_by_id (NULL,
+                                                              self->incremental_source),
+                            "[ide] ide_unique_list_model_deduplicate_source");
+}
+
+static void
+ide_unique_list_model_sorted_items_changed_cb (IdeUniqueListModel *self,
+                                               guint               position,
+                                               guint               removed,
+                                               guint               added,
+                                               GtkSortListModel   *sorted)
+{
+  guint sorter_removed = 0;
+  guint sorter_added = 0;
+
+  g_assert (IDE_IS_UNIQUE_LIST_MODEL (self));
+
+  if (removed == 0 && added == 0)
+    return;
+
+  if (removed > 0)
+    sorter_removed = gtk_bitset_get_size_in_range (self->unique, position, position + removed - 1);
+
+  gtk_bitset_splice (self->unique, position, removed, added);
+  if (self->pending != NULL)
+    gtk_bitset_splice (self->pending, position, removed, added);
+
+  if (added > 0)
+    {
+      ide_unique_list_model_start_deduplicating (self, gtk_bitset_new_range (position, added));
+      sorter_added = gtk_bitset_get_size_in_range (self->unique, position, position + added - 1);
+    }
+
+  if (sorter_removed > 0 || sorter_added > 0)
+    g_list_model_items_changed (G_LIST_MODEL (self),
+                                position == 0 ? 0 : gtk_bitset_get_size_in_range (self->unique, 0, position - 1),
+                                sorter_removed,
+                                sorter_added);
+
+  if (sorter_removed != sorter_added)
+    g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_N_ITEMS]);
+}
+
 static void
 ide_unique_list_model_dispose (GObject *object)
 {
   IdeUniqueListModel *self = (IdeUniqueListModel *)object;
 
-  g_clear_handle_id (&self->incremental_source, g_source_remove);
-
-  g_clear_object (&self->model);
   g_clear_object (&self->sorter);
-  g_clear_pointer (&self->unique, gtk_bitset_unref);
+
+  gtk_sort_list_model_set_model (self->sorted, NULL);
+  gtk_sort_list_model_set_sorter (self->sorted, NULL);
+
+  g_clear_handle_id (&self->incremental_source, g_source_remove);
+  g_clear_pointer (&self->pending, gtk_bitset_unref);
 
   G_OBJECT_CLASS (ide_unique_list_model_parent_class)->dispose (object);
+}
+
+static void
+ide_unique_list_model_finalize (GObject *object)
+{
+  IdeUniqueListModel *self = (IdeUniqueListModel *)object;
+
+  g_clear_object (&self->sorted);
+  g_clear_pointer (&self->unique, gtk_bitset_unref);
+
+  G_OBJECT_CLASS (ide_unique_list_model_parent_class)->finalize (object);
 }
 
 static void
@@ -122,6 +347,14 @@ ide_unique_list_model_get_property (GObject    *object,
 
     case PROP_MODEL:
       g_value_set_object (value, ide_unique_list_model_get_model (self));
+      break;
+
+    case PROP_N_ITEMS:
+      g_value_set_uint (value, g_list_model_get_n_items (G_LIST_MODEL (self)));
+      break;
+
+    case PROP_PENDING:
+      g_value_set_uint (value, ide_unique_list_model_get_pending (self));
       break;
 
     case PROP_SORTER:
@@ -166,6 +399,7 @@ ide_unique_list_model_class_init (IdeUniqueListModelClass *klass)
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
   object_class->dispose = ide_unique_list_model_dispose;
+  object_class->finalize = ide_unique_list_model_finalize;
   object_class->get_property = ide_unique_list_model_get_property;
   object_class->set_property = ide_unique_list_model_set_property;
 
@@ -179,6 +413,16 @@ ide_unique_list_model_class_init (IdeUniqueListModelClass *klass)
                          G_TYPE_LIST_MODEL,
                          (G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS));
 
+  properties[PROP_N_ITEMS] =
+    g_param_spec_uint ("n-items", NULL, NULL,
+                       0, G_MAXUINT, 0,
+                       (G_PARAM_READABLE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS));
+
+  properties[PROP_PENDING] =
+    g_param_spec_uint ("pending", NULL, NULL,
+                       0, G_MAXUINT, 0,
+                       (G_PARAM_READABLE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS));
+
   properties[PROP_SORTER] =
     g_param_spec_object ("sorter", NULL, NULL,
                          GTK_TYPE_SORTER,
@@ -190,6 +434,14 @@ ide_unique_list_model_class_init (IdeUniqueListModelClass *klass)
 static void
 ide_unique_list_model_init (IdeUniqueListModel *self)
 {
+  self->unique = gtk_bitset_new_empty ();
+  self->sorted = gtk_sort_list_model_new (NULL, NULL);
+  gtk_sort_list_model_set_incremental (self->sorted, FALSE);
+  g_signal_connect_object (self->sorted,
+                           "items-changed",
+                           G_CALLBACK (ide_unique_list_model_sorted_items_changed_cb),
+                           self,
+                           G_CONNECT_SWAPPED);
 }
 
 /**
@@ -239,7 +491,7 @@ ide_unique_list_model_get_model (IdeUniqueListModel *self)
 {
   g_return_val_if_fail (IDE_IS_UNIQUE_LIST_MODEL (self), NULL);
 
-  return self->model;
+  return gtk_sort_list_model_get_model (self->sorted);
 }
 
 /**
@@ -257,6 +509,11 @@ ide_unique_list_model_set_model (IdeUniqueListModel *self,
 {
   g_return_if_fail (IDE_IS_UNIQUE_LIST_MODEL (self));
   g_return_if_fail (!model || G_IS_LIST_MODEL (model));
+
+  if (model == ide_unique_list_model_get_model (self))
+    return;
+
+  gtk_sort_list_model_set_model (self->sorted, model);
 
   g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_MODEL]);
 }
@@ -295,7 +552,11 @@ ide_unique_list_model_set_sorter (IdeUniqueListModel *self,
   g_return_if_fail (IDE_IS_UNIQUE_LIST_MODEL (self));
   g_return_if_fail (!sorter || GTK_IS_SORTER (sorter));
 
-  g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_SORTER]);
+  if (g_set_object (&self->sorter, sorter))
+    {
+      gtk_sort_list_model_set_sorter (self->sorted, sorter);
+      g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_SORTER]);
+    }
 }
 
 /**
@@ -330,6 +591,31 @@ ide_unique_list_model_set_incremental (IdeUniqueListModel *self,
   if (self->incremental != incremental)
     {
       self->incremental = incremental;
+      gtk_sort_list_model_set_incremental (self->sorted, incremental);
       g_object_notify_by_pspec (G_OBJECT (self), properties [PROP_INCREMENTAL]);
     }
+}
+
+/**
+ * ide_unique_list_model_get_pending:
+ * @self: an #IdeUniqueListModel
+ *
+ * Returns the number of items that have not yet been filtered.
+ *
+ * You can use this value to check if @self is busy filtering by
+ * comparing the return value to 0 or you can compute the percentage
+ * of the filter remaining by dividing the return value by the total
+ * number of items in the underlying model:
+ *
+ * Returns: The number of items noet yet filtered
+ */
+guint
+ide_unique_list_model_get_pending (IdeUniqueListModel *self)
+{
+  g_return_val_if_fail (IDE_IS_UNIQUE_LIST_MODEL (self), 0);
+
+  if (self->pending == NULL)
+    return 0;
+
+  return gtk_bitset_get_size (self->pending);
 }
--- src/libide/gtk/ide-unique-list-model.h
+++ src/libide/gtk/ide-unique-list-model.h
@@ -1,21 +1,24 @@
-/* ide-unique-list-model.h
+/*
+ * Copyright © 2018 Benjamin Otte
+ * Copyright © 2023 Christian Hergert
  *
- * Copyright 2023 Christian Hergert <chergert@redhat.com>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  *
- * SPDX-License-Identifier: GPL-3.0-or-later
+ * Authors: Benjamin Otte <otte@gnome.org>
+ *          Christian Hergert <chergert@gnome.org>
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  */
 
 #pragma once
@@ -49,5 +52,7 @@ gboolean            ide_unique_list_model_get_incremental (IdeUniqueListModel *s
 IDE_AVAILABLE_IN_44
 void                ide_unique_list_model_set_incremental (IdeUniqueListModel *self,
                                                            gboolean            incremental);
+IDE_AVAILABLE_IN_44
+guint               ide_unique_list_model_get_pending     (IdeUniqueListModel *self);
 
 G_END_DECLS
