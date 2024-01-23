934b1d78568c905facc7804368f6b7319901145c
Christian Hergert <chergert@redhat.com>
2024-01-23 14:16:32 -0800

    libide/terminal: remove background removal
    We don't hit this anymore now that we disable clearning the background so
    it should be safe to #if 0 for now.

--- src/libide/terminal/ide-terminal.c
+++ src/libide/terminal/ide-terminal.c
@@ -1058,7 +1058,6 @@ ide_terminal_rewrite_snapshot (GtkWidget   *widget,
   g_autoptr(GtkSnapshot) alternate = NULL;
   g_autoptr(GskRenderNode) root = NULL;
   g_autoptr(GPtrArray) children = NULL;
-  gboolean dropped_bg = FALSE;
 
   g_assert (GTK_IS_SNAPSHOT (snapshot));
 
@@ -1079,6 +1078,11 @@ ide_terminal_rewrite_snapshot (GtkWidget   *widget,
           GskRenderNode *node = gsk_container_node_get_child (root, i);
           GskRenderNodeType node_type = gsk_render_node_get_node_type (node);
 
+#if 0
+          /* NOTE: This should no longer be necessary because we disable
+           * clearing the background of IdeTerminal.
+           */
+
           /* Drop the color node because we get that for free from our
            * background recoloring. This avoids an extra large overdraw
            * as a bonus optimization while we fix clipping.
@@ -1088,6 +1092,7 @@ ide_terminal_rewrite_snapshot (GtkWidget   *widget,
               dropped_bg = TRUE;
               continue;
             }
+#endif
 
           /* If we get a clip node here, it's because we're in some
            * sort of window size that has partial line offset in the
