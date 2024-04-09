01f5c095dc541ea33a0c5750e551f991979476ed
Christian Hergert <chergert@redhat.com>
2024-04-09 16:35:47 -0700

    plugins/manuals: implement search sections for queries
    This makes ManualsSearchQuery a GtkSectionModel so that we can group
    results by their respective SDKs without having to load every single
    item into memory.
    
    This sort of cheats to do that by doing a query for each SDK. But in
    practice that's a small number anyway so should be fine.

--- src/plugins/manuals/manuals-search-query.c
+++ src/plugins/manuals/manuals-search-query.c
@@ -23,9 +23,11 @@
 
 #include <gtk/gtk.h>
 
+#include "manuals-book.h"
 #include "manuals-gom.h"
 #include "manuals-keyword.h"
 #include "manuals-repository.h"
+#include "manuals-sdk.h"
 #include "manuals-search-model.h"
 #include "manuals-search-result.h"
 #include "manuals-search-query.h"
@@ -82,8 +84,33 @@ list_model_iface_init (GListModelInterface *iface)
   iface->get_n_items = manuals_search_query_get_n_items;
 }
 
+static void
+manuals_search_query_get_section (GtkSectionModel *model,
+                                  guint            position,
+                                  guint           *out_start,
+                                  guint           *out_end)
+{
+  ManualsSearchQuery *self = MANUALS_SEARCH_QUERY (model);
+
+  if (self->state == STATE_COMPLETED && self->results != NULL)
+    return gtk_section_model_get_section (GTK_SECTION_MODEL (self->results),
+                                          position,
+                                          out_start,
+                                          out_end);
+
+  *out_start = *out_end = 0;
+}
+
+static void
+section_model_iface_init (GtkSectionModelInterface *iface)
+{
+  iface->get_section = manuals_search_query_get_section;
+}
+
 G_DEFINE_FINAL_TYPE_WITH_CODE (ManualsSearchQuery, manuals_search_query, G_TYPE_OBJECT,
-                               G_IMPLEMENT_INTERFACE (G_TYPE_LIST_MODEL, list_model_iface_init))
+                               G_IMPLEMENT_INTERFACE (G_TYPE_LIST_MODEL, list_model_iface_init)
+                               G_IMPLEMENT_INTERFACE (GTK_TYPE_SECTION_MODEL, section_model_iface_init))
+
 
 enum {
   PROP_0,
@@ -223,39 +250,6 @@ like_string (const char *str)
 }
 
 static DexFuture *
-manuals_search_query_execute_cb (DexFuture *completed,
-                                 gpointer   user_data)
-{
-  g_autoptr(ManualsSearchModel) model = NULL;
-  GListStore *store = user_data;
-  GomResourceGroup *group;
-  const GValue *value;
-
-  g_assert (DEX_IS_FUTURE (completed));
-  g_assert (G_IS_LIST_STORE (store));
-
-  value = dex_future_get_value (completed, NULL);
-  group = g_value_get_object (value);
-  model = manuals_search_model_new (group);
-
-  g_list_store_append (store, model);
-
-  return dex_future_new_for_boolean (TRUE);
-}
-
-static DexFuture *
-manuals_search_query_completed_cb (DexFuture *completed,
-                                   gpointer   user_data)
-{
-  GListStore *store = user_data;
-
-  g_assert (DEX_IS_FUTURE (completed));
-  g_assert (G_IS_LIST_STORE (store));
-
-  return dex_future_new_take_object (gtk_flatten_list_model_new (g_object_ref (G_LIST_MODEL (store))));
-}
-
-static DexFuture *
 manuals_search_query_propagate_cb (DexFuture *completed,
                                    gpointer   user_data)
 {
@@ -275,10 +269,102 @@ manuals_search_query_propagate_cb (DexFuture *completed,
   if (new_len)
     {
       g_list_model_items_changed (G_LIST_MODEL (self), 0, 0, new_len);
+      gtk_section_model_sections_changed (GTK_SECTION_MODEL (self), 0, new_len);
       g_object_notify_by_pspec (G_OBJECT (self), properties[PROP_N_ITEMS]);
     }
 
   return dex_future_new_for_boolean (TRUE);
+}
+
+typedef struct
+{
+  ManualsRepository *repository;
+  GomFilter *keyword_filter;
+} Execute;
+
+static void
+execute_free (Execute *execute)
+{
+  g_clear_object (&execute->keyword_filter);
+  g_clear_object (&execute->repository);
+  g_free (execute);
+}
+
+static DexFuture *
+manuals_search_query_execute_fiber (gpointer user_data)
+{
+  g_autoptr(GListModel) sdks = NULL;
+  g_autoptr(GListStore) store = NULL;
+  g_autoptr(GPtrArray) futures = NULL;
+  g_autoptr(GError) error = NULL;
+  Execute *execute = user_data;
+  guint n_sdks;
+
+  g_assert (execute != NULL);
+  g_assert (GOM_IS_FILTER (execute->keyword_filter));
+  g_assert (MANUALS_IS_REPOSITORY (execute->repository));
+
+  if (!(sdks = dex_await_object (manuals_repository_list_sdks_by_newest (execute->repository), &error)))
+    return dex_future_new_for_error (g_steal_pointer (&error));
+
+  futures = g_ptr_array_new_with_free_func (dex_unref);
+  n_sdks = g_list_model_get_n_items (sdks);
+
+  for (guint i = 0; i < n_sdks; i++)
+    {
+      g_autoptr(ManualsSdk) sdk = g_list_model_get_item (sdks, i);
+      g_autoptr(GArray) values = g_array_new (FALSE, TRUE, sizeof (GValue));
+      g_autoptr(GString) str = g_string_new ("\"book-id\" IN (");
+      g_autoptr(GListModel) books = NULL;
+      g_autoptr(GomFilter) book_filter = NULL;
+      g_autoptr(GomFilter) filter = NULL;
+      guint n_books;
+
+      if (!(books = dex_await_object (manuals_sdk_list_books (sdk), NULL)) ||
+          (n_books = g_list_model_get_n_items (books)) == 0)
+        continue;
+
+      for (guint j = 0; j < n_books; j++)
+        {
+          g_autoptr(ManualsBook) book = g_list_model_get_item (books, j);
+          GValue value = G_VALUE_INIT;
+
+          g_value_init (&value, G_TYPE_INT64);
+          g_value_set_int64 (&value, manuals_book_get_id (book));
+
+          g_array_append_val (values, value);
+          g_string_append_c (str, '?');
+
+          if (j + 1 < n_books)
+            g_string_append_c (str, ',');
+        }
+
+      g_string_append_c (str, ')');
+
+      book_filter = gom_filter_new_sql (str->str, values);
+      filter = gom_filter_new_and (book_filter, execute->keyword_filter);
+
+      g_ptr_array_add (futures,
+                       gom_repository_find (GOM_REPOSITORY (execute->repository),
+                                            MANUALS_TYPE_KEYWORD,
+                                            filter));
+    }
+
+  if (!dex_await (dex_future_allv ((DexFuture **)futures->pdata, futures->len), &error))
+    return dex_future_new_for_error (g_steal_pointer (&error));
+
+  store = g_list_store_new (G_TYPE_LIST_MODEL);
+
+  for (guint i = 0; i < futures->len; i++)
+    {
+      DexFuture *future = g_ptr_array_index (futures, i);
+      GomResourceGroup *group = g_value_get_object (dex_future_get_value (future, NULL));
+      g_autoptr(ManualsSearchModel) wrapped = manuals_search_model_new (group);
+
+      g_list_store_append (store, wrapped);
+    }
+
+  return dex_future_new_take_object (gtk_flatten_list_model_new (g_object_ref (G_LIST_MODEL (store))));
 }
 
 DexFuture *
@@ -286,9 +372,9 @@ manuals_search_query_execute (ManualsSearchQuery *self,
                               ManualsRepository  *repository)
 {
   g_autoptr(GListStore) store = NULL;
-  g_autoptr(GomFilter) keyword_filter = NULL;
   g_auto(GValue) like_value = G_VALUE_INIT;
   DexFuture *future;
+  Execute *execute;
 
   g_return_val_if_fail (MANUALS_IS_SEARCH_QUERY (self), NULL);
   g_return_val_if_fail (MANUALS_IS_REPOSITORY (repository), NULL);
@@ -298,20 +384,17 @@ manuals_search_query_execute (ManualsSearchQuery *self,
 
   self->state = STATE_RUNNING;
 
-  store = g_list_store_new (G_TYPE_LIST_MODEL);
-
   g_value_init (&like_value, G_TYPE_STRING);
   g_value_take_string (&like_value, like_string (self->text));
-  keyword_filter = gom_filter_new_like (MANUALS_TYPE_KEYWORD, "name", &like_value);
 
-  future = dex_future_then (gom_repository_find (GOM_REPOSITORY (repository), MANUALS_TYPE_KEYWORD, keyword_filter),
-                            manuals_search_query_execute_cb,
-                            g_object_ref (store),
-                            g_object_unref);
-  future = dex_future_then (future,
-                            manuals_search_query_completed_cb,
-                            g_object_ref (store),
-                            g_object_unref);
+  execute = g_new0 (Execute, 1);
+  execute->keyword_filter = gom_filter_new_like (MANUALS_TYPE_KEYWORD, "name", &like_value);
+  execute->repository = g_object_ref (repository);
+
+  future = dex_scheduler_spawn (NULL, 0,
+                                manuals_search_query_execute_fiber,
+                                execute,
+                                (GDestroyNotify)execute_free);
   future = dex_future_then (future,
                             manuals_search_query_propagate_cb,
                             g_object_ref (self),
